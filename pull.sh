2464f4a71a3b348e2ad4563ba1c30a7123140278
Ben Smith <ben.thesmith@gmail.com>
2010-07-07 18:11:46 +0100

    changed svn build to git pull

---
+++ pull.sh
@@ -0,0 +1,6 @@
+cd ../common/ && git pull origin master && mvn clean install && mvn eclipse:eclipse
+cd ../atlas-model/ && git pull origin master && mvn clean install && mvn eclipse:eclipse
+cd ../atlas-feeds/ && git pull origin master && mvn clean install && mvn eclipse:eclipse
+cd ../atlas-persistence/ && git pull origin master && mvn clean install && mvn eclipse:eclipse
+cd ../atlas-client/ && git pull origin master && mvn clean install && mvn eclipse:eclipse
+cd ../atlas/ && git pull origin master && mvn clean install && mvn eclipse:eclipse
