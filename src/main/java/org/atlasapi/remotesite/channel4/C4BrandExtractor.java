b0a39f7d849c4922c93ac974421784314812fb9c
Ben Smith <ben.thesmith@gmail.com>
2010-08-12 11:08:39 +0100

    lots of little fixes, broadcasts, series, etc

--- src/main/java/org/atlasapi/remotesite/channel4/C4BrandExtractor.java
+++ src/main/java/org/atlasapi/remotesite/channel4/C4BrandExtractor.java
@@ -34,189 +34,206 @@ public class C4BrandExtractor implements ContentExtractor<Feed, Brand> {
 
     private static final Log LOG = LogFactory.getLog(C4BrandExtractor.class);
     private static final Pattern BAD_EPISODE_REDIRECT = Pattern.compile("(\\/episode-guide\\/series-\\d+)");
-    
-	private final C4BrandBasicDetailsExtractor basicDetailsExtractor = new C4BrandBasicDetailsExtractor();
-	private final C4SeriesExtractor seriesExtractor = new C4SeriesExtractor();
-	private final C4EpisodesExtractor itemExtrator = new C4EpisodesExtractor().includeOnDemands().includeBroadcasts();
-	private final C4EpisodeBroadcastExtractor broadcastExtractor = new C4EpisodeBroadcastExtractor();
-	private final RemoteSiteClient<Feed> feedClient;
 
-	public C4BrandExtractor() {
-		this(new AtomClient());
-	}
+    private final C4BrandBasicDetailsExtractor basicDetailsExtractor = new C4BrandBasicDetailsExtractor();
+    private final C4SeriesExtractor seriesExtractor = new C4SeriesExtractor();
+    private final C4EpisodesExtractor itemExtrator = new C4EpisodesExtractor().includeOnDemands().includeBroadcasts();
+    private final C4EpisodeBroadcastExtractor broadcastExtractor = new C4EpisodeBroadcastExtractor();
+    private final RemoteSiteClient<Feed> feedClient;
 
-	public C4BrandExtractor(RemoteSiteClient<Feed> atomClient) {
-		feedClient = atomClient;
-	}
+    public C4BrandExtractor() {
+        this(new AtomClient());
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public Brand extract(Feed source) {
-		Brand brand = basicDetailsExtractor.extract(source);
-		
-		List<Episode> items = Lists.newArrayList();
-		for (Series sery : fetchSeries(brand)) {
-			items.addAll((List) sery.getItems());
-		}
+    public C4BrandExtractor(RemoteSiteClient<Feed> atomClient) {
+        feedClient = atomClient;
+    }
 
-		Map<String, Episode> onDemandEpisodes = onDemandEpisodes(brand);
-		
-		for (Episode episode : items) {
-			Episode odEpisode = onDemandEpisodes.get(episode.getCanonicalUri());
-			if (odEpisode != null) {
-				episode.setVersions(odEpisode.getVersions());
-			}
-			if (episode.getTitle().equals(brand.getTitle())) {
-				if (episode.getSeriesNumber() != null && episode.getEpisodeNumber() != null) {
-					episode.setTitle("Series " + episode.getSeriesNumber() + " Episode " + episode.getEpisodeNumber());
-				}
-			}
-		}
-		
-		populateBroadcasts(items, brand);
-		
-		brand.setItems(items);
-		return brand;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public Brand extract(Feed source) {
+        Brand brand = basicDetailsExtractor.extract(source);
 
-	
-	private static Pattern ID_PATTERN = Pattern.compile("tag:www.channel4.com,2009:/programmes/([a-z0-9\\-]+)/episode-guide(?:/series-(\\d+)(?:/episode-(\\d+))?)?");
-	
-	private List<Series> fetchSeries(Brand brand) {
-		Feed episodeGuide = readEpisodeGuide(brand);
-		
-		Matcher matcher = ID_PATTERN.matcher(episodeGuide.getId());
-		
-		if (!matcher.matches()) {
-			throw new FetchException("Series guide id not recognised: " + episodeGuide.getId());
-		}
-		
-		// the feed has a series number
-		if (matcher.group(2) != null) {
-			// the feed also has an episode number, this is not the right feed -- read the series feed instead
-			if (matcher.group(3) != null) {
-				int seriesNumber = Integer.valueOf(matcher.group(2));
-				return loadSeriesFromFeeds(ImmutableList.of(C4AtomApi.requestForBrand(brand.getCanonicalUri(), "/episode-guide/series-" + seriesNumber + ".atom")));
-			} else {
-				// the feed is a series, pass to extractor
-				return ImmutableList.of(seriesExtractor.extract(episodeGuide));
-			}
-		}
-		// a real series guide
-		return loadSeriesFromFeeds(extractSeriesAtomFeedsFrom(episodeGuide));
-	}
+        List<Episode> items = Lists.newArrayList();
+        for (Series sery : fetchSeries(brand)) {
+            items.addAll((List) sery.getItems());
+        }
 
-	private List<Series> loadSeriesFromFeeds(List<String> seriesFeeds) {
-		List<Series> series = Lists.newArrayList();
-		for (String uri : seriesFeeds) {
-			try {
-				series.add(seriesExtractor.extract(feedClient.get(uri)));
-			} catch (Exception e) {
-				throw new FetchException("Could not fetch series: "  + uri, e);
-			}
-		}
-		return series;
-	}
+        Map<String, Episode> onDemandEpisodes = onDemandEpisodes(brand);
 
-	@SuppressWarnings("unchecked")
-	private List<String> extractSeriesAtomFeedsFrom(Feed episodeGuide) {
-		List<String> seriesUris = Lists.newArrayList();
-		for (Entry entry : (List<Entry>) episodeGuide.getEntries()) {
-			List<Link> alternateLinks = entry.getOtherLinks();
-			for (Link link : alternateLinks) {
-				if (MimeType.APPLICATION_ATOM_XML.toString().equals(link.getType())) {
-					seriesUris.add(link.getHref());
-				}
-			}
-		}
-		return seriesUris;
-	}
+        for (Episode episode : items) {
+            Episode odEpisode = onDemandEpisodes.get(episode.getCanonicalUri());
+            if (odEpisode != null) {
+                episode.setVersions(odEpisode.getVersions());
+            }
+            if (episode.getTitle().equals(brand.getTitle())) {
+                if (episode.getSeriesNumber() != null && episode.getEpisodeNumber() != null) {
+                    episode.setTitle("Series " + episode.getSeriesNumber() + " Episode " + episode.getEpisodeNumber());
+                }
+            }
+        }
 
-	/**
-	 * @return The feed if it exists or null if the page is probably a special case
-	 */
-	private Feed readEpisodeGuide(Brand brand) {
-		try {
-			return fetch(brand, "/episode-guide.atom");
-		} catch (HttpStatusCodeException e) {
-		    if (e.getStatusCode() == 403) {
-		        Matcher matcher = BAD_EPISODE_REDIRECT.matcher(e.getResponse().finalUrl());
-		        if (matcher.find()) {
-		            try {
-                        return fetch(brand, matcher.group(1)+".atom");
-		            } catch (HttpStatusCodeException e1) {
-		                return fetchDefaultSeriesOrGiveUp(brand);
-                    } catch (Exception e1) {}
-		        }
-		    } 
-		    if (e.getStatusCode() == HttpServletResponse.SC_NOT_FOUND) {
-               return fetchDefaultSeriesOrGiveUp(brand);
-		    }
-		    throw new FetchException("could not fetch series guide for " + brand.getCanonicalUri(), e);
-		} catch (Exception e) {
-			throw new FetchException("could not read episode guide for ", e);
-		}
-	}
+        populateBroadcasts(items, brand);
 
-	private Feed fetchDefaultSeriesOrGiveUp(Brand brand) {
-		try {
-			return fetch(brand, "/episode-guide/series-1.atom");
-		} catch (Exception e) {
-			throw new FetchException("Could not find any series information for " + brand.getCanonicalUri(), e);
-		}
-	}
-	
-	private Map<String, Episode> onDemandEpisodes(Brand brand) {
-		try {
-			return toMap(itemExtrator.extract(fetch(brand, "/4od.atom")));
-		} catch (HttpStatusCodeException e) {
-			if (HttpServletResponse.SC_NOT_FOUND == e.getStatusCode()) {
-				return ImmutableMap.of();
-			}
-			throw new FetchException("could not read on demand info for " + brand.getCanonicalUri(), e);
-		} catch (Exception e) {
-			throw new FetchException("could not read on demand info for " + brand.getCanonicalUri(), e);
-		} 
-	}
-	
-	private void populateBroadcasts(List<Episode> episodes, Brand brand) {
-	    try {
-            Feed epg = fetch(brand, "/epg.atom");
-            List<Episode> broadcastEpisodes = broadcastExtractor.extract(epg);
-            
-            for (Episode broadcastEpisode: broadcastEpisodes) {
-                for (Episode episode: episodes) {
-                    if (episode.getCanonicalUri().equals(broadcastEpisode.getCanonicalUri())) {
-                        Broadcast broadcast = broadcastEpisode.getVersions().iterator().next().getBroadcasts().iterator().next();
-                        Version version = episode.getVersions().iterator().next();
-                        
-                        for (Broadcast currentBroadcast: version.getBroadcasts()) {
-                            if (currentBroadcast.equals(broadcast)) {
-                                currentBroadcast.setAliases(broadcast.getAliases());
-                                return;
-                            }
+        brand.setItems(items);
+        return brand;
+    }
+
+    private static Pattern ID_PATTERN = Pattern.compile("tag:www.channel4.com,\\d+:/programmes/([a-z0-9\\-]+)/episode-guide(?:/series-(\\d+)(?:/episode-(\\d+))?)?");
+
+    private List<Series> fetchSeries(Brand brand) {
+        Feed episodeGuide = readEpisodeGuide(brand);
+
+        Matcher matcher = ID_PATTERN.matcher(episodeGuide.getId());
+
+        if (!matcher.matches()) {
+            throw new FetchException("Series guide id not recognised: " + episodeGuide.getId());
+        }
+
+        // the feed has a series number
+        if (matcher.group(2) != null) {
+            // the feed also has an episode number, this is not the right feed
+            // -- read the series feed instead
+            if (matcher.group(3) != null) {
+                int seriesNumber = Integer.valueOf(matcher.group(2));
+                return loadSeriesFromFeeds(ImmutableList.of(C4AtomApi.requestForBrand(brand.getCanonicalUri(), "/episode-guide/series-" + seriesNumber + ".atom")));
+            } else {
+                // the feed is a series, pass to extractor
+                return ImmutableList.of(seriesExtractor.extract(episodeGuide));
+            }
+        }
+        // a real series guide
+        return loadSeriesFromFeeds(extractSeriesAtomFeedsFrom(episodeGuide));
+    }
+
+    private List<Series> loadSeriesFromFeeds(List<String> seriesFeeds) {
+        List<Series> series = Lists.newArrayList();
+        for (String uri : seriesFeeds) {
+            try {
+                series.add(seriesExtractor.extract(feedClient.get(uri)));
+            } catch (Exception e) {
+                throw new FetchException("Could not fetch series: " + uri, e);
+            }
+        }
+        return series;
+    }
+
+    private static final Pattern SERIES_LINK = Pattern.compile("^(https?://api.channel4.com/programmes/[a-z0-9\\-]+/episode-guide/series-\\d+).*(.atom.*)$");
+
+    @SuppressWarnings("unchecked")
+    private List<String> extractSeriesAtomFeedsFrom(Feed episodeGuide) {
+        List<String> seriesUris = Lists.newArrayList();
+        for (Entry entry : (List<Entry>) episodeGuide.getEntries()) {
+            List<Link> alternateLinks = entry.getOtherLinks();
+            for (Link link : alternateLinks) {
+                if (MimeType.APPLICATION_ATOM_XML.toString().equals(link.getType())) {
+                    Matcher matcher = SERIES_LINK.matcher(link.getHref());
+                    if (matcher.matches()) {
+                        String href = matcher.group(1) + matcher.group(2);
+                        if (!seriesUris.contains(href)) {
+                            seriesUris.add(href);
                         }
-                        
-                        version.addBroadcast(broadcast);
-                        return;
                     }
                 }
             }
-        } catch (Exception e) {
-            LOG.warn("Unable to retrieve epg information for brand: "+brand.getCanonicalUri());
         }
-	}
+        return seriesUris;
+    }
 
-	private static <T extends Content> Map<String, T> toMap(Iterable<T> contents) {
-		Map<String, T> lookup = Maps.newHashMap();
-		for (T content : contents) {
-			lookup.put(content.getCanonicalUri(), content);
-		}
-		return lookup;
-	}
+    /**
+     * @return The feed if it exists or null if the page is probably a special
+     *         case
+     */
+    private Feed readEpisodeGuide(Brand brand) {
+        try {
+            return fetch(brand, "/episode-guide.atom");
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == 403) {
+                Matcher matcher = BAD_EPISODE_REDIRECT.matcher(e.getResponse().finalUrl());
+                if (matcher.find()) {
+                    try {
+                        return fetch(brand, matcher.group(1) + ".atom");
+                    } catch (HttpStatusCodeException e1) {
+                        return fetchDefaultSeriesOrGiveUp(brand);
+                    } catch (Exception e1) {
+                    }
+                }
+            }
+            if (e.getStatusCode() == HttpServletResponse.SC_NOT_FOUND) {
+                return fetchDefaultSeriesOrGiveUp(brand);
+            }
+            throw new FetchException("could not fetch series guide for " + brand.getCanonicalUri(), e);
+        } catch (Exception e) {
+            throw new FetchException("could not read episode guide for ", e);
+        }
+    }
 
-	private Feed fetch(Brand brand, String extension) throws Exception {
-		String url = C4AtomApi.requestForBrand(brand.getCanonicalUri(), extension);
-		return feedClient.get(url);
-	}
+    private Feed fetchDefaultSeriesOrGiveUp(Brand brand) {
+        try {
+            return fetch(brand, "/episode-guide/series-1.atom");
+        } catch (Exception e) {
+            throw new FetchException("Could not find any series information for " + brand.getCanonicalUri(), e);
+        }
+    }
+
+    private Map<String, Episode> onDemandEpisodes(Brand brand) {
+        try {
+            return toMap(itemExtrator.extract(fetch(brand, "/4od.atom")));
+        } catch (HttpStatusCodeException e) {
+            if (HttpServletResponse.SC_NOT_FOUND == e.getStatusCode()) {
+                return ImmutableMap.of();
+            }
+            throw new FetchException("could not read on demand info for " + brand.getCanonicalUri(), e);
+        } catch (Exception e) {
+            throw new FetchException("could not read on demand info for " + brand.getCanonicalUri(), e);
+        }
+    }
+
+    private void populateBroadcasts(List<Episode> episodes, Brand brand) {
+        List<Episode> broadcastEpisodes = Lists.newArrayList();
+        try {
+            Feed epg = fetch(brand, "/epg.atom");
+            broadcastEpisodes = broadcastExtractor.extract(epg);
+        } catch (Exception e) {
+            LOG.warn("Unable to retrieve epg information for brand: " + brand.getCanonicalUri());
+        }
+
+        for (Episode broadcastEpisode : broadcastEpisodes) {
+            for (Episode episode : episodes) {
+                if (episode.getCanonicalUri().equals(broadcastEpisode.getCanonicalUri())) {
+                    Broadcast broadcast = broadcastEpisode.getVersions().iterator().next().getBroadcasts().iterator().next();
+                    Version version = null;
+                    if (episode.getVersions().isEmpty()) {
+                        version = new Version();
+                        episode.addVersion(version);
+                    } else {
+                        version = episode.getVersions().iterator().next();
+                    }
+
+                    for (Broadcast currentBroadcast : version.getBroadcasts()) {
+                        if (currentBroadcast.equals(broadcast)) {
+                            currentBroadcast.setAliases(broadcast.getAliases());
+                            return;
+                        }
+                    }
+
+                    version.addBroadcast(broadcast);
+                    return;
+                }
+            }
+        }
+    }
+
+    private static <T extends Content> Map<String, T> toMap(Iterable<T> contents) {
+        Map<String, T> lookup = Maps.newHashMap();
+        for (T content : contents) {
+            lookup.put(content.getCanonicalUri(), content);
+        }
+        return lookup;
+    }
+
+    private Feed fetch(Brand brand, String extension) throws Exception {
+        String url = C4AtomApi.requestForBrand(brand.getCanonicalUri(), extension);
+        return feedClient.get(url);
+    }
 }
