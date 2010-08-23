e5e2b61fed4fee0a177b7b128629195d2d51f56c
John Ayres <john@metabroadcast.com>
2010-08-23 14:05:56 +0100

    package rename

--- src/main/java/org/atlasapi/tracking/TrackingModule.java
+++ src/main/java/org/atlasapi/tracking/TrackingModule.java
@@ -12,8 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.collect.ImmutableSet;
-import com.metabroadcast.common.social.buzz.twitter.TweetProcessor;
-import com.metabroadcast.common.social.buzz.twitter.stream.TwitterFilteredPipe;
+import com.metabroadcast.common.social.twitter.stream.TweetProcessor;
+import com.metabroadcast.common.social.twitter.stream.TwitterFilteredPipe;
 
 @Configuration
 public class TrackingModule {
