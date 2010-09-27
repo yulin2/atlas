2b0aaadca55f69867e2c561bd903e78eac3438cc
John Ayres <john@metabroadcast.com>
2010-09-27 11:43:02 +0100

    add common health controller

--- src/main/java/org/atlasapi/logging/AtlasLoggingModule.java
+++ src/main/java/org/atlasapi/logging/AtlasLoggingModule.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.collect.ImmutableList;
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.metabroadcast.common.webapp.health.HealthController;
+import com.metabroadcast.common.webapp.health.HealthProbe;
+import com.metabroadcast.common.webapp.health.probes.DiskSpaceProbe;
+import com.metabroadcast.common.webapp.health.probes.MemoryInfoProbe;
 import com.mongodb.Mongo;
 
 @Configuration
@@ -22,5 +27,7 @@ public class AtlasLoggingModule {
 		return new LogViewingController(adapterLog());
 	}
 	
-	
+	public @Bean HealthController healthController() {
+		return new HealthController(ImmutableList.<HealthProbe>of(new MemoryInfoProbe(), new DiskSpaceProbe()));
+	}
 }
