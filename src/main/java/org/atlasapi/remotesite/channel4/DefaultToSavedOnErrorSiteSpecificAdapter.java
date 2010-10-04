e267f3de89a2e2655dd0902816a46c7190d7fe63
John Ayres <john@metabroadcast.com>
2010-10-04 15:40:46 +0100

    check publisher matches

--- src/main/java/org/atlasapi/remotesite/channel4/DefaultToSavedOnErrorSiteSpecificAdapter.java
+++ src/main/java/org/atlasapi/remotesite/channel4/DefaultToSavedOnErrorSiteSpecificAdapter.java
@@ -1,5 +1,7 @@
 package org.atlasapi.remotesite.channel4;
 
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.logging.AdapterLogEntry;
@@ -11,10 +13,12 @@ public class DefaultToSavedOnErrorSiteSpecificAdapter<T> implements SiteSpecific
 	private final ContentResolver contentStore;
 	private final SiteSpecificAdapter<T> delegate;
 	private final AdapterLog log;
+	private final Publisher publisher;
 
-	public DefaultToSavedOnErrorSiteSpecificAdapter(SiteSpecificAdapter<T> delegate, ContentResolver contentStore, AdapterLog log) {
+	public DefaultToSavedOnErrorSiteSpecificAdapter(SiteSpecificAdapter<T> delegate, ContentResolver contentStore, Publisher publisher, AdapterLog log) {
 		this.delegate = delegate;
 		this.contentStore = contentStore;
+		this.publisher = publisher;
 		this.log = log;
 	}
 	
@@ -30,7 +34,11 @@ public class DefaultToSavedOnErrorSiteSpecificAdapter<T> implements SiteSpecific
 			return delegate.fetch(uri);
 		} catch (Exception e) {
 			log.record(new AdapterLogEntry(Severity.ERROR).withCause(e).withUri(uri).withSource(delegate.getClass()));
-			return (T) contentStore.findByUri(uri);
+			Content found = contentStore.findByUri(uri);
+			if (publisher.equals(found.getPublisher())) {
+				return (T) found;
+			}
+			return null;
 		}
 	}
 }
