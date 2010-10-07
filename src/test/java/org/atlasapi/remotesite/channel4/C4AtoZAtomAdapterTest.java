ceaeb51d1372fd4e5ad514a4e30f5c3dde8b2ced
John Ayres <john@metabroadcast.com>
2010-10-07 18:21:05 +0100

    fix test

--- src/test/java/org/atlasapi/remotesite/channel4/C4AtoZAtomAdapterTest.java
+++ src/test/java/org/atlasapi/remotesite/channel4/C4AtoZAtomAdapterTest.java
@@ -65,7 +65,7 @@ public class C4AtoZAtomAdapterTest extends MockObjectTestCase {
 			
 			one(writer).createOrUpdatePlaylist(brand101, true);
 			one(writer).createOrUpdatePlaylist(brand202, true);
-			one(writer).createOrUpdatePlaylist(new Playlist(uri, null), true);
+			one(writer).createOrUpdatePlaylistSkeleton(new Playlist(uri, null));
 
 			allowing(brandAdapter).fetch("http://www.channel4.com/programmes/a-bipolar-expedition"); will(returnValue(brand101));
 			allowing(brandAdapter).fetch("http://www.channel4.com/programmes/a-bipolar-expedition-part-2"); will(returnValue(brand202));
