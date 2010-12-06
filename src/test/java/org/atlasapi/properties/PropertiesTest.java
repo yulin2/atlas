b8cdb2364391f6d756ca754967b6fac319be3871
Ben Smith <ben.thesmith@gmail.com>
2010-12-06 17:29:25 +0000

    fixing a stupid test, but didnt make it less stupid

--- src/test/java/org/atlasapi/properties/PropertiesTest.java
+++ src/test/java/org/atlasapi/properties/PropertiesTest.java
@@ -7,6 +7,6 @@ import com.metabroadcast.common.properties.Configurer;
 public class PropertiesTest extends TestCase {
     public void testShouldTakeProdProperties() throws Exception {
         Configurer.load("prod");
-        assertEquals("10.228.167.143", Configurer.get("mongo.host").get());
+        assertEquals("10.235.57.61", Configurer.get("mongo.host").get());
     }
 }
