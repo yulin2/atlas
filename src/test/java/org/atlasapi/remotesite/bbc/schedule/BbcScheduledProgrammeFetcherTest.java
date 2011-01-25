432fd7fa0d06611aa733c92b55590193be6cd1bf
John Ayres <john@metabroadcast.com>
2011-01-25 15:11:26 +0000

    fix test broken by bad merge

--- src/test/java/org/atlasapi/remotesite/bbc/schedule/BbcScheduledProgrammeFetcherTest.java
+++ src/test/java/org/atlasapi/remotesite/bbc/schedule/BbcScheduledProgrammeFetcherTest.java
@@ -45,22 +45,15 @@ public class BbcScheduledProgrammeFetcherTest extends MockObjectTestCase {
 	
 	public void testFetchExtractedEpisodePid() throws Exception {
 	    
-	    final Episode containedInBrand = new Episode("containedInBrandUri", "containedInBrandCurie", Publisher.BBC);
-        
-	    final Brand brand = new Brand("brandUri", "brandCurie", Publisher.BBC);
         brand.setContents(containedInBrand);
         
-        final Episode replacingEpisode = new Episode("containedInBrandUri", "replacingCurie", Publisher.BBC);
         replacingEpisode.setContainer(brand);
-        
-        final Episode withoutBrand = new Episode("episodeWithoutBrandUri", "episodeWithoutBrandCurie", Publisher.BBC);
-        
+		
 		BbcScheduledProgrammeUpdater scheduleFetcher = new BbcScheduledProgrammeUpdater(scheduleClient, localFetcher, fetcher, writer, Lists.newArrayList("http://www.bbc.co.uk/bbctwo/programmes/schedules/england/2009/11/05.xml"), new NullAdapterLog());
 		
 		checking(new Expectations() {{ 
 			one(scheduleClient).get("http://www.bbc.co.uk/bbctwo/programmes/schedules/england/2009/11/05.xml"); will(returnValue(schedule));
-			one(fetcher).fetch("http://www.bbc.co.uk/programmes/b00abcd"); will(returnValue(replacingEpisode));
-			one(fetcher).fetch("http://www.bbc.co.uk/programmes/b00efgh"); will(returnValue(withoutBrand));
+
 			one(localFetcher).findByCanonicalUri("brandUri"); will(returnValue(brand));
 			one(writer).createOrUpdate(with(any(Brand.class)), with(true));
 			one(writer).createOrUpdate(with(any(Item.class)));
