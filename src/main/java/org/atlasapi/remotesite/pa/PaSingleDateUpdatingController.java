fa4779f6f3564186a3b3b080f2c9eab049edbccf
Ben Smith <ben@thesmith.co.uk>
2011-03-23 15:51:07 +0000

    refactored pa updating

--- src/main/java/org/atlasapi/remotesite/pa/PaSingleDateUpdatingController.java
+++ src/main/java/org/atlasapi/remotesite/pa/PaSingleDateUpdatingController.java
@@ -6,13 +6,16 @@ import java.util.concurrent.Executors;
 import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletResponse;
 
+import org.atlasapi.media.entity.Channel;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.remotesite.pa.data.PaProgrammeDataStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.metabroadcast.common.base.Maybe;
 
 @Controller
 public class PaSingleDateUpdatingController {
@@ -37,5 +40,18 @@ public class PaSingleDateUpdatingController {
     public void runUpdate(@PathVariable String dateString, HttpServletResponse response) {
         executor.execute(new PaSingleDateUpdater(processor, log, fileManager, dateString));
         response.setStatus(200);
+    }
+    
+    @RequestMapping("/system/update/pa/{dateString}/{channelString}")
+    public void runUpdate(@PathVariable String dateString, @PathVariable String channelString, HttpServletResponse response) {
+        Maybe<Channel> channel = Channel.fromKey(channelString);
+        if (channel.hasValue()) {
+            PaSingleDateUpdater updater = new PaSingleDateUpdater(processor, log, fileManager, dateString);
+            updater.supportChannels(ImmutableList.of(channel.requireValue()));
+            executor.execute(updater);
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 }
