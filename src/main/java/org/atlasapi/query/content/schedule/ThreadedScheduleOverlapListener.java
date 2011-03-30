c64a4365a101a6b01f086fa22daa79592d993426
Ben Smith <ben.thesmith@gmail.com>
2011-03-30 13:49:12 +0100

    added logging to overlap listener

--- src/main/java/org/atlasapi/query/content/schedule/ThreadedScheduleOverlapListener.java
+++ src/main/java/org/atlasapi/query/content/schedule/ThreadedScheduleOverlapListener.java
@@ -7,16 +7,21 @@ import javax.annotation.PreDestroy;
 
 import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.media.entity.Item;
+import org.atlasapi.persistence.logging.AdapterLog;
+import org.atlasapi.persistence.logging.AdapterLogEntry;
+import org.atlasapi.persistence.logging.AdapterLogEntry.Severity;
 
 public class ThreadedScheduleOverlapListener implements ScheduleOverlapListener {
 
     private final ScheduleOverlapListener delegate;
     private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final AdapterLog log;
 
-    public ThreadedScheduleOverlapListener(ScheduleOverlapListener delegate) {
+    public ThreadedScheduleOverlapListener(ScheduleOverlapListener delegate, AdapterLog log) {
         this.delegate = delegate;
+        this.log = log;
     }
-    
+
     @PreDestroy
     public void shutdown() {
         executor.shutdown();
@@ -26,9 +31,9 @@ public class ThreadedScheduleOverlapListener implements ScheduleOverlapListener 
     public void itemRemovedFromSchedule(Item item, Broadcast broadcast) {
         executor.execute(new NotifyDelegate(item, broadcast));
     }
-    
+
     class NotifyDelegate implements Runnable {
-        
+
         private final Item item;
         private final Broadcast broadcast;
 
@@ -36,9 +41,13 @@ public class ThreadedScheduleOverlapListener implements ScheduleOverlapListener 
             this.item = item;
             this.broadcast = broadcast;
         }
-        
+
         public void run() {
+            try {
             delegate.itemRemovedFromSchedule(item, broadcast);
+            } catch (Exception e) {
+                log.record(new AdapterLogEntry(Severity.ERROR).withCause(e).withSource(getClass()).withDescription(e.getMessage()));
+            }
         }
     }
 }
