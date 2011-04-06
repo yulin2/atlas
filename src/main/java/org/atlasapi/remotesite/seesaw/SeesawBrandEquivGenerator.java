dd58060cc08e149fe6e5290d3387353862056b0e
Ben Smith <ben.thesmith@gmail.com>
2011-04-06 15:38:12 +0100

    removing series from the flattened titles as seesaw is leaving that in

--- src/main/java/org/atlasapi/remotesite/seesaw/SeesawBrandEquivGenerator.java
+++ src/main/java/org/atlasapi/remotesite/seesaw/SeesawBrandEquivGenerator.java
@@ -109,6 +109,7 @@ public class SeesawBrandEquivGenerator implements EquivGenerator<Container<?>> {
                 Set<Integer> seriesNumbers = seriesNumbersBuilder.build();
                 if (! Strings.isNullOrEmpty(brand.getTitle()) && ! seriesNumbers.isEmpty()) {
                     String flattened = brand.getTitle().replaceAll("\\W", "").toLowerCase();
+                    flattened = flattened.replaceFirst("series$", "");
                     if (! Strings.isNullOrEmpty(flattened)) {
                         return Maybe.just(new BrandEquivKey(flattened, seriesNumbers));
                     }
