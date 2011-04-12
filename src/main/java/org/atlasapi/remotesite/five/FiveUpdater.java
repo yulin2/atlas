0833dd754b24f55f2f3635c40042155858a8c50d
Daniel Bell <shinyhappydan@gmail.com>
2011-04-12 11:08:31 +0100

    Five updater downloads the whole XML file before processing to avoid holding the connection open

--- src/main/java/org/atlasapi/remotesite/five/FiveUpdater.java
+++ src/main/java/org/atlasapi/remotesite/five/FiveUpdater.java
@@ -9,12 +9,18 @@ import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.logging.AdapterLogEntry;
 import org.atlasapi.persistence.logging.AdapterLogEntry.Severity;
+import org.atlasapi.remotesite.HttpClients;
+
+import com.metabroadcast.common.time.SystemClock;
+import com.metabroadcast.common.time.Timestamp;
+import com.metabroadcast.common.time.Timestamper;
 
 public class FiveUpdater implements Runnable {
     
     private final AdapterLog log;
     private final String baseApiUrl;
     private final FiveBrandProcessor processor;
+    private final Timestamper timestamper = new SystemClock();
 
     public FiveUpdater(ContentWriter contentWriter, AdapterLog log, String baseApiUrl) {
         this.log = log;
@@ -25,12 +31,14 @@ public class FiveUpdater implements Runnable {
     @Override
     public void run() {
         try {
+            Timestamp start = timestamper.timestamp();
             log.record(new AdapterLogEntry(Severity.INFO).withDescription("Five update started").withSource(getClass()));
             
             Builder parser = new Builder(new ShowProcessingNodeFactory());
-            parser.build(baseApiUrl + "/shows");
+            parser.build(HttpClients.webserviceClient().getContentsOf(baseApiUrl + "/shows"), null);
             
-            log.record(new AdapterLogEntry(Severity.INFO).withDescription("Five update completed").withSource(getClass()));
+            Timestamp end = timestamper.timestamp();
+            log.record(new AdapterLogEntry(Severity.INFO).withDescription("Five update completed in " + start.durationTo(end).getStandardSeconds() + " seconds").withSource(getClass()));
         }
         catch (Exception e) {
             log.record(new AdapterLogEntry(Severity.ERROR).withCause(e).withSource(getClass()).withDescription("Exception when processing shows document"));
@@ -38,6 +46,7 @@ public class FiveUpdater implements Runnable {
     }
     
     private class ShowProcessingNodeFactory extends NodeFactory {
+        
         @Override
         public Nodes finishMakingElement(Element element) {
             if (element.getLocalName().equalsIgnoreCase("show")) {
