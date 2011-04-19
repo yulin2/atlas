7db14972d29c080ec8a96e598623a22796ef52ba
John Ayres <john@metabroadcast.com>
2011-04-19 15:39:56 +0100

    don't load full episode from feed unless we've not seen it before

--- src/main/java/org/atlasapi/remotesite/bbc/BbcModule.java
+++ src/main/java/org/atlasapi/remotesite/bbc/BbcModule.java
@@ -49,7 +49,7 @@ public class BbcModule {
         scheduler.schedule(bbcFeedsUpdater(), BRAND_UPDATE_TIME);
         scheduler.schedule(bbcHighlightsUpdater(), HIGHLIGHTS_UPDATE_TIME);
         scheduler.schedule(bbcIonUpdater(0, 0).withItemFetchClient(new BbcIonEpisodeDetailItemFetcherClient(log)), TEN_MINUTES);
-        scheduler.schedule(bbcIonUpdater(7, 7).withItemFetchClient(new BbcIonEpisodeDetailItemFetcherClient(log)).withAlwaysUseRemote(), ONE_HOUR);
+        scheduler.schedule(bbcIonUpdater(7, 7).withItemFetchClient(new BbcIonEpisodeDetailItemFetcherClient(log)), ONE_HOUR);
         scheduler.schedule(bbcIonOndemandChangeUpdater(), SEVEN_MINUTES);
         log.record(new AdapterLogEntry(Severity.INFO).withSource(getClass()).withDescription("BBC update scheduled tasks installed"));
     }
