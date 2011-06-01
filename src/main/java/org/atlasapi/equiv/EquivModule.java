8e3d84b93555eb97fbb5c21e8972da64cbb58096
Fred van den Driessche <fred@metabroadcast.com>
2011-06-01 18:14:25 +0100

    removed old equiv

--- src/main/java/org/atlasapi/equiv/EquivModule.java
+++ src/main/java/org/atlasapi/equiv/EquivModule.java
@@ -43,25 +43,18 @@ import org.atlasapi.equiv.update.ContentEquivalenceUpdateTask;
 import org.atlasapi.equiv.update.ContentEquivalenceUpdater;
 import org.atlasapi.equiv.update.LookupWritingEquivalenceUpdater;
 import org.atlasapi.equiv.update.RootEquivalenceUpdater;
-import org.atlasapi.equiv.www.EquivController;
 import org.atlasapi.media.entity.Container;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Publisher;
-import org.atlasapi.persistence.content.AggregateContentListener;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.RetrospectiveContentLister;
 import org.atlasapi.persistence.content.ScheduleResolver;
 import org.atlasapi.persistence.content.SearchResolver;
-import org.atlasapi.persistence.content.mongo.MongoDbBackedContentStore;
-import org.atlasapi.persistence.equiv.EquivalentUrlStore;
-import org.atlasapi.persistence.equiv.MongoEquivStore;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.lookup.LookupWriter;
 import org.atlasapi.persistence.lookup.TransitiveLookupWriter;
 import org.atlasapi.persistence.lookup.mongo.MongoLookupEntryStore;
-import org.atlasapi.remotesite.EquivGenerator;
-import org.atlasapi.remotesite.freebase.FreebaseBrandEquivGenerator;
 import org.joda.time.Duration;
 import org.joda.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,7 +62,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
 import com.metabroadcast.common.scheduling.RepetitionRule;
@@ -81,79 +73,8 @@ import com.metabroadcast.common.time.DayOfWeek;
 public class EquivModule {
 
 	private @Autowired DatabasedMongo db;
-	private @Autowired AggregateContentListener aggregateContentListener;
-	private @Value("${freebase.enabled}") String freebaseEnabled;
-	private @Value("${seesaw.equiv.enabled}") String seesawEquivEnabled;
 	private @Value("${equiv.updater.enabled}") String updaterEnabled;
-	private @Value("${itunes.equiv.enabled}") String itunesEquivEnabled;
-	
-	@Bean EquivController manualEquivAssignmentController() {
-		return new EquivController(store());
-	}
-	
-	public @Bean EquivalentUrlStore store() {
-		return new MongoEquivStore(db);
-	}
-	
-	@Bean EquivContentListener equivContentListener() {
-	    ImmutableList.Builder<EquivGenerator<Container<?>>> brandEquivGenerators = ImmutableList.builder(); 
-	    ImmutableList.Builder<EquivGenerator<Item>> itemEquivGenerators = ImmutableList.builder();
-	    if (Boolean.parseBoolean(freebaseEnabled)) {
-	        brandEquivGenerators.add(new FreebaseBrandEquivGenerator());
-	    }
-	    if (Boolean.parseBoolean(seesawEquivEnabled)) {
-	        PublisherCachingBrandEquivGenerator seesawBrandEquivGenerator = new PublisherCachingBrandEquivGenerator(Publisher.SEESAW, new MongoDbBackedContentStore(db));
-            brandEquivGenerators.add(seesawBrandEquivGenerator);
-	        itemEquivGenerators.add(new ItemDelegatingToBrandEquivGenerator(seesawBrandEquivGenerator));
-	    }
-	    if (Boolean.parseBoolean(itunesEquivEnabled)) {
-	        PublisherCachingBrandEquivGenerator itunesBrandEquivGenerator = new PublisherCachingBrandEquivGenerator(Publisher.ITUNES, new MongoDbBackedContentStore(db));
-            brandEquivGenerators.add(itunesBrandEquivGenerator);
-            itemEquivGenerators.add(new ItemDelegatingToBrandEquivGenerator(itunesBrandEquivGenerator));
-	    }
-	    
-	    BrandEquivUpdater brandUpdater = new BrandEquivUpdater(brandEquivGenerators.build(), store());
-	    ItemEquivUpdater itemUpdater = new ItemEquivUpdater(itemEquivGenerators.build(), store());
-	    
-	    EquivContentListener equivContentListener = new EquivContentListener(brandUpdater, itemUpdater);
-	    aggregateContentListener.addListener(equivContentListener);
-	    return equivContentListener;
-	}
-	
-//	private @Autowired SimpleScheduler scheduler;
-//	
-//	@PostConstruct
-//	public void scheduleEquivUpdaters() {
-//	    if(Boolean.valueOf(updaterEnabled)) {
-//	        scheduler.schedule(new PaEquivUpdateTask(new MongoDbBackedContentStore(db), itemBasedBrandEquivUpdater(), filmEquivUpdater(), equivResultStore(), log), RepetitionRules.every(Duration.standardHours(10)));
-//	    }
-//	}
-//	
-//	@Bean ItemEquivGenerator itemEquivGenerator() {
-//	    ItemEquivGenerator broadcastEquivGen = new BroadcastMatchingItemEquivGenerator(scheduleResolver);
-//	    return new DelegatingItemEquivGenerator(ImmutableList.of(broadcastEquivGen));
-//	}
-//	
-//	@Bean ItemBasedBrandEquivUpdater itemBasedBrandEquivUpdater() {
-//	    MongoDbBackedContentStore mongoDbBackedContentStore = new MongoDbBackedContentStore(db);
-//	    return new ItemBasedBrandEquivUpdater(itemEquivGenerator(), mongoDbBackedContentStore, mongoDbBackedContentStore).writesResults(true);
-//	}
-//	
-//	@Bean FilmEquivUpdater filmEquivUpdater() {
-//	    return new FilmEquivUpdater(searchResolver, new MongoDbBackedContentStore(db));
-//	}
-//	
-//	@Bean EquivResultStore equivResultStore() {
-//	    return new CachingEquivResultStore(new MongoEquivResultStore(db));
-//	}
-//	
-//	@Bean EquivResultController equivResultController() {
-//	    return new EquivResultController(equivResultStore());
-//	}
-//	
-//	@Bean SingleBrandEquivUpdateController singleBrandUpdater() {
-//	    return new SingleBrandEquivUpdateController(itemBasedBrandEquivUpdater(), new MongoDbBackedContentStore(db));
-//	}
+
 	private static final RepetitionRule EQUIVALENCE_REPETITION = RepetitionRules.weekly(DayOfWeek.MONDAY, new LocalTime(9, 00));
     
     private @Autowired ScheduleResolver scheduleResolver;
