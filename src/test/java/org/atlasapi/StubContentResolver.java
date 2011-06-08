593cb628458ce75a5ff1dc3a39cb137ea509cb8f
John Ayres <john@metabroadcast.com>
2011-06-08 12:54:32 +0100

    fix tests

--- src/test/java/org/atlasapi/StubContentResolver.java
+++ src/test/java/org/atlasapi/StubContentResolver.java
@@ -5,13 +5,14 @@ import java.util.Map;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ResolvedContent;
+import org.atlasapi.persistence.content.ResolvedContent.ResolvedContentBuilder;
 
-import com.google.common.base.Predicates;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
 
 public class StubContentResolver implements ContentResolver {
 
+	public static ContentResolver RESOLVES_NOTHING = new StubContentResolver();
+	
 	private Map<String, Content> data = Maps.newHashMap();
 	
 	public StubContentResolver respondTo(Content content) {
@@ -20,8 +21,12 @@ public class StubContentResolver implements ContentResolver {
 	}
 
 	@Override
-	public ResolvedContent findByCanonicalUris(Iterable<String> uri) {
-		return ResolvedContent.builder().putAll(Maps.filterKeys(data, Predicates.in(ImmutableSet.copyOf(uri)))).build();
+	public ResolvedContent findByCanonicalUris(Iterable<String> uris) {
+		ResolvedContentBuilder builder = ResolvedContent.builder();
+		for (String uri : uris) {
+			builder.put(uri, data.get(uri));
+		}
+		return builder.build();
 	}
 }
 
