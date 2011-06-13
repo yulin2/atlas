7cad4603e85493aaa8ce899a999239fd4ab62bfc
Fred van den Driessche <fred@metabroadcast.com>
2011-06-13 16:13:36 +0100

    added tracking to ITV updater

--- src/main/java/org/atlasapi/remotesite/itv/ItvUpdater.java
+++ src/main/java/org/atlasapi/remotesite/itv/ItvUpdater.java
@@ -1,5 +1,7 @@
 package org.atlasapi.remotesite.itv;
 
+import static org.atlasapi.persistence.logging.AdapterLogEntry.infoEntry;
+
 import java.util.List;
 
 import org.atlasapi.media.entity.Brand;
@@ -9,7 +11,9 @@ import org.atlasapi.persistence.logging.AdapterLogEntry.Severity;
 import org.atlasapi.persistence.system.RemoteSiteClient;
 import org.atlasapi.remotesite.SiteSpecificAdapter;
 
-public class ItvUpdater implements Runnable {
+import com.metabroadcast.common.scheduling.ScheduledTask;
+
+public class ItvUpdater extends ScheduledTask {
     public static final String ITV_URI = "http://www.itv.com/_data/xml/CatchUpData/CatchUp360/CatchUpMenu.xml";
 
     private final RemoteSiteClient<List<ItvProgramme>> client;
@@ -29,17 +33,30 @@ public class ItvUpdater implements Runnable {
     }
 
     @Override
-    public void run() {
+    public void runTask() {
         try {
+            log.record(infoEntry().withSource(getClass()).withDescription("ITV Updater started"));
+            
+            reportStatus("Fetching brands...");
             List<ItvProgramme> itvBrands = client.get(ITV_URI);
-
+            
+            int total = itvBrands.size();
+            log.record(AdapterLogEntry.debugEntry().withSource(getClass()).withDescription(String.format("Fetched %s ItvProgrammes", total)));
+            
+            int processed = 0;
+            int failed = 0;
             for (ItvProgramme programme : itvBrands) {
                 try {
                     brandAdapter.fetch(ItvMercuryBrandAdapter.BASE_URL + programme.programmeId());
                 } catch (Exception e) {
-                    log.record(new AdapterLogEntry(Severity.WARN).withDescription("Unable to update ITV brand: "+programme.programmeId()).withCause(e));
+                    log.record(new AdapterLogEntry(Severity.WARN).withDescription("Unable to update ITV brand: " + programme.programmeId()).withCause(e));
+                    failed++;
+                } finally {
+                    reportStatus(String.format("Processed %s / %s. %s problems", ++processed, total, failed));
                 }
             }
+            
+            log.record(infoEntry().withSource(getClass()).withDescription(String.format("ITV Updater finished. %s problems.", failed)));
         } catch (Exception e) {
             log.record(new AdapterLogEntry(Severity.WARN).withDescription("Unable to update ITV content").withCause(e));
         }
