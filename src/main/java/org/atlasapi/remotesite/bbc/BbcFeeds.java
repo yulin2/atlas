a385031fc081e08c512d40befa8370b86889c839
John Ayres <john@metabroadcast.com>
2011-06-14 18:54:59 +0100

    defend against bad pids

--- src/main/java/org/atlasapi/remotesite/bbc/BbcFeeds.java
+++ src/main/java/org/atlasapi/remotesite/bbc/BbcFeeds.java
@@ -18,6 +18,9 @@ public class BbcFeeds {
 	}
 
 	public static String slashProgrammesUriForPid(String pid) {
+	    if (!PID_FINDER.matcher(pid).matches()) {
+	        throw new IllegalArgumentException("PID " + pid + " did not match the BBC PID pattern " + PID_FINDER);
+	    }
 		return SLASH_PROGRAMMES_BASE + pid;
 	}
 	
