2ebe9cfd3ff4380526d6ddb072fd62436b2f8f85
John Ayres <john@metabroadcast.com>
2011-06-29 18:00:39 +0100

    fix test

--- src/test/java/org/atlasapi/remotesite/channel4/epg/C4EpgUpdaterTest.java
+++ src/test/java/org/atlasapi/remotesite/channel4/epg/C4EpgUpdaterTest.java
@@ -51,7 +51,7 @@ public class C4EpgUpdaterTest extends TestCase {
     
     private final BroadcastTrimmer trimmer = new BroadcastTrimmer(Publisher.C4, scheduleResolver, resolver,  contentWriter, log);
     
-    private final C4EpgUpdater updater = new C4EpgUpdater(c4AtomFetcher, new C4EpgEntryProcessor(contentWriter, resolver, log), trimmer, log, new DayRangeGenerator());
+    private final C4EpgUpdater updater = new C4EpgUpdater(c4AtomFetcher, new C4EpgEntryProcessor(contentWriter, resolver, log), new C4EpgBrandlessEntryProcessor(contentWriter, resolver, log), trimmer, log, new DayRangeGenerator());
     
     @Override
     public void setUp() throws Exception {
