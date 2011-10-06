14d98bcd5410ce4b400b9c2772eef478c40f3b8a
Fred van den Driessche <fred@metabroadcast.com>
2011-10-06 16:16:41 +0100

    better exception handling in equiv tasks

--- src/main/java/org/atlasapi/equiv/update/tasks/ContentEquivalenceUpdateTask.java
+++ src/main/java/org/atlasapi/equiv/update/tasks/ContentEquivalenceUpdateTask.java
@@ -42,49 +42,55 @@ public class ContentEquivalenceUpdateTask extends ScheduledTask {
     
     @Override
     protected void runTask() {
-        ContentListingProgress currentProgress = progressStore.progressForTask(schedulingKey);
-        log.record(AdapterLogEntry.infoEntry().withSource(getClass()).withDescription("Started: %s from %s", schedulingKey, startProgress(currentProgress)));
-        
-        ContentListingCriteria criteria = defaultCriteria().forPublishers(publishers).forContent(CONTAINER, TOP_LEVEL_ITEM).startingAt(currentProgress).build();
-        Iterator<Content> contents = contentStore.listContent(criteria);
-
-        int processed = 0;
-        boolean shouldContinue = shouldContinue();
-        Content content = null;
-        
-        while (shouldContinue && contents.hasNext()) {
-            content = contents.next();
-            reportStatus(String.format("Processed %d. Processing %s", processed, content.getCanonicalUri()));
-            updateEquivalence(content);
-            if(++processed % 100 == 0) {
-                progressStore.storeProgress(schedulingKey, progressFrom(content));
-            }
-            shouldContinue = shouldContinue();
-        }
-        
-        if(shouldContinue) {
-            progressStore.storeProgress(schedulingKey, ContentListingProgress.START);
-            reportStatus(String.format("Processed %d", processed));
-            log.record(infoEntry().withSource(getClass()).withDescription("Finished: %s", schedulingKey));
-        } else {
-            if(content != null) {
-                progressStore.storeProgress(schedulingKey, progressFrom(content));
-                log.record(infoEntry().withSource(getClass()).withDescription("Stopped: %s at %s", schedulingKey, content.getCanonicalUri()));
-                reportStatus(String.format("Stopped. Processed %d", processed));
-            }
-        }
-    }
-    
-    public void updateEquivalence(Content content) {
         try {
-            rootUpdater.updateEquivalences(content);
+            
+            ContentListingProgress currentProgress = progressStore.progressForTask(schedulingKey);
+            log.record(AdapterLogEntry.infoEntry().withSource(getClass()).withDescription("Started: %s from %s", schedulingKey, startProgress(currentProgress)));
+            
+            ContentListingCriteria criteria = defaultCriteria().forPublishers(publishers).forContent(CONTAINER, TOP_LEVEL_ITEM).startingAt(currentProgress).build();
+            Iterator<Content> contents = contentStore.listContent(criteria);
+    
+            int processed = 0;
+            boolean shouldContinue = shouldContinue();
+            Content content = null;
+            
+            while (shouldContinue && contents.hasNext()) {
+                try {
+                    content = contents.next();
+                    reportStatus(String.format("Processed %d. Processing %s", processed, content.getCanonicalUri()));
+    
+                    rootUpdater.updateEquivalences(content);
+                    
+                    if(++processed % 100 == 0) {
+                        progressStore.storeProgress(schedulingKey, progressFrom(content));
+                    }
+                    shouldContinue = shouldContinue();
+                } catch (Exception e) {
+                    log.record(AdapterLogEntry.errorEntry().withCause(e).withSource(getClass()).withDescription("Exception updating equivalence for "+content.getCanonicalUri()));
+                }
+            }
+            
+            if(shouldContinue) {
+                progressStore.storeProgress(schedulingKey, ContentListingProgress.START);
+                reportStatus(String.format("Processed %d", processed));
+                log.record(infoEntry().withSource(getClass()).withDescription("Finished: %s", schedulingKey));
+            } else {
+                if(content != null) {
+                    progressStore.storeProgress(schedulingKey, progressFrom(content));
+                    log.record(infoEntry().withSource(getClass()).withDescription("Stopped: %s at %s", schedulingKey, content.getCanonicalUri()));
+                    reportStatus(String.format("Stopped. Processed %d", processed));
+                }
+            }
         } catch (Exception e) {
-            log.record(AdapterLogEntry.errorEntry().withCause(e).withSource(getClass()).withDescription("Exception updating equivalence for "+content.getCanonicalUri()));
+            log.record(AdapterLogEntry.errorEntry().withCause(e).withSource(getClass()).withDescription("Exception running task " + schedulingKey));
         }
     }
 
     private String startProgress(ContentListingProgress progress) {
-        return progress == null ? "start" : String.format("%s %s %s", progress.getCategory(), progress.getPublisher(), progress.getUri());
+        if (progress == null || ContentListingProgress.START.equals(progress)) {
+            return "start";
+        }
+        return String.format("%s %s %s", progress.getCategory(), progress.getPublisher(), progress.getUri());
     }
     
     public ContentEquivalenceUpdateTask forPublishers(Publisher... publishers) {
