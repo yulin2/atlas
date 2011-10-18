94f75d195acc171d5f9d2ed04ed9e3f40327b3a1
Fred van den Driessche <fred@metabroadcast.com>
2011-10-18 14:35:22 +0100

    Revert "rate limit redux client"
    This reverts commit 27469275bd42c1c25ed8e606224df1dfae4b8378.

--- src/main/java/org/atlasapi/remotesite/redux/HttpBackedReduxClient.java
+++ src/main/java/org/atlasapi/remotesite/redux/HttpBackedReduxClient.java
@@ -9,7 +9,6 @@ import java.util.List;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.logging.NullAdapterLog;
 import org.atlasapi.remotesite.HttpClients;
-import org.atlasapi.remotesite.channel4.RequestLimitingSimpleHttpClient;
 import org.atlasapi.remotesite.redux.model.BaseReduxProgramme;
 import org.atlasapi.remotesite.redux.model.FullReduxProgramme;
 import org.atlasapi.remotesite.redux.model.PaginatedBaseProgrammes;
@@ -46,7 +45,6 @@ public class HttpBackedReduxClient implements ReduxClient {
         private UsernameAndPassword credentials;
         private String basePath = "/";
         private AdapterLog log = new NullAdapterLog();
-        private int maxRequests = 0;
 
         public Builder(HostSpecifier host) {
             this.host = host;
@@ -67,18 +65,11 @@ public class HttpBackedReduxClient implements ReduxClient {
             return this;
         }
         
-        public Builder withMaxRequestsPerSecond(int maxRequests) {
-            this.maxRequests  = maxRequests;
-            return this;
-        }
-        
         public HttpBackedReduxClient build() {
-            SimpleHttpClient baseClient = new SimpleHttpClientBuilder()
+            return new HttpBackedReduxClient(new SimpleHttpClientBuilder()
                 .withUserAgent(HttpClients.ATLAS_USER_AGENT)
                 .withPreemptiveBasicAuth(credentials)
-                .withAcceptHeader(MimeType.APPLICATION_JSON).build();
-            baseClient = maxRequests > 0 ? new RequestLimitingSimpleHttpClient(baseClient, maxRequests) : baseClient;
-            return new HttpBackedReduxClient(baseClient, "http://" + host.toString() + basePath, log);
+                .withAcceptHeader(MimeType.APPLICATION_JSON).build(), "http://" + host.toString() + basePath, log);
         }
     }
     
