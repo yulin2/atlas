68c33072b91583c0b798b15156b16100d4825608
Fred van den Driessche <fred@metabroadcast.com>
2011-10-18 11:31:41 +0100

    add manual /programmes update controller

--- src/main/java/org/atlasapi/remotesite/bbc/BbcModule.java
+++ src/main/java/org/atlasapi/remotesite/bbc/BbcModule.java
@@ -132,6 +132,10 @@ public class BbcModule {
 		return new BbcSlashProgrammesAtoZUpdater(contentWriters, topicStore, log);
 	}
 	
+	@Bean BbcSlashProgrammesController bbcFeedsController() {
+	    return new BbcSlashProgrammesController(contentWriters, topicStore, log);
+	}
+	
 	@Bean BbcIonOndemandChangeUpdater bbcIonOndemandChangeUpdater() {
 	    return new BbcIonOndemandChangeUpdater(bbcIonOndemandChangeUpdateBuilder(), log);
 	}
