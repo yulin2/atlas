d58f2dbd1882f1aabbddb6a0ec237df2980b5a1e
Fred van den Driessche <fred@metabroadcast.com>
2011-10-18 15:51:20 +0100

    print errors from /programmes controller

--- src/main/java/org/atlasapi/remotesite/bbc/BbcSlashProgrammesController.java
+++ src/main/java/org/atlasapi/remotesite/bbc/BbcSlashProgrammesController.java
@@ -1,11 +1,15 @@
 package org.atlasapi.remotesite.bbc;
 
+import java.io.IOException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.atlasapi.media.entity.Identified;
 import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.topic.TopicStore;
@@ -28,14 +32,24 @@ public class BbcSlashProgrammesController {
     }
 
     @RequestMapping(value = "/system/update/bbc/programmes/{pid}", method = RequestMethod.POST)
-    public void updatePid(HttpServletResponse response, HttpServletRequest request, @PathVariable("pid") final String pid) {
+    public void updatePid(HttpServletResponse response, HttpServletRequest request, @PathVariable("pid") final String pid) throws IOException {
         
-        executor.submit(new Runnable() {
+        Future<Identified> result = executor.submit(new Callable<Identified>() {
             @Override
-            public void run() {
-                fetcher.createOrUpdate(BbcFeeds.slashProgrammesUriForPid(pid));
+            public Identified call() {
+                return fetcher.createOrUpdate(BbcFeeds.slashProgrammesUriForPid(pid));
             }
         });
+        
+        try {
+            Identified ided = result.get();
+            response.setStatus(200);
+            response.setContentLength(ided.getCanonicalUri().length()+1);
+            response.getWriter().println(ided.getCanonicalUri());
+        } catch (Exception e) {
+            response.setStatus(500);
+            e.printStackTrace(response.getWriter());
+        }
         
     }
 
