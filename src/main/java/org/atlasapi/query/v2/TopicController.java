b64b850520f37cc16d9252f75013f5595679e2bd
Tom McAdam <tom@metabroadcast.com>
2012-02-01 09:23:10 +0000

    Incorporate application changes into topics changes

--- src/main/java/org/atlasapi/query/v2/TopicController.java
+++ src/main/java/org/atlasapi/query/v2/TopicController.java
@@ -64,7 +64,7 @@ public class TopicController extends BaseController<Topic> {
         Topic topic = topicForUri.requireValue();
         
         //TODO: train wreck: query.allowsPublisher(publisher)?;
-        if(!query.getConfiguration().getIncludedPublishers().contains(topic.getPublisher())) {
+        if(!query.getConfiguration().includedPublishers().contains(topic.getPublisher())) {
             outputter.writeError(req, resp, FORBIDDEN.withMessage("Topic " + topicUri + " unavailable"));
             return;
         }
@@ -88,7 +88,7 @@ public class TopicController extends BaseController<Topic> {
         Topic topic = topicForUri.requireValue();
         
         //TODO: train wreck: query.allowsPublisher(publisher)?;
-        if(!query.getConfiguration().getIncludedPublishers().contains(topic.getPublisher())) {
+        if(!query.getConfiguration().includedPublishers().contains(topic.getPublisher())) {
             outputter.writeError(req, resp, FORBIDDEN.withMessage("Topic " + topicUri + " unavailable"));
             return;
         }
