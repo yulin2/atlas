fca54b618928de051c26fd446a165c25c5c7d586
Lukewh <luke@lukewh.com>
2012-02-07 15:02:05 +0000

    Update to lists. Added filter

--- src/main/webapp/static/js/applications.js
+++ src/main/webapp/static/js/applications.js
@@ -76,20 +76,16 @@ $("a.approve-link").live('click', function(){
 
 addApplication = function(slug, data) {
 	var app = null;
-	$.each(data.applications, function(i,appl) {
-		if (appl.slug == slug) {
-			app = appl;
+	for(var i = 0, ii = data.applications.length; i<ii; i++){
+		if(data.applications[i].slug === slug){
+			app = data.applications[i];
+			break;
 		}
-	});
-	var slug = app.slug;
-	var title= app.title;
-	var appList = $('#applications-list')
-	if (appList.attr('data-apps') == 0) {
-		appList.children().slideUp().remove();
 	}
-	appList.append('<li style="display:none"><a class="app-link" href="/admin/applications/'+slug+'">'+title+'</a></li>');
-	appList.children().last().slideDown();
-	appList.attr('data-apps', appList.children().length);
+	if(app !== null){
+		apps.push(app);
+		$('.app-link:last').after(atlas.templates.applications.widgets.applicationLink({app: app}));
+	}
 	$('.overlayBlocker').hide();
 }
 
