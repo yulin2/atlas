97ae3e5ee354743a0719b2795a89c23940a92814
Fred van den Driessche <fred@metabroadcast.com>
2012-02-21 14:04:37 +0000

    extract Cannon Twitter Topics client

--- src/main/java/org/atlasapi/remotesite/voila/CannonTwitterTopicsUpdater.java
+++ src/main/java/org/atlasapi/remotesite/voila/CannonTwitterTopicsUpdater.java
@@ -1,58 +1,49 @@
 package org.atlasapi.remotesite.voila;
 
-import static org.atlasapi.persistence.logging.AdapterLogEntry.errorEntry;
-
 import java.util.List;
 
-import org.atlasapi.persistence.logging.AdapterLog;
-import org.atlasapi.persistence.system.RemoteSiteClient;
+import org.atlasapi.remotesite.redux.UpdateProgress;
 
 import com.google.common.base.Optional;
-import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
-import com.google.common.net.HostSpecifier;
 import com.metabroadcast.common.scheduling.ScheduledTask;
 
 public class CannonTwitterTopicsUpdater extends ScheduledTask {
 
-    private final RemoteSiteClient<ContentWordsIdList> idListClient;
-    private final String idListUri;
-    private final AdapterLog log;
     private final ContentTwitterTopicsUpdater idUpdater;
+    private final CannonTwitterTopicsClient cannonTopicsClient;
 
-    public CannonTwitterTopicsUpdater(RemoteSiteClient<ContentWordsIdList> client, HostSpecifier cannonHost, Optional<Integer> cannonPort, ContentTwitterTopicsUpdater idUpdater, AdapterLog log) {
-        this.idListUri = String.format("http://%s%s/contentWordsList", cannonHost, cannonPort.isPresent() ? ":"+cannonPort.get():"");
-        this.idListClient = client;
-        this.idUpdater = idUpdater;
-        this.log = log;
+    public CannonTwitterTopicsUpdater(CannonTwitterTopicsClient cannonTopicsClient, ContentTwitterTopicsUpdater contentTopicsUpdater) {
+        this.cannonTopicsClient = cannonTopicsClient;
+        this.idUpdater = contentTopicsUpdater;
     }
-    
+
     @Override
     protected void runTask() {
-        reportStatus(String.format("Retrieving ID list from %s", idListUri));
-        
-        ContentWordsIdList contentWordsList = null;
-        try {
-            contentWordsList = idListClient.get(idListUri);
-        } catch (Exception e) {
-            log.record(errorEntry().withCause(e).withSource(getClass()).withDescription("Failed to get contentWordsList"));
-            throw Throwables.propagate(e);
+        reportStatus("Retrieving ID list");
+
+        Optional<ContentWordsIdList> idList = cannonTopicsClient.getIdList();
+
+        if (!idList.isPresent()) {
+            reportStatus("Couldn't get ID list");
+            throw new RuntimeException("Couldn't get ID list");
         }
-        
-        int done = 0;
-        for (List<String> idPart : Lists.partition(contentWordsList.contentIds, 10)) {
-            reportStatus(String.format("Fetched %s of %s IDs", done, contentWordsList.contentIds.size()));
-            idUpdater.updateTopics(idPart);
-            done += idPart.size();
+
+        UpdateProgress updateProgress = UpdateProgress.START;
+
+        ContentWordsIdList contentWordsIdList = idList.get();
+        for (List<String> idPart : Lists.partition(contentWordsIdList.contentIds, 10)) {
+            reportStatus(String.format("%s. %s total", updateProgress, contentWordsIdList.contentIds.size()));
+            updateProgress = updateProgress.reduce(idUpdater.updateTopics(idPart));
         }
-        
-        reportStatus(String.format("Fetched %s IDs", done));
+
+        reportStatus(String.format("%s. %s total", updateProgress, contentWordsIdList.contentIds.size()));
     }
 
     public static class ContentWordsIdList {
-        
+
         List<String> contentIds;
-        
+
     }
-    
+
 }
--- src/main/java/org/atlasapi/remotesite/voila/ContentTwitterTopicsUpdater.java
+++ src/main/java/org/atlasapi/remotesite/voila/ContentTwitterTopicsUpdater.java
@@ -1,6 +1,8 @@
 package org.atlasapi.remotesite.voila;
 
 import static org.atlasapi.persistence.logging.AdapterLogEntry.warnEntry;
+import static org.atlasapi.remotesite.redux.UpdateProgress.FAILURE;
+import static org.atlasapi.remotesite.redux.UpdateProgress.SUCCESS;
 
 import java.util.List;
 
@@ -16,81 +18,97 @@ import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.logging.AdapterLogEntry;
-import org.atlasapi.persistence.system.RemoteSiteClient;
 import org.atlasapi.persistence.topic.TopicStore;
+import org.atlasapi.remotesite.redux.UpdateProgress;
 import org.atlasapi.remotesite.voila.ContentWords.ContentWordsList;
 
 import com.google.common.base.Function;
-import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSet.Builder;
 import com.google.common.collect.Iterables;
-import com.google.common.net.HostSpecifier;
 import com.metabroadcast.common.base.Maybe;
 
 public class ContentTwitterTopicsUpdater {
 
-    private final RemoteSiteClient<ContentWordsList> client;
-    private final String contentWordsRequestBase;
-    private final AdapterLog log;
-
-    private final Joiner joiner = Joiner.on(',');
     private final ContentResolver contentResolver;
     private final TopicStore topicStore;
     private final ContentWriter contentWriter;
+    private final CannonTwitterTopicsClient cannonTopicsClient;
+    private final AdapterLog log;
 
-    public ContentTwitterTopicsUpdater(RemoteSiteClient<ContentWordsList> client, HostSpecifier cannonHost, Optional<Integer> cannonPort, ContentResolver contentResolver, TopicStore topicStore,
-            ContentWriter contentWriter, AdapterLog log) {
-        this.client = client;
+    public ContentTwitterTopicsUpdater(CannonTwitterTopicsClient cannonTopicsClient, ContentResolver contentResolver, TopicStore topicStore, ContentWriter contentWriter, AdapterLog log) {
+        this.cannonTopicsClient = cannonTopicsClient;
         this.contentResolver = contentResolver;
         this.topicStore = topicStore;
         this.contentWriter = contentWriter;
-        this.contentWordsRequestBase = String.format("http://%s%s/contentWords?ids=", cannonHost, cannonPort.isPresent() ? ":"+cannonPort.get() : "");
         this.log = log;
     }
 
-    public void updateTopics(List<String> contentIds) {
+    public UpdateProgress updateTopics(List<String> contentIds) {
         
-        ContentWordsList contentWords = null;
-        try {
-            contentWords = client.get(joiner.appendTo(new StringBuilder(contentWordsRequestBase), contentIds).toString());
-        } catch (Exception e) {
-            log.record(AdapterLogEntry.errorEntry().withCause(e).withSource(getClass()).withDescription("Failed to get words for %s",joiner.join(contentIds)));
+        Optional<ContentWordsList> possibleContentWords = cannonTopicsClient.getContentWordsForIds(contentIds);
+        
+        if (!possibleContentWords.isPresent()) {
+            return new UpdateProgress(0, contentIds.size());
         }
 
-        ResolvedContent resolvedContent = contentResolver.findByCanonicalUris(Iterables.transform(contentWords.getResults(), new Function<ContentWords, String>() {
+        ContentWordsList contentWords = possibleContentWords.get();
+        
+        ResolvedContent resolvedContent = contentResolver.findByCanonicalUris(urisForWords(contentWords));
+        
+        UpdateProgress result = UpdateProgress.START;
+        for (ContentWords contentWordSet : contentWords) {
+            try {
+                Maybe<Identified> possibleContent = resolvedContent.get(contentWordSet.getUri());
+                if(possibleContent.hasValue()) {
+                    Content content = (Content) possibleContent.requireValue();
+                    content.setTopicRefs(getTopicRefsFor(contentWordSet).addAll(content.getTopicRefs()).build());
+                    write(content);
+                    result = result.reduce(SUCCESS);
+                } else {
+                    log.record(warnEntry().withSource(getClass()).withDescription("Couldn't resolve content for %s", contentWordSet.getUri()));
+                    result = result.reduce(FAILURE);
+                }
+            } catch (Exception e) {
+                log.record(AdapterLogEntry.errorEntry().withCause(e).withSource(getClass()).withDescription("Failed to update topics for %s", contentWordSet.getUri()));
+                result = result.reduce(FAILURE);
+            }
+        }
+        
+        return result;
+    }
+
+    public void write(Content content) {
+        if (content instanceof Container) {
+            contentWriter.createOrUpdate((Container) content);
+        } else {
+            contentWriter.createOrUpdate((Item) content);
+        }
+    }
+
+    public Builder<TopicRef> getTopicRefsFor(ContentWords contentWordSet) {
+        Builder<TopicRef> topicRefs = ImmutableSet.builder();
+        for (String word : ImmutableSet.copyOf(contentWordSet.getWords())) {
+            Maybe<Topic> possibleTopic = topicStore.topicFor("twitter", word);
+            if (possibleTopic.hasValue()) {
+                Topic topic = possibleTopic.requireValue();
+                topic.setPublisher(Publisher.METABROADCAST);
+                topic.setType(Topic.Type.SUBJECT);
+                topicStore.write(topic);
+                topicRefs.add(new TopicRef(topic, 1.0f, false));
+            }
+        }
+        return topicRefs;
+    }
+
+    public Iterable<String> urisForWords(ContentWordsList contentWords) {
+        return ImmutableSet.copyOf(Iterables.transform(contentWords.getResults(), new Function<ContentWords, String>() {
             @Override
             public String apply(ContentWords input) {
                 return input.getUri();
             }
         }));
-        
-        for (ContentWords contentWordSet : contentWords.getResults()) {
-            Maybe<Identified> possibleContent = resolvedContent.get(contentWordSet.getUri());
-            if(possibleContent.hasValue()) {
-                ImmutableSet.Builder<TopicRef> topicRefs = ImmutableSet.builder();
-                for (String word : ImmutableSet.copyOf(contentWordSet.getWords())) {
-                    Maybe<Topic> possibleTopic = topicStore.topicFor("twitter", word);
-                    if (possibleTopic.hasValue()) {
-                        Topic topic = possibleTopic.requireValue();
-                        topic.setPublisher(Publisher.METABROADCAST);
-                        topic.setType(Topic.Type.SUBJECT);
-                        topicStore.write(topic);
-                        topicRefs.add(new TopicRef(topic, 1.0f, false));
-                    }
-                }
-                Content content = (Content) possibleContent.requireValue();
-                content.setTopicRefs(topicRefs.addAll(content.getTopicRefs()).build());
-                if (content instanceof Container) {
-                    contentWriter.createOrUpdate((Container)content);
-                } else {
-                    contentWriter.createOrUpdate((Item)content);
-                }
-            } else {
-                log.record(warnEntry().withSource(getClass()).withDescription("Couldn't resolve content for %s", contentWordSet.getUri()));
-            }
-        }
-        
     }
 
 }
