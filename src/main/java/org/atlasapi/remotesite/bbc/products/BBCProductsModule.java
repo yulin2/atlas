94906ef30ecee61d54825b13962285bb76cf5291
Fred van den Driessche <fred@metabroadcast.com>
2012-03-13 18:27:55 +0000

    enable BBC products updater on prod

--- src/main/java/org/atlasapi/remotesite/bbc/products/BBCProductsModule.java
+++ src/main/java/org/atlasapi/remotesite/bbc/products/BBCProductsModule.java
@@ -18,12 +18,9 @@ import org.atlasapi.media.product.ProductStore;
 @Configuration
 public class BBCProductsModule {
 
-    private @Autowired
-    SimpleScheduler scheduler;
-    private @Autowired
-    ProductStore productStore;
-    private @Autowired
-    AdapterLog log;
+    private @Autowired SimpleScheduler scheduler;
+    private @Autowired ProductStore productStore;
+    private @Autowired AdapterLog log;
 
     @PostConstruct
     public void startBackgroundTasks() {
@@ -31,7 +28,7 @@ public class BBCProductsModule {
         log.record(new AdapterLogEntry(Severity.INFO).withSource(getClass()).withDescription("Installed BBC Products updater"));
     }
 
-    @Bean
+    @Bean(name="bbcproductsupdater")
     public BBCProductsUpdater updater() {
         return new BBCProductsUpdater(productStore, log, Configurer.get("s3.access").get(), Configurer.get("s3.secret").get());
     }
