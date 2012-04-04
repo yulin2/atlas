e4716c2fe2d42651587380cd05daf91daef323ff
Fred van den Driessche <fred@metabroadcast.com>
2012-04-04 15:59:47 +0100

    schedule probe runs over 7 days

--- src/main/java/org/atlasapi/remotesite/health/ScheduleProbe.java
+++ src/main/java/org/atlasapi/remotesite/health/ScheduleProbe.java
@@ -14,6 +14,7 @@ import org.atlasapi.media.entity.ScheduleEntry;
 import org.atlasapi.persistence.content.ScheduleResolver;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.joda.time.LocalDate;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
@@ -21,22 +22,28 @@ import com.metabroadcast.common.health.HealthProbe;
 import com.metabroadcast.common.health.ProbeResult;
 import com.metabroadcast.common.health.ProbeResult.ProbeResultEntry;
 import com.metabroadcast.common.time.Clock;
+import com.metabroadcast.common.time.DateTimeZones;
 
 public class ScheduleProbe implements HealthProbe {
 
     private static final Duration MAX_GAP = Duration.standardMinutes(5);
-    private static final Duration MAX_STALENESS = Duration.standardHours(1);
     
     private final Publisher publisher;
     private final Channel channel;
     private final ScheduleResolver scheduleResolver;
     private final Clock clock;
-
+    private final int days;
+    
     public ScheduleProbe(Publisher publisher, Channel channel, ScheduleResolver scheduleResolver, Clock clock) {
+        this(publisher, channel, scheduleResolver, clock, 7);
+    }
+
+    public ScheduleProbe(Publisher publisher, Channel channel, ScheduleResolver scheduleResolver, Clock clock, int days) {
         this.publisher = publisher;
         this.channel = channel;
         this.scheduleResolver = scheduleResolver;
         this.clock = clock;
+        this.days = days;
     }
 
     @Override
@@ -47,27 +54,34 @@ public class ScheduleProbe implements HealthProbe {
             return result;
         }
         
-        DateTime date = clock.now().withTime(0, 0, 0, 0);
-
-        Schedule schedule = scheduleResolver.schedule(date.minusMillis(1), date.plusDays(1), ImmutableSet.of(channel), ImmutableSet.of(publisher));
-        List<Item> items = Iterables.getOnlyElement(schedule.scheduleChannels()).items();
-        result.addEntry(scheduleSize(items.size()));
-
-        if(items.isEmpty()) {
-            return result;
+        LocalDate date = clock.now().toDateTime(DateTimeZones.LONDON).toLocalDate();
+        
+        for (int i = 0; i < days; i++, date = date.plusDays(1)) {
+            DateTime start = date.toDateTimeAtStartOfDay();
+            Schedule schedule = scheduleResolver.schedule(start, start.plusDays(1), ImmutableSet.of(channel), ImmutableSet.of(publisher));
+            result.addEntry(scheduleSize(schedule));
+            addContiguityEntries(schedule, result);
         }
-
-        addContiguityEntries(items, result);
-        addLastFetchedCheckEntry(items, result);
 
         return result;
     }
 
-    private ProbeResultEntry scheduleSize(int scheduleEntries) {
-        return new ProbeResultEntry(scheduleEntries > 0 ? INFO : FAILURE, "Schedule Entries", String.valueOf(scheduleEntries));
+    private ProbeResultEntry scheduleSize(Schedule schedule) {
+        int scheduleSize = Iterables.getOnlyElement(schedule.scheduleChannels()).items().size();
+        return new ProbeResultEntry(scheduleSize > 0 ? INFO : FAILURE, startString(schedule) + " entries", String.valueOf(scheduleSize));
     }
 
-    private void addContiguityEntries(List<Item> items, ProbeResult result) {
+    private String startString(Schedule schedule) {
+        return schedule.interval().getStart().toString("dd/MM/yyyy");
+    }
+
+    private void addContiguityEntries(Schedule schedule, ProbeResult result) {
+        List<Item> items = Iterables.getOnlyElement(schedule.scheduleChannels()).items();
+        
+        if(items.isEmpty()) {
+            return;
+        }
+
         int breaks = 0, overlaps = 0;
         DateTime lastEnd = ScheduleEntry.BROADCAST.apply(items.get(0)).getTransmissionTime();
 
@@ -84,20 +98,9 @@ public class ScheduleProbe implements HealthProbe {
             lastEnd = broadcast.getTransmissionEndTime();
         }
 
-        result.add("Schedule Breaks", String.valueOf(breaks), !(breaks > 0));
-        result.add("Schedule Overlaps", String.valueOf(overlaps), !(overlaps > 0));
-    }
-
-    private void addLastFetchedCheckEntry(List<Item> items, ProbeResult result) {
-        DateTime oldestFetch = clock.now();
-        
-        for (Item item : items) {
-            if(item.getLastFetched().isBefore(oldestFetch)) {
-                oldestFetch = item.getLastFetched();
-            }
-        }
-        
-        result.add("Oldest Fetch", oldestFetch.toString("HH:mm:ss dd/MM/yyyy"), oldestFetch.isAfter(clock.now().minus(MAX_STALENESS)));
+        String date = startString(schedule);
+        result.add(date + " gaps", String.valueOf(breaks), !(breaks > 0));
+        result.add(date + " overlaps", String.valueOf(overlaps), !(overlaps > 0));
     }
 
     @Override
@@ -107,6 +110,6 @@ public class ScheduleProbe implements HealthProbe {
 
     @Override
     public String slug() {
-        return String.format("%s%dschedule", publisher.name(), channel == null ? 0 : channel.getId());
+        return String.format("%s%sschedule", publisher.name(), channel == null ? 0 : channel.key());
     }
 }
--- src/test/java/org/atlasapi/remotesite/health/ScheduleProbeTest.java
+++ src/test/java/org/atlasapi/remotesite/health/ScheduleProbeTest.java
@@ -34,20 +34,21 @@ import com.metabroadcast.common.time.TimeMachine;
 
 public class ScheduleProbeTest extends TestCase {
 
-	private static final Channel CHANNEL4 = new Channel(Publisher.METABROADCAST, "Channel 4", "c4", MediaType.AUDIO, "http://channel4.com");
-
-    private final TimeMachine clock = new TimeMachine();
     private final Mockery context = new Mockery();
     private final ScheduleResolver scheduleResolver = context.mock(ScheduleResolver.class);
+
+    private final TimeMachine clock = new TimeMachine();
+    private static final Channel CHANNEL4 = new Channel(Publisher.METABROADCAST, "Channel 4", "c4", MediaType.AUDIO, "http://channel4.com");
     private final Set<Channel> channels = ImmutableSet.of(CHANNEL4);
     private final Set<Publisher> publishers = ImmutableSet.of(Publisher.C4);
+
+    private final ScheduleProbe probe = new ScheduleProbe(Publisher.C4, CHANNEL4, scheduleResolver, clock, 1);
 
     @Test
     public void testProbeProducesFailureResultWhenNoEntriesInSchedule() throws Exception {
 
         clock.jumpTo(new DateTime(500, DateTimeZones.UTC));
         
-        ScheduleProbe probe = new ScheduleProbe(Publisher.C4, CHANNEL4, scheduleResolver, clock);
         
         context.checking(new Expectations(){{
             one(scheduleResolver).schedule(with(any(DateTime.class)), with(any(DateTime.class)), with(channels), with(publishers));
@@ -64,8 +65,6 @@ public class ScheduleProbeTest extends TestCase {
     public void testScheduleWithNoGapsPasses() throws Exception {
         
         clock.jumpTo(new DateTime(500, DateTimeZones.UTC));
-        
-        ScheduleProbe probe = new ScheduleProbe(Publisher.C4, CHANNEL4, scheduleResolver, clock);
         
                                     //start, duration in ms
         final Item item1 = broadcastItem(10, 10);
@@ -86,8 +85,6 @@ public class ScheduleProbeTest extends TestCase {
         
         clock.jumpTo(new DateTime(500, DateTimeZones.UTC));
         
-        ScheduleProbe probe = new ScheduleProbe(Publisher.C4, CHANNEL4, scheduleResolver, clock);
-        
         final Item item1 = broadcastItem(10, 10);
         final Item item2 = broadcastItem(300022, 10); //starts 5mins and 2 millis after
         
@@ -102,15 +99,12 @@ public class ScheduleProbeTest extends TestCase {
         assertThat(Iterables.get(result.entries(),0).isFailure(), is(false));
         assertThat(Iterables.get(result.entries(),1).isFailure(), is(true));
         assertThat(Iterables.get(result.entries(),2).isFailure(), is(false)); 
-        assertThat(Iterables.get(result.entries(),3).isFailure(), is(false)); 
     }
 
     @Test
     public void testScheduleWithOverlapsFails() throws Exception {
         
         clock.jumpTo(new DateTime(500, DateTimeZones.UTC));
-        
-        ScheduleProbe probe = new ScheduleProbe(Publisher.C4, CHANNEL4, scheduleResolver, clock);
         
         final Item item1 = broadcastItem(10, 10);
         final Item item2 = broadcastItem(18, 10); //previous ends at 20
@@ -126,31 +120,6 @@ public class ScheduleProbeTest extends TestCase {
         assertThat(Iterables.get(result.entries(),0).isFailure(), is(false));
         assertThat(Iterables.get(result.entries(),1).isFailure(), is(false));
         assertThat(Iterables.get(result.entries(),2).isFailure(), is(true)); 
-        assertThat(Iterables.get(result.entries(),3).isFailure(), is(false)); 
-    }
-
-    @Test
-    public void testScheduleWithStaleItemsFails() throws Exception {
-        
-        clock.jumpTo(new DateTime(3600020, DateTimeZones.UTC));
-        
-        ScheduleProbe probe = new ScheduleProbe(Publisher.C4, CHANNEL4, scheduleResolver, clock);
-        
-        final Item item1 = broadcastItem(10, 10, 10);
-        final Item item2 = broadcastItem(22, 10, 10);
-        
-        context.checking(new Expectations(){{
-            one(scheduleResolver).schedule(with(any(DateTime.class)), with(any(DateTime.class)), with(channels), with(publishers));
-            will(returnValue(schedule(CHANNEL4, ImmutableList.<Item>of(item1, item2), dayIntervalAround(clock.now()))));
-        }});
-        
-        ProbeResult result = probe.probe();
-        
-        assertThat(result.isFailure(), is(true));
-        assertThat(Iterables.get(result.entries(),0).isFailure(), is(false));
-        assertThat(Iterables.get(result.entries(),1).isFailure(), is(false));
-        assertThat(Iterables.get(result.entries(),2).isFailure(), is(false));
-        assertThat(Iterables.get(result.entries(),3).isFailure(), is(true)); 
     }
     
     private Item broadcastItem(long start, long duration) {
