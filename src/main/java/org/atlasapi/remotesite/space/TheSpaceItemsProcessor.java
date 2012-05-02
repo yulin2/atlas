4bd9ab8267e5c4df2dd0c607058f3031bb560231
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-02 17:25:03 +0100

    Improved The Space ingestion to support both stage and production environments.

--- src/main/java/org/atlasapi/remotesite/space/TheSpaceItemsProcessor.java
+++ src/main/java/org/atlasapi/remotesite/space/TheSpaceItemsProcessor.java
@@ -14,12 +14,14 @@ import org.codehaus.jackson.map.ObjectMapper;
  */
 public class TheSpaceItemsProcessor {
 
+    private final String url;
     private final SimpleHttpClient client;
     private final AdapterLog log;
     private final ContentResolver contentResolver;
     private final ContentWriter contentWriter;
 
-    public TheSpaceItemsProcessor(SimpleHttpClient client, AdapterLog log, ContentResolver contentResolver, ContentWriter contentWriter) {
+    public TheSpaceItemsProcessor(String url, SimpleHttpClient client, AdapterLog log, ContentResolver contentResolver, ContentWriter contentWriter) {
+        this.url = url;
         this.client = client;
         this.log = log;
         this.contentResolver = contentResolver;
@@ -29,14 +31,14 @@ public class TheSpaceItemsProcessor {
     public void process(JsonNode items) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         //
-        TheSpaceItemProcessor processor = new TheSpaceItemProcessor(client, log, contentResolver, contentWriter);
+        TheSpaceItemProcessor processor = new TheSpaceItemProcessor(url, client, log, contentResolver, contentWriter);
         //
         Iterator<JsonNode> results = items.get("results").getElements();
         while (results.hasNext()) {
             JsonNode item = results.next();
             String pid = item.get("pid").asText();
             try {
-                JsonNode node = client.get(new SimpleHttpRequest<JsonNode>(TheSpaceUpdater.BASE_API_URL + "/items/" + pid + ".json", new JSonNodeHttpResponseTransformer(mapper)));
+                JsonNode node = client.get(new SimpleHttpRequest<JsonNode>(url + "/items/" + pid + ".json", new JSonNodeHttpResponseTransformer(mapper)));
                 processor.process(node.get("programme"));
             } catch (Exception ex) {
                 log.record(new AdapterLogEntry(AdapterLogEntry.Severity.WARN).withCause(ex));
