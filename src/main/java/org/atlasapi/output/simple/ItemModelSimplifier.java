9f411fdc08982474788074cbf1009be96650c801
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-14 18:23:38 +0100

    Fixed item simplifier to copy song duration too.

--- src/main/java/org/atlasapi/output/simple/ItemModelSimplifier.java
+++ src/main/java/org/atlasapi/output/simple/ItemModelSimplifier.java
@@ -142,6 +142,7 @@ public class ItemModelSimplifier extends ContentModelSimplifier<Item, org.atlasa
         } else if (fullItem instanceof Song) {
             Song song = (Song) fullItem;
             simpleItem.setIsrc(song.getIsrc());
+            simpleItem.setDuration(song.getDuration().getStandardSeconds());
         }
     }
 
