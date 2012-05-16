5537349cb26ebf87d2ed00e74ea5764c9fcb632a
Jonathan Tweed <jonathan@metabroadcast.com>
2012-05-16 15:16:02 +0100

    Changed the permitted full fetch periods for tv and radio.

--- src/main/java/org/atlasapi/remotesite/bbc/ion/ScheduleBasedItemUpdatingBroadcastHandler.java
+++ src/main/java/org/atlasapi/remotesite/bbc/ion/ScheduleBasedItemUpdatingBroadcastHandler.java
@@ -5,6 +5,7 @@ import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.remotesite.bbc.ion.model.IonBroadcast;
 import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
 
 import com.metabroadcast.common.time.DateTimeZones;
 
@@ -16,9 +17,17 @@ public class ScheduleBasedItemUpdatingBroadcastHandler extends DefaultBbcIonBroa
     
     @Override
     protected boolean fullFetchPermitted(IonBroadcast broadcast, String itemUri) {
-        DateTime now = new DateTime(DateTimeZones.UTC);
-        return now.plusDays(2).isAfter(broadcast.getStart()) 
-            && now.minusDays(2).isBefore(broadcast.getStart());
+        LocalTime today = new DateTime(DateTimeZones.UTC).toLocalDate();
+        LocalTime broadcastDay = broadcast.getStart().toLocalDate();
+        
+        String mediaType = broadcast.getMediaType();
+        
+        // today -4 for radio, today ± 2 for tv
+        if (mediaType.equals("audio")) {
+            return today.plusDays(1).isAfter(broadcastDay) && today.minusDays(5).isBefore(broadcastDay);    
+        } else {
+            return today.plusDays(3).isAfter(broadcastDay) && today.minusDays(3).isBefore(broadcastDay);
+        }
     }
     
 }
