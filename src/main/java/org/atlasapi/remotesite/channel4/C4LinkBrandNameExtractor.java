43b34afb504d72120c7eaf35af046e00b596623c
Fred van den Driessche <fred@metabroadcast.com>
2012-05-17 19:03:00 +0100

    C4 PC/Xbox adapter work-in-progress

---
+++ src/main/java/org/atlasapi/remotesite/channel4/C4LinkBrandNameExtractor.java
@@ -0,0 +1,31 @@
+package org.atlasapi.remotesite.channel4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Optional;
+
+class C4LinkBrandNameExtractor {
+
+    private final Pattern channel4Link = Pattern.compile(
+        "https?://.+\\.channel4\\.com/[^/]+/([^./]+).*"
+    );
+    
+    private static final String PROGRAMMES_BASE = "http://www.channel4.com/programmes/";
+    
+    public Optional<String> brandNameFrom(String url) {
+       Matcher matcher = channel4Link.matcher(url);
+       if (matcher.matches()) {
+           return Optional.of(matcher.group(1));
+       }
+       return Optional.absent();
+    }
+    
+    public Optional<String> canonicalBrandUriFrom(String url) {
+        Optional<String> brandName = brandNameFrom(url);
+        if (brandName.isPresent()) {
+            return Optional.of(PROGRAMMES_BASE + brandName.get());
+        }
+        return Optional.absent();
+    }
+}
