21eb7a786ff44058fd2dd2547112102a60c45365
Chris Keeley <keeley@metabroadcast.com>
2012-05-23 11:12:29 +0100

    Moved RequestLimitingSimpleHttpClient to common

--- src/main/java/org/atlasapi/remotesite/channel4/C4Module.java
+++ src/main/java/org/atlasapi/remotesite/channel4/C4Module.java
@@ -32,6 +32,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.metabroadcast.common.http.RequestLimitingSimpleHttpClient;
 import com.metabroadcast.common.http.SimpleHttpClient;
 import com.metabroadcast.common.http.SimpleHttpClientBuilder;
 import com.metabroadcast.common.media.MimeType;
