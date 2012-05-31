f01907c8f28d8063a6a4a9deb8cb3563d5fb2082
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-31 16:23:55 +0100

    Fixed restriction message in EmiPubProcessor.

--- src/test/java/org/atlasapi/remotesite/music/emipub/EmiPubProcessorTest.java
+++ src/test/java/org/atlasapi/remotesite/music/emipub/EmiPubProcessorTest.java
@@ -30,14 +30,14 @@ public class EmiPubProcessorTest {
                 capture.getAllValues().get(0).getCanonicalUri());
         assertEquals(1,
                 capture.getAllValues().get(0).getPeople().size());
-        assertEquals("Synchronisation Right:100.0~Performing Right:50.0~Material Change/Adaptation:100.0~Mechanical Right:100.0~Other:100.0",
+        assertEquals("Synchronisation Right:100.0,Performing Right:50.0,Material Change/Adaptation:100.0,Mechanical Right:100.0,Other:100.0",
                 capture.getAllValues().get(0).getVersions().iterator().next().getRestriction().getMessage());
         
         assertEquals("http://emimusicpub.com/works/1380618",
                 capture.getAllValues().get(1).getCanonicalUri());
         assertEquals(2,
                 capture.getAllValues().get(1).getPeople().size());
-        assertEquals("Synchronisation Right:100.0~Performing Right:50.0~Material Change/Adaptation:100.0~Mechanical Right:100.0~Other:100.0",
+        assertEquals("Synchronisation Right:100.0,Performing Right:50.0,Material Change/Adaptation:100.0,Mechanical Right:100.0,Other:100.0",
                 capture.getAllValues().get(1).getVersions().iterator().next().getRestriction().getMessage());
     }
 }
