25eda2bec8093be344db06da1cdd86f6d3b54043
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-21 19:30:59 +0100

    Implemented first version of ElasticSearch-based Item indexing as of MBST-2218.

--- src/main/java/org/atlasapi/messaging/workers/ESIndexer.java
+++ src/main/java/org/atlasapi/messaging/workers/ESIndexer.java
@@ -1,44 +1,38 @@
 package org.atlasapi.messaging.workers;
 
 import java.util.Arrays;
-import org.atlasapi.media.entity.Container;
 import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Item;
-import org.atlasapi.persistence.content.ContentResolver;
-import org.atlasapi.persistence.content.ContentWriter;
-import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.messaging.event.EntityUpdatedEvent;
+import org.atlasapi.persistence.content.ContentIndexer;
+import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.ResolvedContent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- */
-public class CassandraReplicator extends AbstractWorker {
+public class ESIndexer extends AbstractWorker {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     //
-    private final ContentResolver mongoContentResolver;
-    private final ContentWriter cassandraContentWriter;
+    private final ContentResolver contentResolver;
+    private final ContentIndexer contentIndexer;
 
-    public CassandraReplicator(ContentResolver mongoContentResolver, ContentWriter cassandraContentWriter) {
-        this.mongoContentResolver = mongoContentResolver;
-        this.cassandraContentWriter = cassandraContentWriter;
+    public ESIndexer(ContentResolver contentResolver, ContentIndexer contentIndexer) {
+        this.contentResolver = contentResolver;
+        this.contentIndexer = contentIndexer;
     }
 
     @Override
     public void process(EntityUpdatedEvent event) {
         // TODO_SB : ResolvedContent should be eliminated or made way simpler:
-        ResolvedContent results = mongoContentResolver.findByCanonicalUris(Arrays.asList(event.getEntityId()));
+        ResolvedContent results = contentResolver.findByCanonicalUris(Arrays.asList(event.getEntityId()));
         //
         if (results.getAllResolvedResults().size() > 1) {
             throw new IllegalStateException("More than one content found for id: " + event.getEntityId());
         } else if (results.getAllResolvedResults().size() == 1) {
             Identified source = results.getFirstValue().requireValue();
-            if (source instanceof Container) {
-                cassandraContentWriter.createOrUpdate((Container) source);
-            } else if (source instanceof Item) {
-                cassandraContentWriter.createOrUpdate((Item) source);
+            if (source instanceof Item) {
+                contentIndexer.index((Item) source);
             } else {
                 log.warn("Unexpected type {} found for id {} on event of type {} and id {}.",
                         new Object[]{source.getClass().getName(), event.getEntityId(), event.getClass().getName(), event.getChangeId()});
