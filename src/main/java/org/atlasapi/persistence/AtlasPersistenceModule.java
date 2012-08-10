e7f5501ad4828c7bfb22d2653e9dffcbcc183e79
Tom McAdam <tom@metabroadcast.com>
2012-08-10 17:59:49 +0100

    Add missing import

--- src/main/java/org/atlasapi/persistence/AtlasPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/AtlasPersistenceModule.java
@@ -50,6 +50,7 @@ import org.atlasapi.persistence.media.channel.MongoChannelStore;
 import org.atlasapi.persistence.media.segment.MongoSegmentResolver;
 import org.atlasapi.persistence.shorturls.MongoShortUrlSaver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.atlasapi.persistence.topic.TopicStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
