30ae87c362c5a1a72bdcb9279ae9f44a8355e4c1
Tom McAdam <tom@metabroadcast.com>
2012-08-14 17:24:58 +0100

    Ordering of Publisher should be complete, so add any Publisher not specified in precedence

--- src/test/java/org/atlasapi/equiv/query/MergeOnOutputQueryExecutorTest.java
+++ src/test/java/org/atlasapi/equiv/query/MergeOnOutputQueryExecutorTest.java
@@ -84,6 +84,17 @@ public class MergeOnOutputQueryExecutorTest extends TestCase {
 		assertEquals(ImmutableList.of(clip1), ((Episode)Iterables.getOnlyElement(merged.get(item1.getCanonicalUri()))).getClips());
 	}
 	
+	public void testMergingWithExplicitPrecidenceMissingNewPublisher() throws Exception {
+	    MergeOnOutputQueryExecutor merger = new MergeOnOutputQueryExecutor(delegate(item1, item2));
+
+	    // Let's not specify a precedence for YouTube, but content will be returned for YouTube
+        ContentQuery query = ContentQuery.MATCHES_EVERYTHING.copyWithApplicationConfiguration(ApplicationConfiguration.DEFAULT_CONFIGURATION.copyWithPrecedence(ImmutableList.of(Publisher.BBC /*, Publisher.YOUTUBE */)));
+        Map<String, List<Identified>> merged = ImmutableMap.copyOf(merger.executeUriQuery(ImmutableList.of(item1.getCanonicalUri()), query));
+        
+        assertEquals(ImmutableList.of(item1), merged.get(item1.getCanonicalUri()));
+        assertEquals(ImmutableList.of(clip1), ((Episode)Iterables.getOnlyElement(merged.get(item1.getCanonicalUri()))).getClips());
+	}
+	
     public void testMergingPeople() throws Exception {
         MergeOnOutputQueryExecutor merger = new MergeOnOutputQueryExecutor(delegate(film1, film2));
 
