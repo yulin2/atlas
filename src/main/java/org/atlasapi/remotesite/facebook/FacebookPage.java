09264f4458702034f3b2cf0f2f81b48839afe608
Fred van den Driessche <fred@metabroadcast.com>
2012-08-30 14:01:15 +0100

    Add on-request Facebook Brand adapter
    Uses standard SiteSpecificAdapter + ContentExtractor pattern autowired
    into UriFetchingQueryExecutor to fetch Facebook Pages as Brands as they
    are requested.

---
+++ src/main/java/org/atlasapi/remotesite/facebook/FacebookPage.java
@@ -0,0 +1,78 @@
+package org.atlasapi.remotesite.facebook;
+
+public class FacebookPage {
+
+    private String id;
+    private String name;
+    private String plotOutline;
+    private String link;
+    private String website;
+    private String starring;
+    private String directedBy;
+    private String writtenBy;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPlotOutline(String plotOutline) {
+        this.plotOutline = plotOutline;
+    }
+    
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setStarring(String starring) {
+        this.starring = starring;
+    }
+
+    public void setDirectedBy(String directedBy) {
+        this.directedBy = directedBy;
+    }
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPlotOutline() {
+        return this.plotOutline;
+    }
+
+    public String getLink() {
+        return this.link;
+    }
+
+    public String getWebsite() {
+        return this.website;
+    }
+
+    public String getStarring() {
+        return this.starring;
+    }
+
+    public String getDirectedBy() {
+        return this.directedBy;
+    }
+
+    public String getWrittenBy() {
+        return this.writtenBy;
+    }
+    
+}
