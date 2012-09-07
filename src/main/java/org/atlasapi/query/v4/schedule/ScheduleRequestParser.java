2791b765349a14c51ee803e845acf97d0709cd61
Fred van den Driessche <fred@metabroadcast.com>
2012-09-07 15:22:55 +0100

    Fixes for v4 schedule end-point

--- src/main/java/org/atlasapi/query/v4/schedule/ScheduleRequestParser.java
+++ src/main/java/org/atlasapi/query/v4/schedule/ScheduleRequestParser.java
@@ -1,7 +1,8 @@
 package org.atlasapi.query.v4.schedule;
 
-
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.metabroadcast.common.webapp.query.DateTimeInQueryParser.queryDateTimeParser;
+import static org.atlasapi.output.Annotation.defaultAnnotations;
 
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -17,9 +18,11 @@ import org.atlasapi.output.Annotation;
 import org.atlasapi.persistence.media.channel.ChannelResolver;
 import org.atlasapi.query.v2.QueryParameterAnnotationsExtractor;
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.joda.time.Interval;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
 import com.metabroadcast.common.base.Maybe;
 import com.metabroadcast.common.ids.NumberToShortStringCodec;
 import com.metabroadcast.common.ids.SubstitutionTableNumberCodec;
@@ -28,70 +31,68 @@ import com.metabroadcast.common.webapp.query.DateTimeInQueryParser;
 class ScheduleRequestParser {
     
     private static final Pattern CHANNEL_ID_PATTERN = Pattern.compile(
-        ".*/([^.]+)(.[\\w\\d.]+)?$"
+        ".*schedules/([^.]+)(.[\\w\\d.]+)?$"
     );
     
     private final ChannelResolver channelResolver;
     private final ApplicationConfigurationFetcher applicationStore;
+
+    private final RequestParameterValidator validator = new RequestParameterValidator(ImmutableSet.of("from","to","source"),ImmutableSet.of("annotations","apiKey"));
     
     private final NumberToShortStringCodec idCodec;
     private final DateTimeInQueryParser dateTimeParser;
     private final QueryParameterAnnotationsExtractor annotationExtractor;
+    private Duration maxQueryDuration;
 
-
-    public ScheduleRequestParser(ChannelResolver channelResolver, ApplicationConfigurationFetcher appFetcher) {
+    public ScheduleRequestParser(ChannelResolver channelResolver, ApplicationConfigurationFetcher appFetcher, Duration maxQueryDuration) {
         this.channelResolver = channelResolver;
         this.applicationStore = appFetcher;
+        this.maxQueryDuration = maxQueryDuration;
         this.idCodec = new SubstitutionTableNumberCodec();
-        this.dateTimeParser = new DateTimeInQueryParser();
+        this.dateTimeParser = queryDateTimeParser()
+                .parsesIsoDateTimes()
+                .parsesIsoTimes()
+                .parsesIsoDates()
+                .parsesOffsets()
+                .build();
         this.annotationExtractor = new QueryParameterAnnotationsExtractor();
     }
 
     public ScheduleQuery queryFrom(HttpServletRequest request) {
+        // Attempt to extract channel first so we can 404 if missing before
+        // 400ing from bad params.
+        Channel channel = extractChannel(request);
 
-        String channelId = getChannelId(request.getRequestURI());
-        long cid = idCodec.decode(channelId).longValue();
-        Maybe<Channel> channel = channelResolver.fromId(cid);
+        validator.validateParameters(request);
         
-        checkArgument(channel.hasValue(), "Unknown channel %s", channelId);
-        
-        DateTime from = dateTimeParser.parse(getParameter(request, "from"));
-        DateTime to = dateTimeParser.parse(getParameter(request, "to"));
-        
-        String pubKey = getParameter(request, "publisher");
-        Maybe<Publisher> publisher = Publisher.fromKey(pubKey);
-        
-        checkArgument(publisher.hasValue(), "Unknown publisher %s", pubKey);
-        
-        Set<Annotation> annotations = annotationExtractor.extract(request).or(Annotation.defaultAnnotations());
+        Publisher publisher = extractPublisher(request);
+        Interval queryInterval = extractInterval(request);
         
         ApplicationConfiguration appConfig = getConfiguration(request);
+        checkArgument(appConfig.getEnabledSources().contains(publisher), "Source %s not enabled", publisher);
         
-        return new ScheduleQuery(
-            publisher.requireValue(),
-            channel.requireValue(), 
-            new Interval(from, to),
-            appConfig,
-            annotations);
+        Set<Annotation> annotations = annotationExtractor.extract(request).or(defaultAnnotations());
+
+        return new ScheduleQuery(publisher, channel, queryInterval, appConfig, annotations);
     }
 
-    private ApplicationConfiguration getConfiguration(HttpServletRequest request) {
-        Maybe<ApplicationConfiguration> config = applicationStore.configurationFor(request);
-        if (config.hasValue()) {
-            return config.requireValue();
-        }
-        // request doesn't specify apiKey so use default configuration.
-        if (request.getParameter("apiKey") == null) {
-            return ApplicationConfiguration.DEFAULT_CONFIGURATION;
-        }
-        // the request has an apiKey param but no config is found.
-        throw new RuntimeException("Uknown application " + request);
+    private Channel extractChannel(HttpServletRequest request) {
+        String channelId = getChannelId(request.getRequestURI());
+        Maybe<Channel> channel = resolveChannel(channelId);
+        
+        //Exception thrown should be translated to 404 not 400.
+        checkArgument(channel.hasValue(), "Unknown channel '%s'", channelId);
+        return channel.requireValue();
     }
 
-    private String getParameter(HttpServletRequest request, String param) {
-        String paramValue = request.getParameter(param);
-        checkArgument(!Strings.isNullOrEmpty(paramValue), "Missing required parameter %s", param);
-        return paramValue;
+    private Maybe<Channel> resolveChannel(String channelId) {
+        long cid;
+        try {
+            cid = idCodec.decode(channelId).longValue();
+        } catch (IllegalArgumentException e) {
+            return Maybe.nothing();
+        }
+        return channelResolver.fromId(cid);
     }
 
     private String getChannelId(String requestUri) {
@@ -99,7 +100,43 @@ class ScheduleRequestParser {
         if (matcher.matches()) {
             return matcher.group(1);
         }
-        return "";
+        throw new IllegalArgumentException("Channel identifier missing");
+    }
+    
+    private Interval extractInterval(HttpServletRequest request) {
+        DateTime from = dateTimeParser.parse(getParameter(request, "from"));
+        DateTime to = dateTimeParser.parse(getParameter(request, "to"));
+        
+        Interval queryInterval = new Interval(from, to);
+        checkArgument(!queryInterval.toDuration().isLongerThan(maxQueryDuration), "Query interval cannot be longer than %s", maxQueryDuration);
+        return queryInterval;
+    }
+
+    private Publisher extractPublisher(HttpServletRequest request) {
+        String pubKey = getParameter(request, "source");
+        Maybe<Publisher> publisher = Publisher.fromKey(pubKey);
+        checkArgument(publisher.hasValue(), "Unknown source %s", pubKey);
+        return publisher.requireValue();
+    }
+
+    private ApplicationConfiguration getConfiguration(HttpServletRequest request) {
+        Maybe<ApplicationConfiguration> config = applicationStore.configurationFor(request);
+        if (config.hasValue()) {
+            return config.requireValue();
+        }
+        String apiKeyParam = request.getParameter("apiKey");
+        // request doesn't specify apiKey so use default configuration.
+        if (apiKeyParam == null) {
+            return ApplicationConfiguration.DEFAULT_CONFIGURATION;
+        }
+        // the request has an apiKey param but no config is found.
+        throw new IllegalArgumentException("Unknown API key " + apiKeyParam);
+    }
+
+    private String getParameter(HttpServletRequest request, String param) {
+        String paramValue = request.getParameter(param);
+        checkArgument(!Strings.isNullOrEmpty(paramValue), "Missing required parameter %s", param);
+        return paramValue;
     }
 
 }
