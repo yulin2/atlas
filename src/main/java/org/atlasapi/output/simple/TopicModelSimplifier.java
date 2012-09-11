c245c3f475e42b68cc4db9fd5290f5d56776db7b
Sergio Bossa <sergio.bossa@gmail.com>
2012-09-11 17:17:20 +0100

    Added extension to make topics reachable.

--- src/main/java/org/atlasapi/output/simple/TopicModelSimplifier.java
+++ src/main/java/org/atlasapi/output/simple/TopicModelSimplifier.java
@@ -8,6 +8,8 @@ import org.atlasapi.output.Annotation;
 
 public class TopicModelSimplifier extends IdentifiedModelSimplifier<Topic, org.atlasapi.media.entity.simple.Topic> {
 
+    private final static String EXTENSION = ".json";
+    //
     private String topicUriBase;
     
     public TopicModelSimplifier(String localHostName) {
@@ -18,7 +20,7 @@ public class TopicModelSimplifier extends IdentifiedModelSimplifier<Topic, org.a
     public org.atlasapi.media.entity.simple.Topic simplify(Topic fullTopic, Set<Annotation> annotations, final ApplicationConfiguration config) {
         org.atlasapi.media.entity.simple.Topic topic = new org.atlasapi.media.entity.simple.Topic();
         copyIdentifiedAttributesTo(fullTopic, topic, annotations);
-        topic.setUri(topicUriBase+topic.getId());
+        topic.setUri(topicUriBase + topic.getId() + EXTENSION);
         topic.setTitle(fullTopic.getTitle());
         topic.setDescription(fullTopic.getDescription());
         topic.setImage(fullTopic.getImage());
