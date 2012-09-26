2bcde56054c8e7f984716f91e28def84af8c6c5a
Sergio Bossa <sergio.bossa@gmail.com>
2012-09-26 18:09:11 +0100

    Implemented lookup entry bootstrap.

--- src/main/java/org/atlasapi/persistence/AtlasPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/AtlasPersistenceModule.java
@@ -323,6 +323,7 @@ public class AtlasPersistenceModule {
         bootstrapper.withChannelListers(channelStore());
         bootstrapper.withContentGroupListers(contentGroupResolver());
         bootstrapper.withContentListers(contentLister());
+        bootstrapper.withLookupEntryListers(lookupStore());
         bootstrapper.withPeopleListers(personStore());
         bootstrapper.withProductListers(productStore());
         bootstrapper.withSegmentListers(segmentResolver());
