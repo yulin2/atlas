a1c1ba59a21326787506d69671b8c7f10851b26b
Fred van den Driessche <fred@metabroadcast.com>
2012-09-26 11:38:28 +0100

    Use only Cassandra for query content resolution

--- src/main/java/org/atlasapi/query/QueryModule.java
+++ src/main/java/org/atlasapi/query/QueryModule.java
@@ -21,9 +21,7 @@ import org.atlasapi.equiv.query.MergeOnOutputQueryExecutor;
 import org.atlasapi.equiv.update.ContentEquivalenceUpdater;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.persistence.content.ContentResolver;
-import org.atlasapi.persistence.content.FilterScheduleOnlyKnownTypeContentResolver;
 import org.atlasapi.persistence.content.KnownTypeContentResolver;
-import org.atlasapi.persistence.content.SimpleKnownTypeContentResolver;
 import org.atlasapi.persistence.content.query.KnownTypeQueryExecutor;
 import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
 import org.atlasapi.query.content.ApplicationConfigurationQueryExecutor;
@@ -48,7 +46,7 @@ public class QueryModule {
     @Qualifier("remoteSiteContentResolver")
     private CanonicalisingFetcher localOrRemoteFetcher;
     @Autowired
-    private LookupEntryStore mongoStore;
+    private LookupEntryStore lookupEntry;
     @Autowired
     private KnownTypeContentResolver mongoResolver;
     @Autowired @Qualifier(value="cassandra")
@@ -64,9 +62,8 @@ public class QueryModule {
     @Bean
     public KnownTypeQueryExecutor queryExecutor() {
 
-        KnownTypeQueryExecutor queryExecutor = new LookupResolvingQueryExecutor(new SimpleKnownTypeContentResolver(cassandraResolver),
-                new FilterScheduleOnlyKnownTypeContentResolver(mongoResolver),
-                mongoStore);
+        KnownTypeQueryExecutor queryExecutor = new LookupResolvingQueryExecutor(cassandraResolver,
+            lookupEntry);
 
         queryExecutor = new UriFetchingQueryExecutor(localOrRemoteFetcher, queryExecutor, equivUpdater, ImmutableSet.of(FACEBOOK));
 
