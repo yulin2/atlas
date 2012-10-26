e0d9155f9012c93723392da09427e37038694946
Sergio Bossa <sergio.bossa@gmail.com>
2012-10-26 16:18:11 +0100

    Removed hash check for content changes from MessageQueueingContentWriter, as based on Mongo.

--- src/main/java/org/atlasapi/media/content/util/MessageQueueingContentWriter.java
+++ src/main/java/org/atlasapi/media/content/util/MessageQueueingContentWriter.java
@@ -9,8 +9,6 @@ import org.atlasapi.media.content.Container;
 import org.atlasapi.media.content.Content;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.persistence.content.ContentWriter;
-import org.atlasapi.persistence.media.entity.ContainerTranslator;
-import org.atlasapi.persistence.media.entity.ItemTranslator;
 import org.atlasapi.messaging.EntityUpdatedMessage;
 import org.atlasapi.serialization.json.JsonFactory;
 import org.slf4j.Logger;
@@ -28,51 +26,37 @@ import java.util.UUID;
 public class MessageQueueingContentWriter implements ContentWriter {
 
     private static final Logger log = LoggerFactory.getLogger(MessageQueueingContentWriter.class);
-    
     private final ObjectMapper mapper = JsonFactory.makeJsonMapper();
-    
-	private final JmsTemplate template;
-	private final ContentWriter delegate;
-	private final Clock clock;
-
-    private final ItemTranslator itemTranslator;
-    private final ContainerTranslator containerTranslator;
+    private final JmsTemplate template;
+    private final ContentWriter delegate;
+    private final Clock clock;
 
     public MessageQueueingContentWriter(JmsTemplate template, ContentWriter delegate) {
         this(template, delegate, new SystemClock());
     }
-	
-	public MessageQueueingContentWriter(JmsTemplate template, ContentWriter delegate, Clock clock) {
-        NumberToShortStringCodec idCodec = new SubstitutionTableNumberCodec();
-		this.template = template;
-		this.delegate = delegate;
-		this.clock = clock;
-		this.itemTranslator = new ItemTranslator(idCodec);
-		this.containerTranslator = new ContainerTranslator(idCodec);
-	}
-	
-	@Override
-	public void createOrUpdate(Item item) {
-		delegate.createOrUpdate(item);
-	    if (!item.hashChanged(itemTranslator.hashCodeOf(item))) {
-            log.debug("{} not changed", item.getCanonicalUri());
-            return;
-        } 
-		enqueueMessageUpdatedMessage(item);
-	}
 
-	@Override
-	public void createOrUpdate(Container container) {
-	    delegate.createOrUpdate(container);
-        if (!container.hashChanged(containerTranslator.hashCodeOf(container))) {
-            log.debug("{} un-changed", container.getCanonicalUri());
-            return;
-        } 
-	    enqueueMessageUpdatedMessage(container);
-	}
+    public MessageQueueingContentWriter(JmsTemplate template, ContentWriter delegate, Clock clock) {
+        NumberToShortStringCodec idCodec = new SubstitutionTableNumberCodec();
+        this.template = template;
+        this.delegate = delegate;
+        this.clock = clock;
+    }
+
+    @Override
+    public void createOrUpdate(Item item) {
+        delegate.createOrUpdate(item);
+        enqueueMessageUpdatedMessage(item);
+    }
+
+    @Override
+    public void createOrUpdate(Container container) {
+        delegate.createOrUpdate(container);
+        enqueueMessageUpdatedMessage(container);
+    }
 
     private void enqueueMessageUpdatedMessage(final Content content) {
         template.send(new MessageCreator() {
+
             @Override
             public Message createMessage(Session session) throws JMSException {
                 TextMessage message = session.createTextMessage(serialize(createEntityUpdatedMessage(content)));
@@ -83,18 +67,17 @@ public class MessageQueueingContentWriter implements ContentWriter {
 
     private EntityUpdatedMessage createEntityUpdatedMessage(Content content) {
         return new EntityUpdatedMessage(
-            UUID.randomUUID().toString(),
-            clock.now().getMillis(),
-            content.getCanonicalUri(), 
-            content.getClass().getSimpleName().toLowerCase(),
-            content.getPublisher().key()
-        );
+                UUID.randomUUID().toString(),
+                clock.now().getMillis(),
+                content.getCanonicalUri(),
+                content.getClass().getSimpleName().toLowerCase(),
+                content.getPublisher().key());
     }
 
     private String serialize(final EntityUpdatedMessage message) {
         String result = null;
         try {
-             result = mapper.writeValueAsString(message);
+            result = mapper.writeValueAsString(message);
         } catch (Exception e) {
             log.error(message.getEntityId(), e);
         }
