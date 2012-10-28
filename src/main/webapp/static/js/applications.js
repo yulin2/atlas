8e10dcb91c9e6868a348279a0da5a3324208741e
liamgh <liam@greenhughes.com>
2012-10-28 17:27:24 +0000

    New code to enable and disable publishers

--- src/main/webapp/static/js/applications.js
+++ src/main/webapp/static/js/applications.js
@@ -263,48 +263,42 @@ var updatePrecedence = function(callback) {
 var updateEnabled = function(callback){
 	var slug = app.slug;
 	var count = 0;
+	var pubs = {'enabled':[], 'disabled':[]};
 	for(var i = 0, ii = app.configuration.publishers.length; i<ii; i++){
 		var enabled = app.configuration.publishers[i].enabled;
 		var publisher = app.configuration.publishers[i].key;
 		var state = app.configuration.publishers[i].state;
-		
 		if (state != "available") {
-			if(count === ii-1){
-				callback();
-			}
-			count++;
 			continue;
 		}
-		var type = 'delete';
-		var data = null;
-		if(enabled){
-			type = 'post';
-			data = {pubkey: publisher};
+		if (enabled) {
+			pubs.enabled.push(publisher);
+		} else {
+			pubs.disabled.push(publisher);
 		}
-		var url = "/admin/applications/"+slug+"/publishers/enabled";
-		if(!enabled){
-			url += "/"+publisher;
-		}
-		url += ".json";
-		$.ajax({
-			type: type,
-			url: url,
-			data: data,
-			async: false,
-			success:function(responseData, textStatus, XMLHttpRequest) {
-				
-			},
-			error:function(textStatus) {
-				console.log("fail:", textStatus);
-			},
-			complete: function(){
-				if(count === ii-1){
-					callback();
-				}
-				count++;
-			}
-		});
+		
 	}
+	var url = "/admin/applications/"+slug+"/publishers/update.json";
+	var data = "enabled=" + pubs.enabled.join(",") + "&" + "disabled=" + pubs.disabled.join(",");
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: data,
+		async: false,
+		success:function(responseData, textStatus, XMLHttpRequest) {
+			$('#saveApplicationSources').val("Changes saved");
+			setTimeout(function() {
+				$('#saveApplicationSources').val("Save Changes");
+			}, 2000);
+		},
+		error:function(textStatus) {
+			console.log("fail:", textStatus);
+		},
+		complete: function(){
+			callback();
+		}
+	});
 }
 
 var disablePrecedence = function(callback) {
