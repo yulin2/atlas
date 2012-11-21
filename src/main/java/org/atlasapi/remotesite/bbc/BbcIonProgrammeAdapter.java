32272351b9fa3753e900d92fc960707ae49fbd20
Fred van den Driessche <fred@metabroadcast.com>
2012-11-21 16:20:16 +0000

    Merge BBC content on full update

--- src/main/java/org/atlasapi/remotesite/bbc/BbcIonProgrammeAdapter.java
+++ src/main/java/org/atlasapi/remotesite/bbc/BbcIonProgrammeAdapter.java
@@ -12,10 +12,12 @@ import org.atlasapi.media.entity.Clip;
 import org.atlasapi.media.entity.Container;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Episode;
+import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Series;
 import org.atlasapi.media.entity.Version;
 import org.atlasapi.media.segment.SegmentEvent;
+import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.system.RemoteSiteClient;
 import org.atlasapi.remotesite.SiteSpecificAdapter;
@@ -24,29 +26,39 @@ import org.atlasapi.remotesite.bbc.SlashProgrammesRdf.SlashProgrammesClip;
 import org.atlasapi.remotesite.bbc.SlashProgrammesRdf.SlashProgrammesSeriesContainer;
 import org.atlasapi.remotesite.bbc.SlashProgrammesRdf.SlashProgrammesSeriesRef;
 import org.atlasapi.remotesite.bbc.ion.BbcExtendedDataContentAdapter;
+import org.atlasapi.remotesite.bbc.ion.BbcIonItemMerger;
 
 import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Predicates;
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
+import com.metabroadcast.common.base.Maybe;
 
-public class BbcIonProgrammeAdapter implements SiteSpecificAdapter<Content> {
+public class BbcIonProgrammeAdapter extends AbstractBbcAdapter<Content> {
 
-    private ContentWriter writer;
-    private SiteSpecificAdapter<Item> episodeAdapter;
-    private BbcExtendedDataContentAdapter extendedDataAdapter;
-    private SiteSpecificAdapter<List<SegmentEvent>> segmentAdapter;
-    private RemoteSiteClient<SlashProgrammesRdf> slashProgrammesClient;
-    private SiteSpecificAdapter<Container> containerAdapter;
-    private SiteSpecificAdapter<Clip> clipAdapter;
-    private ListeningExecutorService executor;
+    private final ContentResolver resolver;
+    private final ContentWriter writer;
+    
+    private final SiteSpecificAdapter<Item> episodeAdapter;
+    private final SiteSpecificAdapter<List<SegmentEvent>> segmentAdapter;
+    private final BbcIonItemMerger merger;
 
-    public BbcIonProgrammeAdapter(ContentWriter writer, SiteSpecificAdapter<Item> episodeAdapter, BbcExtendedDataContentAdapter extendedDataAdapter, SiteSpecificAdapter<List<SegmentEvent>> segmentAdapter, RemoteSiteClient<SlashProgrammesRdf> slashProgrammesClient, SiteSpecificAdapter<Container> containerAdapter, SiteSpecificAdapter<Clip> clipAdapter, ExecutorService executor) {
+    private final BbcExtendedDataContentAdapter extendedDataAdapter;
+    
+    private final RemoteSiteClient<SlashProgrammesRdf> slashProgrammesClient;
+    private final SiteSpecificAdapter<Container> containerAdapter;
+    private final SiteSpecificAdapter<Clip> clipAdapter;
+    private final ListeningExecutorService executor;
+
+    public BbcIonProgrammeAdapter(ContentResolver resolver, ContentWriter writer, SiteSpecificAdapter<Item> episodeAdapter, BbcExtendedDataContentAdapter extendedDataAdapter, SiteSpecificAdapter<List<SegmentEvent>> segmentAdapter, RemoteSiteClient<SlashProgrammesRdf> slashProgrammesClient, SiteSpecificAdapter<Container> containerAdapter, SiteSpecificAdapter<Clip> clipAdapter, ExecutorService executor) {
+        this.resolver = resolver;
         this.writer = writer;
         this.episodeAdapter = episodeAdapter;
         this.extendedDataAdapter = extendedDataAdapter;
@@ -55,18 +67,14 @@ public class BbcIonProgrammeAdapter implements SiteSpecificAdapter<Content> {
         this.containerAdapter = containerAdapter;
         this.clipAdapter = clipAdapter;
         this.executor = MoreExecutors.listeningDecorator(executor);
+        this.merger = new BbcIonItemMerger();
     }
     
     @Override
-    public boolean canFetch(String uri) {
-        return BbcFeeds.isACanonicalSlashProgrammesUri(uri);
-    }
-
-    @Override
     public Content fetch(String uri) {
-        Item fetchedItem = fetchItem(uri);
+        Preconditions.checkArgument(canFetch(uri), "Can't fetch " + uri);
+        Item fetchedItem = fetchItem(uri, null);
         if (fetchedItem != null) {
-            writer.createOrUpdate(fetchedItem);
             return fetchedItem;
         }
         return fetchContainer(uri);
@@ -77,26 +85,35 @@ public class BbcIonProgrammeAdapter implements SiteSpecificAdapter<Content> {
         if (fetchedContainer == null) {
             return null;
         }
-        mergeExtendedData(fetchedContainer, extendedDataAdapter.fetch(uri));
+        addExtendedData(fetchedContainer, extendedDataAdapter.fetch(uri));
         SlashProgrammesRdf programmeRdf = getSlashProgrammes(uri);
         if (fetchedContainer instanceof Series) {
             for (SlashProgrammesSeriesContainer series :  programmeRdf.series()) {
                 fetchClips(fetchedContainer, series);
             }
-            writer.createOrUpdate(fetchedContainer);
+            mergeAndWrite(fetchedContainer);
             for (SlashProgrammesSeriesContainer seriesContainer : programmeRdf.series()) {
                 fetchSubSeries(seriesContainer, (Series)fetchedContainer);
                 fetchEpisodes(seriesContainer.episodeResourceUris(), (Series)fetchedContainer);
             }
         } else if (fetchedContainer instanceof Brand) {
             fetchClips(fetchedContainer, programmeRdf.brand());
-            writer.createOrUpdate(fetchedContainer);
+            mergeAndWrite(fetchedContainer);
             fetchSeries(programmeRdf.brand().series);
             if (programmeRdf.brand().episodes != null) {
                 fetchEpisodes(programmeRdf.brand().episodeResourceUris(), null);
             }
         }
         return fetchedContainer;
+    }
+
+    private void mergeAndWrite(Container fetchedContainer) {
+        Maybe<Identified> possibleExistingContent = existingContent(fetchedContainer.getCanonicalUri());
+        if (possibleExistingContent.hasValue()) {
+            Container existingContainer = (Container)possibleExistingContent.requireValue();
+            fetchedContainer = merger.mergeContainers(fetchedContainer, existingContainer);
+        }
+        writer.createOrUpdate(fetchedContainer);
     }
 
     private void fetchSubSeries(SlashProgrammesSeriesContainer seriesContainer, Series series) {
@@ -116,34 +133,30 @@ public class BbcIonProgrammeAdapter implements SiteSpecificAdapter<Content> {
         }
     }
 
-    private void fetchEpisodes(List<String> episodes, Series series) {
+    private void fetchEpisodes(List<String> episodes, final Series series) {
         if (episodes != null) {
             for (List<String> episodesPart : Lists.partition(episodes, 10)) {
                 ListenableFuture<List<Item>> fetched = Futures.allAsList(Lists.transform(episodesPart, new Function<String, ListenableFuture<Item>>() {
                     @Override
                     public ListenableFuture<Item> apply(@Nullable final String input) {
-                        return executor.submit(itemFetchTask(input));
+                        return executor.submit(itemFetchTask(input, series));
                     }
                 }));
                 try {
-                    for (Item item : Iterables.filter(Futures.get(fetched, 20, TimeUnit.MINUTES, Exception.class), Predicates.notNull())) {
-                        if (series != null && item instanceof Episode) {
-                            ((Episode)item).setSeriesNumber(series.getSeriesNumber());
-                        }
-                        writer.createOrUpdate(item);
-                    }
+                    Futures.get(fetched, 20, TimeUnit.MINUTES, Exception.class);
                 } catch (Exception e) {
-                    throw Throwables.propagate(e);
+                    Throwables.propagate(e);
                 }
             }
         }
     }
 
-    protected Callable<Item> itemFetchTask(final String episodeUri) {
+    protected Callable<Item> itemFetchTask(final String episodeUri, final Series series) {
         return new Callable<Item>() {
             @Override
             public Item call() throws Exception {
-                return fetchItem(BbcFeeds.slashProgrammesUriForPid(extractPid(episodeUri)));
+                String uri = BbcFeeds.slashProgrammesUriForPid(extractPid(episodeUri));
+                return fetchItem(uri, series);
             }
         };
     }
@@ -181,14 +194,26 @@ public class BbcIonProgrammeAdapter implements SiteSpecificAdapter<Content> {
         return resourceUri.substring(resourceUri.lastIndexOf('/')+1, resourceUri.indexOf('#'));
     }
 
-    protected Item fetchItem(String uri) {
+    protected Item fetchItem(String uri, Series series) {
         Item fetchedItem = episodeAdapter.fetch(uri);
         if (fetchedItem == null) {
             return null;
         }
-        mergeExtendedData(fetchedItem, extendedDataAdapter.fetch(uri));
+        addExtendedData(fetchedItem, extendedDataAdapter.fetch(uri));
         attachSegmentsToVersions(fetchedItem);
+        if (fetchedItem instanceof Episode && series != null) {
+            ((Episode)fetchedItem).setSeriesNumber(series.getSeriesNumber());
+        }
+        Maybe<Identified> possibleExistingContent = existingContent(uri);
+        if (possibleExistingContent.hasValue()) {
+            fetchedItem = merger.merge(fetchedItem, (Item)possibleExistingContent.requireValue());
+        }
+        writer.createOrUpdate(fetchedItem);
         return fetchedItem;
+    }
+
+    private Maybe<Identified> existingContent(String uri) {
+        return resolver.findByCanonicalUris(ImmutableSet.of(uri)).get(uri);
     }
 
     private void attachSegmentsToVersions(Item fetchedItem) {
@@ -197,7 +222,7 @@ public class BbcIonProgrammeAdapter implements SiteSpecificAdapter<Content> {
         }
     }
 
-    private void mergeExtendedData(Content mainContent, Content extendedData) {
+    private void addExtendedData(Content mainContent, Content extendedData) {
         mainContent.setRelatedLinks(extendedData.getRelatedLinks());
         mainContent.setKeyPhrases(extendedData.getKeyPhrases());
         mainContent.setTopicRefs(extendedData.getTopicRefs());
