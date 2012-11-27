983a3f8468f7365f4e0ece9e90bf0567e9f43ce8
Fred van den Driessche <fred@metabroadcast.com>
2012-11-27 19:12:37 +0000

    Revert "Fix equivalence on schedule endpoint"
    This reverts commit 8383f0d8e2422fdc905a5b5d0e3aa26c9dac57e6.

--- src/main/java/org/atlasapi/equiv/query/MergeOnOutputQueryExecutor.java
+++ src/main/java/org/atlasapi/equiv/query/MergeOnOutputQueryExecutor.java
@@ -1,25 +1,48 @@
 package org.atlasapi.equiv.query;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.atlasapi.application.ApplicationConfiguration;
 import org.atlasapi.content.criteria.ContentQuery;
 import org.atlasapi.equiv.OutputContentMerger;
+import org.atlasapi.equiv.SeriesOrder;
 import org.atlasapi.media.common.Id;
+import org.atlasapi.media.content.Container;
 import org.atlasapi.media.content.Content;
+import org.atlasapi.media.entity.Broadcast;
+import org.atlasapi.media.entity.Certificate;
+import org.atlasapi.media.entity.Clip;
+import org.atlasapi.media.entity.Episode;
+import org.atlasapi.media.entity.Film;
 import org.atlasapi.media.entity.Identified;
+import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.KeyPhrase;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.media.entity.ReleaseDate;
+import org.atlasapi.media.entity.Subtitles;
+import org.atlasapi.media.entity.TopicRef;
+import org.atlasapi.media.entity.Version;
 import org.atlasapi.persistence.content.query.KnownTypeQueryExecutor;
 
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSet.Builder;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.Sets;
 
 public class MergeOnOutputQueryExecutor implements KnownTypeQueryExecutor {
 
+    private static final Ordering<Episode> SERIES_ORDER = Ordering.from(new SeriesOrder());
     private final KnownTypeQueryExecutor delegate;
     
     private final OutputContentMerger merger = new OutputContentMerger();
@@ -69,5 +92,5 @@ public class MergeOnOutputQueryExecutor implements KnownTypeQueryExecutor {
             }
         });
     }
-
+    
 }
