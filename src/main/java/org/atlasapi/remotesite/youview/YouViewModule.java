85c7a70ab26ba08a2415b06c227f252af2144963
Oli Hall <oli@metabradcast.com>
2012-12-10 17:52:53 +0000

    First version of youview ingest

---
+++ src/main/java/org/atlasapi/remotesite/youview/YouViewModule.java
@@ -0,0 +1,56 @@
+package org.atlasapi.remotesite.youview;
+
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.atlasapi.media.channel.Channel;
+import org.atlasapi.media.channel.ChannelResolver;
+import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.ContentWriter;
+import org.joda.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.metabroadcast.common.properties.Configurer;
+import com.metabroadcast.common.scheduling.RepetitionRules;
+import com.metabroadcast.common.scheduling.RepetitionRules.Every;
+import com.metabroadcast.common.scheduling.SimpleScheduler;
+
+public class YouViewModule {
+    private final static Every EVERY_15_MINUTES = RepetitionRules.every(Duration.standardMinutes(15));
+    private final static Every EVERY_HOUR = RepetitionRules.every(Duration.standardHours(1));
+    
+    private @Autowired SimpleScheduler scheduler;
+    private @Autowired ChannelResolver channelResolver;
+    private @Autowired ContentResolver contentResolver;
+    private @Autowired ContentWriter contentWriter;
+    
+    @PostConstruct
+    public void startBackgroundTasks() {
+        scheduler.schedule(youViewTodayUpdater().withName("YouView Today Updater"), EVERY_15_MINUTES);
+        scheduler.schedule(youViewFornightUpdater().withName("YouView Updater ±7 Days"), EVERY_HOUR);
+    }
+
+    private YouViewFortnightUpdater youViewFornightUpdater() {
+        return new YouViewFortnightUpdater(youViewFetcher(), youViewXmlElementHandler());
+    }
+
+    private YouViewTodayUpdater youViewTodayUpdater() {
+        return new YouViewTodayUpdater(youViewFetcher(), youViewXmlElementHandler());
+    }
+    
+    private YouViewScheduleFetcher youViewFetcher() {
+        String url = Configurer.get("youview.url").get();
+        int timeout = Configurer.get("youview.timeout").toInt();
+        return new YouViewScheduleFetcher(url, timeout);
+    }
+    
+    private YouViewXmlElementHandler youViewXmlElementHandler() {
+        YouViewContentExtractor extractor = new YouViewContentExtractor(channelMapping());
+        return new DefaultYouViewXmlElementHandler(extractor, contentResolver, contentWriter);
+    }
+
+    private Map<String, Channel> channelMapping() {
+        return YouViewChannelResolver.generateChannelMapping(channelResolver);
+    }
+}
