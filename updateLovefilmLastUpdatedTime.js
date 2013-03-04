733b6a731ce9e977f3c3adcbd7bbb3c63a06226e
Oli Hall <oli@metabradcast.com>
2013-03-04 15:30:19 +0000

    Update all lastUpdated fields for all lovefilm items with mongo script

--- updateLovefilmLastUpdatedTime.js
+++ updateLovefilmLastUpdatedTime.js
@@ -1,3 +1,3 @@
-db.topLevelItems.update({ "publisher" : "lovefilm.com" }, {$set : { "lastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }}, false, true);
-db.children.update({ "publisher" : "lovefilm.com" }, {$set : { "lastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }}, false, true);
-db.containers.update({ "publisher" : "lovefilm.com" }, {$set : { "lastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }}, false, true);
+db.topLevelItems.update({ "publisher" : "lovefilm.com" }, {$set : { "lastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }, $set : { "thisOrChildLastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }}, false, true);
+db.children.update({ "publisher" : "lovefilm.com" }, {$set : { "lastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }, $set : { "thisOrChildLastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }}, false, true);
+db.containers.update({ "publisher" : "lovefilm.com" }, {$set : { "lastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }, $set : { "thisOrChildLastUpdated" : ISODate("2013-03-01T00:00:00.000Z") }}, false, true);
