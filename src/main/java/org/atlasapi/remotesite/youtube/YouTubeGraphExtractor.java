a8d7d34b496e78bc5adb50b279e91f7737e72508
augusto <gutouehara@gmail.com>
2013-03-06 11:47:04 +0000

    MBST-4754: Fixes to YouTube adapter - removed unused imports

--- src/main/java/org/atlasapi/remotesite/youtube/YouTubeGraphExtractor.java
+++ src/main/java/org/atlasapi/remotesite/youtube/YouTubeGraphExtractor.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.atlasapi.media.TransportSubType;
 import org.atlasapi.media.TransportType;
 import org.atlasapi.media.entity.Alias;
 import org.atlasapi.media.entity.Encoding;
