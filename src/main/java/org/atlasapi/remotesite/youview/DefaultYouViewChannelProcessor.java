606c8b7da956f34fff3da7b199c46207b03a8ab1
Oli Hall <oli@metabradcast.com>
2013-03-22 11:36:38 +0000

    Refactor the youview ingest to iterate over days, then channels, to allow updating of schedule blocks directly from the ingest job.

---
+++ src/main/java/org/atlasapi/remotesite/youview/DefaultYouViewChannelProcessor.java
@@ -0,0 +1,74 @@
+package org.atlasapi.remotesite.youview;
+
+import java.util.List;
+
+import nu.xom.Elements;
+
+import org.atlasapi.media.channel.Channel;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.media.entity.ScheduleEntry.ItemRefAndBroadcast;
+import org.atlasapi.persistence.content.schedule.mongo.ScheduleWriter;
+import org.atlasapi.remotesite.channel4.epg.BroadcastTrimmer;
+import org.atlasapi.remotesite.redux.UpdateProgress;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.Lists;
+
+public class DefaultYouViewChannelProcessor implements YouViewChannelProcessor {
+
+    private final ScheduleWriter scheduleWriter;
+    private final YouViewElementProcessor processor;
+    private final BroadcastTrimmer trimmer;
+    private final Logger log = LoggerFactory.getLogger(DefaultYouViewChannelProcessor.class);
+    
+    public DefaultYouViewChannelProcessor(ScheduleWriter scheduleWriter, YouViewElementProcessor processor, BroadcastTrimmer trimmer) {
+        this.scheduleWriter = scheduleWriter;
+        this.processor = processor;
+        this.trimmer = trimmer;
+    }
+    
+    @Override
+    public UpdateProgress process(Channel channel, Elements elements, DateTime startTime) {
+        
+        List<ItemRefAndBroadcast> broadcasts = Lists.newArrayList();
+        Builder<String, String> acceptableBroadcastIds = ImmutableMap.builder();
+        
+        UpdateProgress progress = UpdateProgress.START;
+        try {
+            for (int i = 0; i < elements.size(); i++) {
+                ItemRefAndBroadcast itemAndBroadcast = processor.process(elements.get(i));
+                if (itemAndBroadcast != null) {
+                    broadcasts.add(itemAndBroadcast);
+                    acceptableBroadcastIds.put(itemAndBroadcast.getBroadcast().getSourceId(),itemAndBroadcast.getItemUri());
+                    progress = progress.reduce(UpdateProgress.SUCCESS);
+                } else {
+                    progress = progress.reduce(UpdateProgress.FAILURE);
+                }
+            }
+            if (trimmer != null) {
+                trimmer.trimBroadcasts(new Interval(startTime, startTime.plusDays(1)), channel, acceptableBroadcastIds.build());
+            }
+            scheduleWriter.replaceScheduleBlock(Publisher.YOUVIEW, channel, broadcasts);
+        } catch (Exception e) {
+            //TODO: should we just throw e?
+            log.error(String.format("Error processing channel %s with youview alias %s", channel.key(), getYouViewId(channel)));
+            return UpdateProgress.FAILURE;
+        }
+        
+        return progress;
+    }
+
+    private String getYouViewId(Channel channel) {
+        for (String alias : channel.getAliasUrls()) {
+            if (alias.contains("youview.com")) {
+                return alias;
+            }
+        }
+        return "";
+    }
+}
---
+++ src/main/java/org/atlasapi/remotesite/youview/DefaultYouViewElementProcessor.java
@@ -0,0 +1,58 @@
+package org.atlasapi.remotesite.youview;
+
+import nu.xom.Element;
+
+import org.atlasapi.media.entity.Broadcast;
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Identified;
+import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.ScheduleEntry.ItemRefAndBroadcast;
+import org.atlasapi.media.entity.Version;
+import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.ContentWriter;
+import org.atlasapi.remotesite.ContentMerger;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.internal.Iterables;
+import com.metabroadcast.common.base.Maybe;
+
+
+public class DefaultYouViewElementProcessor implements YouViewElementProcessor {
+
+    private final YouViewContentExtractor extractor;
+    private final ContentResolver resolver;
+    private final ContentWriter writer;
+    
+    public DefaultYouViewElementProcessor(YouViewContentExtractor extractor, ContentResolver resolver, ContentWriter writer) {
+        this.extractor = extractor;
+        this.resolver = resolver;
+        this.writer = writer;
+    }
+    
+    @Override
+    public ItemRefAndBroadcast process(Element element) {
+        Item item = extractor.extract(element);
+        Maybe<Identified> existing = resolve(item.getCanonicalUri());
+        if (existing.isNothing()) {
+            write(item);
+        } else {
+            Identified identified = existing.requireValue();
+                write(ContentMerger.merge(ContentMerger.asItem(identified), item));
+        }
+        return new ItemRefAndBroadcast(item, getBroadcastFromItem(item));
+    }
+
+    private Broadcast getBroadcastFromItem(Item item) {
+        Version version = Iterables.getOnlyElement(item.getVersions());
+        return Iterables.getOnlyElement(version.getBroadcasts());
+    }
+
+    private Maybe<Identified> resolve(String uri) {
+        ImmutableSet<String> uris = ImmutableSet.of(uri);
+        return resolver.findByCanonicalUris(uris).get(uri);
+    }
+
+    private void write(Content content) {
+        writer.createOrUpdate((Item) content);
+    }
+}
