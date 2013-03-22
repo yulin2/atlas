065d8e8e8be5b9edf8d1140a9427fce9ff22fb15
Oli Hall <oli@metabradcast.com>
2013-03-22 11:36:38 +0000

    Refactor the youview ingest to iterate over days, then channels, to allow updating of schedule blocks directly from the ingest job.

--- src/main/java/org/atlasapi/remotesite/youview/YouViewModule.java
+++ src/main/java/org/atlasapi/remotesite/youview/YouViewModule.java
@@ -3,16 +3,24 @@ package org.atlasapi.remotesite.youview;
 import javax.annotation.PostConstruct;
 
 import org.atlasapi.media.channel.ChannelResolver;
+import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
+import org.atlasapi.persistence.content.ScheduleResolver;
+import org.atlasapi.persistence.content.schedule.mongo.ScheduleWriter;
+import org.atlasapi.remotesite.channel4.epg.BroadcastTrimmer;
+import org.atlasapi.remotesite.channel4.epg.ScheduleResolverBroadcastTrimmer;
 import org.joda.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.metabroadcast.common.properties.Configurer;
 import com.metabroadcast.common.scheduling.RepetitionRules;
 import com.metabroadcast.common.scheduling.RepetitionRules.Every;
 import com.metabroadcast.common.scheduling.SimpleScheduler;
 
+@Configuration
 public class YouViewModule {
     private final static Every EVERY_15_MINUTES = RepetitionRules.every(Duration.standardMinutes(15));
     private final static Every EVERY_HOUR = RepetitionRules.every(Duration.standardHours(1));
@@ -21,6 +29,8 @@ public class YouViewModule {
     private @Autowired ChannelResolver channelResolver;
     private @Autowired ContentResolver contentResolver;
     private @Autowired ContentWriter contentWriter;
+    private @Autowired ScheduleWriter scheduleWriter;
+    private @Autowired ScheduleResolver scheduleResolver;
     
     @PostConstruct
     public void startBackgroundTasks() {
@@ -28,26 +38,40 @@ public class YouViewModule {
         scheduler.schedule(youViewFornightUpdater().withName("YouView Updater ±7 Days"), EVERY_HOUR);
     }
 
-    private YouViewFortnightUpdater youViewFornightUpdater() {
-        return new YouViewFortnightUpdater(channelResolver(), youViewFetcher(), youViewXmlElementHandler());
+    @Bean
+    public YouViewFortnightUpdater youViewFornightUpdater() {
+        return new YouViewFortnightUpdater(channelResolver(), youViewFetcher(), youViewChannelProcessor());
     }
-
-    private YouViewTodayUpdater youViewTodayUpdater() {
-        return new YouViewTodayUpdater(channelResolver(), youViewFetcher(), youViewXmlElementHandler());
+        
+    @Bean
+    public YouViewTodayUpdater youViewTodayUpdater() {
+        return new YouViewTodayUpdater(channelResolver(), youViewFetcher(), youViewChannelProcessor());
     }
     
-    private YouViewScheduleFetcher youViewFetcher() {
+    @Bean
+    public YouViewChannelProcessor youViewChannelProcessor() {
+        return new DefaultYouViewChannelProcessor(scheduleWriter, youViewElementProcessor(), broadcastTrimmer());
+    }
+
+    @Bean
+    public YouViewElementProcessor youViewElementProcessor() {
+        YouViewContentExtractor extractor = new YouViewContentExtractor(channelResolver());
+        return new DefaultYouViewElementProcessor(extractor, contentResolver, contentWriter);
+    }
+
+    @Bean
+    public YouViewScheduleFetcher youViewFetcher() {
         String url = Configurer.get("youview.url").get();
         int timeout = Configurer.get("youview.timeout").toInt();
         return new YouViewScheduleFetcher(url, timeout);
     }
-    
-    private YouViewXmlElementHandler youViewXmlElementHandler() {
-        YouViewContentExtractor extractor = new YouViewContentExtractor(channelResolver());
-        return new DefaultYouViewXmlElementHandler(extractor, contentResolver, contentWriter);
-    }
 
-    private YouViewChannelResolver channelResolver() {
+    @Bean
+    public YouViewChannelResolver channelResolver() {
         return new DefaultYouViewChannelResolver(channelResolver);
+    }
+    
+    private BroadcastTrimmer broadcastTrimmer() {
+        return new ScheduleResolverBroadcastTrimmer(Publisher.YOUVIEW, scheduleResolver, contentResolver, contentWriter);
     }
 }
