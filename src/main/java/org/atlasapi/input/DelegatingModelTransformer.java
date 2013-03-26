b04c183ea13d89c260e2c04ab427b84d76b731d2
liamgh <liam@greenhughes.com>
2013-03-26 15:01:01 +0000

    Reformat DelegatingModelTransformer

--- src/main/java/org/atlasapi/input/DelegatingModelTransformer.java
+++ src/main/java/org/atlasapi/input/DelegatingModelTransformer.java
@@ -7,25 +7,25 @@ import org.atlasapi.media.entity.simple.Item;
 import org.atlasapi.media.entity.simple.Playlist;
 
 public class DelegatingModelTransformer implements ModelTransformer<Description, Content> {
-    
+
     private ModelTransformer<Item, org.atlasapi.media.entity.Item> itemTransformer;
     private ModelTransformer<org.atlasapi.media.entity.simple.Playlist, Brand> brandTransformer;
 
-    public DelegatingModelTransformer(ModelTransformer<org.atlasapi.media.entity.simple.Playlist, Brand> brandTransformer, ModelTransformer<Item,org.atlasapi.media.entity.Item> itemTransformer) {
+    public DelegatingModelTransformer(
+            ModelTransformer<org.atlasapi.media.entity.simple.Playlist, Brand> brandTransformer,
+            ModelTransformer<Item, org.atlasapi.media.entity.Item> itemTransformer) {
         this.brandTransformer = brandTransformer;
-    	this.itemTransformer = itemTransformer;
+        this.itemTransformer = itemTransformer;
     }
-    
+
     @Override
     public Content transform(Description simple) {
         if (simple instanceof Item) {
             return itemTransformer.transform((Item) simple);
         } else if (simple instanceof Playlist && simple.getType().equalsIgnoreCase("brand")) {
-        	Brand brand = brandTransformer.transform((Playlist) simple);
-        	return brand;
+            Brand brand = brandTransformer.transform((Playlist) simple);
+            return brand;
         }
         throw new IllegalArgumentException("Can't transform " + simple.getClass());
     }
-
-
 }
