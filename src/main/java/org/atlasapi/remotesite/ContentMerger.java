c6e2ebf38857786cb3d844921159f42587ada649
Oli Hall <oli@metabradcast.com>
2013-04-10 14:46:00 +0100

    Inherit value of activelyPublished from extracted content on merge

--- src/main/java/org/atlasapi/remotesite/ContentMerger.java
+++ src/main/java/org/atlasapi/remotesite/ContentMerger.java
@@ -32,7 +32,7 @@ public class ContentMerger {
     }
 
     private static <C extends Content> C mergeContents(C current, C extracted) {
-        current.setActivelyPublished(true);
+        current.setActivelyPublished(extracted.isActivelyPublished());
         current.setAliasUrls(extracted.getAliasUrls());
         current.setTitle(extracted.getTitle());
         current.setDescription(extracted.getDescription());
