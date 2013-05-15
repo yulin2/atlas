f7aa59faba06060aeee98a0b48f3d9f50596602a
Fred van den Driessche <fred@metabroadcast.com>
2013-05-15 10:04:23 +0100

    Improve Alias handling in Topic bootstrap
    Ensure that Topics are bootstrapped with their namespace and values
    imported as Aliases.
    
    Aliases with 'dbpedia' namespace are replaced with 'uri' namespaces.
    
    Topics with namespace 'magpie' are ignored.

--- src/main/java/org/atlasapi/system/bootstrap/TopicWritingChangeListener.java
+++ src/main/java/org/atlasapi/system/bootstrap/TopicWritingChangeListener.java
@@ -2,13 +2,25 @@ package org.atlasapi.system.bootstrap;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
+import javax.annotation.Nullable;
+
 import org.atlasapi.media.entity.Alias;
 import org.atlasapi.media.topic.Topic;
 import org.atlasapi.media.topic.TopicStore;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+
 public class TopicWritingChangeListener extends AbstractMultiThreadedChangeListener<Topic> {
 
     private final TopicStore topicStore;
+    private Function<Alias, Alias> cleanNamespaces = new Function<Alias, Alias>() {
+        @Override
+        public Alias apply(@Nullable Alias input) {
+            return new Alias(clean(input.getNamespace()), input.getValue());
+        }
+    };
 
     public TopicWritingChangeListener(int concurrencyLevel, TopicStore topicStore) {
         super(concurrencyLevel);
@@ -22,14 +34,27 @@ public class TopicWritingChangeListener extends AbstractMultiThreadedChangeListe
 
     @Override
     protected void onChange(Topic topic) {
-        topic.addAlias(new Alias(namespace(topic), topic.getValue()));
+        if (topic.getNamespace().equals("magpie")) {
+            return;
+        }
+        topic.setAliases(ImmutableSet.<Alias>builder()
+            .add(alias(topic))
+            .addAll(Iterables.transform(topic.getAliases(), cleanNamespaces))
+            .build());
         topicStore.writeTopic(topic);
     }
 
+    private Alias alias(Topic topic) {
+        return new Alias(namespace(topic), topic.getValue());
+    }
+
     private String namespace(Topic topic) {
-        String ns = topic.getNamespace();
-        if (ns.equals("dbpedia") || ns.equals("magpie")) {
-            return "uri";
+        return clean(topic.getNamespace());
+    }
+
+    private String clean(String ns) {
+        if (ns.equals("dbpedia")) {
+            ns = "uri";
         }
         return ns;
     }
