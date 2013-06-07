64987ee4d2ae53772ab472e6634d4d3307460753
Tom McAdam <tom@metabroadcast.com>
2013-06-07 10:06:08 +0100

    Set ID on parent refs

--- src/main/java/org/atlasapi/output/simple/ItemModelSimplifier.java
+++ src/main/java/org/atlasapi/output/simple/ItemModelSimplifier.java
@@ -329,6 +329,8 @@ public class ItemModelSimplifier extends ContentModelSimplifier<Item, org.atlasa
     private BrandSummary summaryFromResolved(ParentRef container, Set<Annotation> annotations) {
         BrandSummary baseSummary = new BrandSummary();
         baseSummary.setUri(container.getUri());
+        Long id = container.getId();
+        baseSummary.setId(id != null ? idCodec.encode(BigInteger.valueOf(id)) : null);
 
         return annotations.contains(Annotation.BRAND_SUMMARY) ? containerSummaryResolver.summarizeTopLevelContainer(container).or(baseSummary) : baseSummary;
     }
