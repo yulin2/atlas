5f5de5377cf678c831e1708a8734a3d15d685f6e
Fred van den Driessche <fred@metabroadcast.com>
2013-06-26 17:27:08 +0100

    filter schedule only content as last step of execution
    Conflicts:
    	src/main/java/org/atlasapi/query/QueryModule.java

---
+++ src/main/java/org/atlasapi/query/content/FilterScheduleOnlyQueryExecutor.java
@@ -0,0 +1,59 @@
+package org.atlasapi.query.content;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import org.atlasapi.content.criteria.ContentQuery;
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Identified;
+import org.atlasapi.persistence.content.query.KnownTypeQueryExecutor;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+
+
+public class FilterScheduleOnlyQueryExecutor implements KnownTypeQueryExecutor {
+    
+    private final KnownTypeQueryExecutor delegate;
+
+    public FilterScheduleOnlyQueryExecutor(KnownTypeQueryExecutor delegate) {
+        this.delegate = checkNotNull(delegate);
+    }
+
+    @Override
+    public Map<String, List<Identified>> executeUriQuery(Iterable<String> uris, ContentQuery query) {
+        return filter(delegate.executeUriQuery(uris, query));
+    }
+    
+    @Override
+    public Map<String, List<Identified>> executeIdQuery(Iterable<Long> ids, ContentQuery query) {
+        return filter(delegate.executeIdQuery(ids, query));
+    }
+    
+    @Override
+    public Map<String, List<Identified>> executeAliasQuery(Optional<String> namespace,
+            Iterable<String> values, ContentQuery query) {
+        return filter(delegate.executeAliasQuery(namespace, values, query));
+    }
+
+    private Map<String, List<Identified>> filter(Map<String, List<Identified>> unfiltered) {
+        return Maps.filterValues(unfiltered, new Predicate<List<Identified>>() {
+            @Override
+            public boolean apply(List<Identified> input) {
+                for (Identified i : input) {
+                    if (i instanceof Content && ((Content)i).isScheduleOnly()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+    
+    
+}
