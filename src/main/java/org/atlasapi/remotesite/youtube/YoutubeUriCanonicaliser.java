c4d34b966aade6c1dcc8b364e61f552abfb12a40
augusto <gutouehara@gmail.com>
2013-07-03 14:57:47 +0100

    made changes acording to pull request

--- src/main/java/org/atlasapi/remotesite/youtube/YoutubeUriCanonicaliser.java
+++ src/main/java/org/atlasapi/remotesite/youtube/YoutubeUriCanonicaliser.java
@@ -14,15 +14,21 @@ permissions and limitations under the License. */
 
 package org.atlasapi.remotesite.youtube;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.atlasapi.query.uri.canonical.Canonicaliser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
 
 public class YoutubeUriCanonicaliser implements Canonicaliser {
+
+    private static final Logger log = LoggerFactory.getLogger(YoutubeUriCanonicaliser.class);
 
 	private static final List<Pattern> alternateUris = Lists.newArrayList(
         Pattern.compile("https?://.*\\.youtube.com.*v=([^\\./&=]+).*"),
@@ -39,30 +45,44 @@ public class YoutubeUriCanonicaliser implements Canonicaliser {
 	    return "http://www.youtube.com/watch?" + videoId;
 	}
 
-	public static String videoIdFrom(String uri) {
-	    if(uri != null){
-    		for (Pattern p : alternateUris) {
-    			Matcher matcher = p.matcher(uri);
-    			if (matcher.matches()) {
-    				return matcher.group(1);
-    			}
-    		}
-    	}
-		return null;
+	public static String videoIdFrom(String uri) throws YouTubeException {
+	    checkNotNull(uri);
+        for (Pattern p : alternateUris) {
+            Matcher matcher = p.matcher(uri);
+            if (matcher.matches()) {
+                return matcher.group(1);
+            }
+        }
+        throw new YouTubeException(String.format("Provided URI %s deosn't match any known YouTube URI patterns", uri));
 	}
 
 	@Override
 	public String canonicalise(String alternate) {
-		String programmeId = videoIdFrom(alternate);
-		if (programmeId == null) {
-			return null;
-		}
-		return canonicalUriFor(programmeId);
+		try {
+		    String programmeId = videoIdFrom(alternate);
+            if (programmeId != null) {
+                return canonicalUriFor(programmeId);
+            }
+        } catch (YouTubeException e) {
+            log.error(e.getMessage(), e);
+        }
+		/**
+		 * This should have been an Optional on the interface.
+		 * Changing this to Optional now means changing way too much into
+		 * the atlas codebase, that is completely unrelated to the YouTube
+		 * Adapter itself, it this is to change, it deserves a task of its own.
+		 */
+		return null;
 	}
 
 	// Curie [ yt:abcd ]
 	public static String curieFor(String uri) {
-		String videoId = videoIdFrom(uri);
+		String videoId = "";
+        try {
+            videoId = videoIdFrom(uri);
+        } catch (YouTubeException e) {
+            YoutubeUriCanonicaliser.log.error(e.getMessage(), e);
+        }
 		return "yt:" + videoId;
 	}
 }
