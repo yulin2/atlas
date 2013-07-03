f7537b8109ed3010747af3942ddfcbd8e7c9b921
Fred van den Driessche <fred@metabroadcast.com>
2013-07-03 13:36:54 +0100

    Add 'scheduled' task for population of person lookup collection

--- src/main/java/org/atlasapi/equiv/ChildRefUpdateModule.java
+++ src/main/java/org/atlasapi/equiv/ChildRefUpdateModule.java
@@ -6,11 +6,14 @@ import org.atlasapi.equiv.update.tasks.ScheduleTaskProgressStore;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.listing.ContentLister;
+import org.atlasapi.persistence.content.people.PersonStore;
+import org.atlasapi.persistence.lookup.mongo.MongoLookupEntryStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.metabroadcast.common.scheduling.RepetitionRule;
 import com.metabroadcast.common.scheduling.RepetitionRules;
 import com.metabroadcast.common.scheduling.SimpleScheduler;
 
@@ -22,11 +25,13 @@ public class ChildRefUpdateModule {
     private @Autowired DatabasedMongo mongo;
     private @Autowired ScheduleTaskProgressStore progressStore;
     private @Autowired SimpleScheduler scheduler;
+    private @Autowired PersonStore personStore;
 
     @PostConstruct
     public void setup() {
         scheduler.schedule(childRefUpdateTask(), RepetitionRules.NEVER);
         scheduler.schedule(personRefUpdateTask(), RepetitionRules.NEVER);
+        scheduler.schedule(personLookupPopulationTask(), RepetitionRules.NEVER);
     }
     
     @Bean
@@ -44,6 +49,12 @@ public class ChildRefUpdateModule {
     public PersonRefUpdateTask personRefUpdateTask() {
         return new PersonRefUpdateTask(lister, mongo, progressStore)
             .forPublishers(Publisher.RADIO_TIMES, Publisher.BBC, Publisher.PA);
+    }
+    
+    @Bean
+    public PersonLookupPopulationTask personLookupPopulationTask() {
+        return new PersonLookupPopulationTask(mongo.collection("people"), 
+                new MongoLookupEntryStore(mongo.collection("peopleLookup")));
     }
     
 }
---
+++ src/main/java/org/atlasapi/equiv/PersonLookupPopulationTask.java
@@ -0,0 +1,48 @@
+package org.atlasapi.equiv;
+
+import org.atlasapi.feeds.utils.UpdateProgress;
+import org.atlasapi.media.entity.Person;
+import org.atlasapi.persistence.lookup.entry.LookupEntry;
+import org.atlasapi.persistence.lookup.mongo.MongoLookupEntryStore;
+import org.atlasapi.persistence.media.entity.PersonTranslator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.metabroadcast.common.scheduling.ScheduledTask;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+
+public class PersonLookupPopulationTask extends ScheduledTask {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonLookupPopulationTask.class);
+    
+    private final DBCollection collection;
+    private final MongoLookupEntryStore lookupEntryStore;
+    private final PersonTranslator translator = new PersonTranslator();
+
+    public PersonLookupPopulationTask(DBCollection collection,
+            MongoLookupEntryStore mongoLookupEntryStore) {
+        this.collection = collection;
+        this.lookupEntryStore = mongoLookupEntryStore;
+    }
+
+    @Override
+    protected void runTask() {
+        DBCursor cursor = collection.find();
+        UpdateProgress progress = UpdateProgress.START;
+        for(DBObject dbo : cursor) {
+            try {
+                Person person = translator.fromDBObject(dbo, null);
+                lookupEntryStore.store(LookupEntry.lookupEntryFrom(person));
+                progress = progress.reduce(UpdateProgress.SUCCESS);
+            } catch (Exception e) {
+                log.error("Problem with a person", e);
+                progress = progress.reduce(UpdateProgress.FAILURE);
+            }
+            reportStatus(progress.toString());
+        }
+    }
+    
+}
