326da2f760e8728a97af43ef3787e462d5039c3c
liamgh <liam@greenhughes.com>
2013-07-05 16:38:35 +0100

    Ad scheduled task

--- src/main/java/org/atlasapi/remotesite/thesun/TheSunModule.java
+++ src/main/java/org/atlasapi/remotesite/thesun/TheSunModule.java
@@ -1,13 +1,21 @@
 package org.atlasapi.remotesite.thesun;
 
+import javax.annotation.PostConstruct;
+
 import nu.xom.Builder;
 import nu.xom.Document;
 
+import org.atlasapi.persistence.content.ContentGroupResolver;
+import org.atlasapi.persistence.content.ContentGroupWriter;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
+import org.atlasapi.persistence.logging.AdapterLog;
+import org.atlasapi.persistence.logging.AdapterLogEntry;
+import org.atlasapi.persistence.logging.AdapterLogEntry.Severity;
 import org.atlasapi.persistence.system.RemoteSiteClient;
 import org.atlasapi.remotesite.HttpClients;
 import org.atlasapi.remotesite.channel4.XmlClient;
+import org.joda.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +23,25 @@ import org.springframework.context.annotation.Configuration;
 
 import com.metabroadcast.common.http.RequestLimitingSimpleHttpClient;
 import com.metabroadcast.common.http.SimpleHttpClient;
+import com.metabroadcast.common.scheduling.RepetitionRules;
+import com.metabroadcast.common.scheduling.SimpleScheduler;
+import com.metabroadcast.common.scheduling.RepetitionRules.Daily;
 
 @Configuration
 public class TheSunModule {
+    private final static Daily ONCE_DAILY = RepetitionRules.daily(new LocalTime(2, 0, 0));
+    private @Autowired SimpleScheduler scheduler;
     private @Autowired @Qualifier("contentResolver") ContentResolver contentResolver;
     private @Autowired @Qualifier("contentWriter") ContentWriter contentWriter;
+    private @Autowired ContentGroupWriter contentGroupWriter;
+    private @Autowired ContentGroupResolver contentGroupResolver;
+    private @Autowired AdapterLog log;
+    
+    @PostConstruct
+    public void startBackgroundTasks() {
+        scheduler.schedule(theSunTvPicksUpdater() , ONCE_DAILY);
+        log.record(new AdapterLogEntry(Severity.INFO).withDescription("The Sun TV Picks scheduled task installed").withSource(getClass()));
+    }
     
     @Bean 
     public RemoteSiteClient<Document> theSunTvPicksClient() {
@@ -29,5 +51,12 @@ public class TheSunModule {
     @Bean 
     protected SimpleHttpClient requestLimitedHttpClient() {
         return new RequestLimitingSimpleHttpClient(HttpClients.webserviceClient(), 4);
+    }
+    
+    @Bean
+    public TheSunTvPicksUpdater theSunTvPicksUpdater() {
+        TheSunTvPicksEntryProcessor entryProcessor = new TheSunTvPicksEntryProcessor(contentWriter, contentResolver, log);
+        TheSunTvPicksContentGroupUpdater groupUpdater = new TheSunTvPicksContentGroupUpdater(contentGroupResolver, contentGroupWriter);
+        return new TheSunTvPicksUpdater(theSunTvPicksClient(), entryProcessor, groupUpdater, log);
     }
 }
