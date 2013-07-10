e826d12ea3f3b2a4eb9ab01ecb16bf32b0fb9470
liamgh <liam@greenhughes.com>
2013-07-10 15:34:50 +0100

    Use Mockito instead of JMock for the Sun updater test

--- src/test/java/org/atlasapi/remotesite/thesun/TheSunTvPicksUpdaterTest.java
+++ src/test/java/org/atlasapi/remotesite/thesun/TheSunTvPicksUpdaterTest.java
@@ -1,5 +1,6 @@
 package org.atlasapi.remotesite.thesun;
 
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -10,7 +11,6 @@ import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.ParsingException;
 import nu.xom.ValidityException;
-import org.atlasapi.media.entity.ContentGroup;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.persistence.content.ContentGroupResolver;
 import org.atlasapi.persistence.content.ContentGroupWriter;
@@ -20,43 +20,41 @@ import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.logging.NullAdapterLog;
 import org.atlasapi.persistence.system.RemoteSiteClient;
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Resources;
 
-@RunWith(JMock.class)
+
 public class TheSunTvPicksUpdaterTest {
     protected static final int ContentGroup = 0;
     private static String FEED_URL = "http://www.thesun.co.uk/sol/homepage/feeds/smartphone/newsection/";
     private static String TEST_CONTENT_ID = "http://pressassociation.com/programmes/abc1234";    
-    private static String CONTENT_GROUP_URI = "http://www.thesun.co.uk/sol/homepage/feeds/smartphone/newsection/";    
-    private final Mockery context = new Mockery();
+    private static String CONTENT_GROUP_URI = "http://www.thesun.co.uk/sol/homepage/feeds/smartphone/newsection/";   
     private final Builder builder = new Builder(new TheSunTvPicksElementFactory());
     private final AdapterLog log = new NullAdapterLog();
-    private final ContentWriter contentWriter = context.mock(ContentWriter.class);
-    private final ContentResolver resolver = context.mock(ContentResolver.class);
-    private final ContentGroupResolver contentGroupResolver = context.mock(ContentGroupResolver.class);
-    private final ContentGroupWriter contentGroupWriter = context.mock(ContentGroupWriter.class);
+    private ContentWriter contentWriter;
+    private ContentResolver resolver;
+    private ContentGroupResolver contentGroupResolver;
+    private ContentGroupWriter contentGroupWriter;
     private ResolvedContent paResolvedContent;
     private ResolvedContent theSunResolvedContent;
     private ResolvedContent groupResolvedContent;
     private TheSunTvPicksEntryProcessor entryProcessor;
     private TheSunTvPicksContentGroupUpdater groupUpdater;
-    
-    @SuppressWarnings("unchecked")
-    private final RemoteSiteClient<Document> rssFetcher = context.mock(RemoteSiteClient.class);
+    private RemoteSiteClient<Document> rssFetcher;
     private Document tvPicksFeed;
     private TheSunTvPicksUpdater updater;
     
+    @SuppressWarnings("unchecked")
     @Before
     public void setUp() throws ValidityException, ParsingException, IOException {
+        contentWriter = mock(ContentWriter.class);
+        resolver = mock(ContentResolver.class);
+        contentGroupResolver = mock(ContentGroupResolver.class);
+        contentGroupWriter = mock(ContentGroupWriter.class);
+        rssFetcher = (RemoteSiteClient<Document>) mock(RemoteSiteClient.class);
         paResolvedContent = generatePaResolvedContent();
         theSunResolvedContent = generateTheSunResolvedContent();
         groupResolvedContent = generateGroupResolvedContent();
@@ -102,19 +100,15 @@ public class TheSunTvPicksUpdaterTest {
     public void testExtraction() throws Exception {
         // Two lookups per item, first when looking for resolved content
         // second when looking for ids for the content group
-        context.checking(new Expectations() {{
-            oneOf(rssFetcher).get(FEED_URL);
-            will(returnValue(tvPicksFeed));
-            atMost(1).of(resolver).findByCanonicalUris(with(ImmutableSet.of(TEST_CONTENT_ID)));
-            will(returnValue(paResolvedContent));
-            atMost(59).of(resolver).findByCanonicalUris(with(any(Iterable.class)));
-            will(returnValue(theSunResolvedContent));
-            atMost(2).of(contentGroupResolver).findByCanonicalUris(ImmutableList.of(CONTENT_GROUP_URI));
-            will(returnValue(groupResolvedContent));
-            allowing(contentWriter).createOrUpdate(with(any(Item.class)));
-            oneOf(contentGroupWriter).createOrUpdate(with(any(ContentGroup.class)));
-        }});
+        when(rssFetcher.get(FEED_URL)).thenReturn(tvPicksFeed);
+        when(resolver.findByCanonicalUris(ImmutableSet.of(TEST_CONTENT_ID))).thenReturn(paResolvedContent);
+        when(resolver.findByCanonicalUris(ImmutableSet.of(anyString()))).thenReturn(theSunResolvedContent);
+        when(contentGroupResolver.findByCanonicalUris(ImmutableList.of(CONTENT_GROUP_URI))).thenReturn(groupResolvedContent);
         updater.run();
+        verify(rssFetcher, times(1)).get(FEED_URL);
+        verify(resolver).findByCanonicalUris(ImmutableSet.of(TEST_CONTENT_ID));
+        verify(resolver, times(10)).findByCanonicalUris(ImmutableSet.of(anyString()));
+        verify(contentGroupResolver, times(1)).findByCanonicalUris(ImmutableList.of(CONTENT_GROUP_URI));
         assertEquals(updater.getNumberOfItemsProcessed(), 5);
         assertEquals(updater.getGroupSize(), 5);
     }
