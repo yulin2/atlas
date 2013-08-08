3ebf3afa346fe6a83f1699003bb0b86f88748e07
Tom McAdam <tom@metabroadcast.com>
2013-08-08 15:12:20 +0100

    Add monitoring health page
    A new Jetty server now runs on port 8081 and exposes a health page at
    /system/health with the number of connections opened in the last
    minute. The port can be overridden using the monitoring.port server
    argument.

--- src/main/java/org/atlasapi/AtlasMain.java
+++ src/main/java/org/atlasapi/AtlasMain.java
@@ -8,26 +8,43 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.joda.time.Duration;
+
+import com.metabroadcast.common.caching.BackgroundTask;
 
 public class AtlasMain {
 
 	private static final boolean IS_PROCESSING = Boolean.parseBoolean(System.getProperty("processing.config"));
 	
     private static final String LOCAL_WAR_DIR = "./src/main/webapp";
-
+    private static SelectChannelConnector CONNECTOR;
+    
 	public static void main(String[] args) throws Exception {
-		
-		WebAppContext ctx = new WebAppContext(warBase(), "/");
-		
-		if(IS_PROCESSING) {
-		    System.out.println(">>> Launching processing configuration");
-		}
+	    if(IS_PROCESSING) {
+            System.out.println(">>> Launching processing configuration");
+        }
 
-		Server server = createServer();
-		server.setHandler(ctx);
+	    createWebApp(warBase() + "/WEB-INF/web.xml", createApiServer());
+	    createWebApp(warBase() + "/WEB-INF/web-monitoring.xml", createMonitoringServer());
 		
-		server.start();
-		server.join();
+	    new BackgroundTask(Duration.standardMinutes(1), new Runnable() {
+	        
+	        @Override
+	        public void run() {
+	            if (CONNECTOR != null) {
+	                CONNECTOR.statsReset();
+	            }
+	        }
+	    }).start();
+	    
+	}
+	
+	private static void createWebApp(String descriptor, final Server server) throws Exception {
+	    WebAppContext ctx = new WebAppContext(warBase(), "/");
+        ctx.setDescriptor(descriptor);
+        
+        server.setHandler(ctx);
+        server.start();
 	}
 	
 	private static String warBase() {
@@ -38,37 +55,67 @@ public class AtlasMain {
         return domain.getCodeSource().getLocation().toString();
 	}
 
-	private static Server createServer() {
-		Server server = new Server();
-		
-		final SelectChannelConnector connector = new SelectChannelConnector();
-		
-		int port = defaultPort();
-		
-		String customPort = System.getProperty("server.port");
+	private static Server createApiServer() throws Exception {
+	    int port = defaultPort();
+        
+        String customPort = System.getProperty("server.port");
         if (customPort != null) {
             port = Integer.parseInt(customPort);
         }
-		
-		connector.setPort(port);
-		
-		connector.setAcceptQueueSize(200);
-		
-		QueuedThreadPool pool = new QueuedThreadPool(100);
-		pool.setName("jetty-request-thread");
-		connector.setThreadPool(pool);
-		
-		// one acceptor per CPU (ish)
-		connector.setAcceptors(4);
-		
-		connector.setRequestBufferSize(1024);
-		connector.setResponseHeaderSize(1024);
-		
-		server.setConnectors(new Connector[] { connector });
+        
+        int requestThreads;
+        String requestThreadsString = System.getProperty("request.threads");
+        if (requestThreadsString == null) {
+            requestThreads = 100;
+        } else {
+            requestThreads = Integer.parseInt(requestThreadsString);
+        }
+		Server server = createServer(port, requestThreads, 200, "api-request-thread");
+		CONNECTOR = (SelectChannelConnector) server.getConnectors()[0];
 		return server;
 	}
+	
+	private static Server createMonitoringServer() throws Exception {
+	    int port = 8081;
+        
+        String customPort = System.getProperty("monitoring.port");
+        if (customPort != null) {
+            port = Integer.parseInt(customPort);
+        }
+        
+        return createServer(port, 10, 20, "monitoring-request-thread");
+    }
+	
+	private static Server createServer(int port, int maxThreads, int acceptQueueSize, String threadNamePrefix) {
+	    Server server = new Server();
+        
+	    SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setStatsOn(true);
+        
+        connector.setPort(port);
+        connector.setAcceptQueueSize(acceptQueueSize);
+        
+        QueuedThreadPool pool = new QueuedThreadPool(maxThreads);
+        pool.setName(threadNamePrefix);
+        connector.setThreadPool(pool);
+        
+        // one acceptor per CPU (ish)
+        connector.setAcceptors(4);
+        
+        connector.setRequestBufferSize(1024);
+        connector.setResponseHeaderSize(1024);
+        
+        server.setConnectors(new Connector[] { connector });
+        
+        return server;
+	}
+	
 
     private static int defaultPort() {
         return IS_PROCESSING ? 8282 : 8080;
+    }
+    
+    public static int numberOfConnectionsInLastMinute() {
+        return CONNECTOR.getConnectionsOpen();
     }
 }
