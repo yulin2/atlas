2c813fa88d7446b22aade9851aeab4d370f0fc43
Tom McAdam <tom@metabroadcast.com>
2013-08-13 18:09:11 +0100

    Push connector into context attributes so it can be fished out elsewhere.

--- src/main/java/org/atlasapi/system/JettyHealthProbe.java
+++ src/main/java/org/atlasapi/system/JettyHealthProbe.java
@@ -1,17 +1,23 @@
 package org.atlasapi.system;
 
-import org.atlasapi.AtlasMain;
+import javax.servlet.ServletContext;
+
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.springframework.web.context.ServletContextAware;
 
 import com.metabroadcast.common.health.HealthProbe;
 import com.metabroadcast.common.health.ProbeResult;
 
 
-public class JettyHealthProbe implements HealthProbe {
+public class JettyHealthProbe implements HealthProbe, ServletContextAware {
+
+    private ServletContext servletContext;
 
     @Override
     public ProbeResult probe() throws Exception {
         ProbeResult probeResult = new ProbeResult("Requests");
-        probeResult.add("request-count", String.valueOf(AtlasMain.numberOfConnectionsInLastMinute()), true);
+        SelectChannelConnector connector = (SelectChannelConnector) servletContext.getAttribute("CONNECTOR");
+        probeResult.add("request-count", String.valueOf(connector.getConnectionsOpen()), true);
         return probeResult;
     }
 
@@ -23,6 +29,13 @@ public class JettyHealthProbe implements HealthProbe {
     @Override
     public String slug() {
         return "jetty";
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+        // TODO Auto-generated method stub
+        
     }
 
 }
