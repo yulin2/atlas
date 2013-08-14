ca1f676a253b9b55db3ae48a755aeed6dce8d923
Tom McAdam <tom@metabroadcast.com>
2013-08-14 09:58:59 +0100

    Refactor AtlasMain to instantiate an instance which can be sorted in the webapp context for access to the number of connections metric through health probes loaded with a different classloader.
    A utility static method is provided which performs the required
    reflective calls to access methods on a supplied AtlasMain, since it will
    be called from a class loaded through a different classloader, so that
    classloader's AtlasMain would differ from the AtlasMain stored in the
    context.

--- src/main/java/org/atlasapi/AtlasMain.java
+++ src/main/java/org/atlasapi/AtlasMain.java
@@ -1,6 +1,7 @@
 package org.atlasapi;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.security.ProtectionDomain;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -14,33 +15,37 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class AtlasMain implements Runnable {
 
+    public static final String CONTEXT_ATTRIBUTE = "ATLAS_MAIN";
+    private static final String LOCAL_WAR_DIR = "./src/main/webapp";
+    
     private static final ScheduledExecutorService CONNECTOR_RESET_THREAD_SERVICE = new ScheduledThreadPoolExecutor(1);
 	private static final boolean IS_PROCESSING = Boolean.parseBoolean(System.getProperty("processing.config"));
 	
-    private static final String LOCAL_WAR_DIR = "./src/main/webapp";
-    private static volatile SelectChannelConnector API_CONNECTOR;
-    
-    @Override
-    public void run() {
-        if (API_CONNECTOR != null) {
-            API_CONNECTOR.statsReset();
-        }
-    }
+    private SelectChannelConnector apiConnector;
     
 	public static void main(String[] args) throws Exception {
 	    if(IS_PROCESSING) {
             System.out.println(">>> Launching processing configuration");
         }
-	    
-	    AtlasMain main = new AtlasMain();
-	    
-	    main.createWebApp(main.warBase() + "/WEB-INF/web.xml", main.createApiServer());
-	    WebAppContext monitoringContext = main.createWebApp(main.warBase() + "/WEB-INF/web-monitoring.xml", main.createMonitoringServer());
-	    monitoringContext.setAttribute("CONNECTOR", API_CONNECTOR);
-	    CONNECTOR_RESET_THREAD_SERVICE.scheduleAtFixedRate(main, 1, 1, TimeUnit.MINUTES);
-	    
+	    new AtlasMain().start();
 	}
 	
+	public void start() throws Exception {
+	    WebAppContext apiContext = createWebApp(warBase() + "/WEB-INF/web.xml", createApiServer());
+        WebAppContext monitoringContext = createWebApp(warBase() + "/WEB-INF/web-monitoring.xml", createMonitoringServer());
+        monitoringContext.setAttribute(CONTEXT_ATTRIBUTE, this);
+        apiContext.setAttribute(CONTEXT_ATTRIBUTE, this);
+        
+        CONNECTOR_RESET_THREAD_SERVICE.scheduleAtFixedRate(this, 1, 1, TimeUnit.MINUTES);
+	}
+	
+	@Override
+    public void run() {
+        if (apiConnector != null) {
+            apiConnector.statsReset();
+        }
+    }
+	    
 	private WebAppContext createWebApp(String descriptor, final Server server) throws Exception {
 	    WebAppContext ctx = new WebAppContext(warBase(), "/");
         ctx.setDescriptor(descriptor);
@@ -58,13 +63,6 @@ public class AtlasMain implements Runnable {
 	}
 
 	private Server createApiServer() throws Exception {
-	    int port = defaultPort();
-        
-        String customPort = System.getProperty("server.port");
-        if (customPort != null) {
-            port = Integer.parseInt(customPort);
-        }
-        
         int requestThreads;
         String requestThreadsString = System.getProperty("request.threads");
         if (requestThreadsString == null) {
@@ -72,27 +70,29 @@ public class AtlasMain implements Runnable {
         } else {
             requestThreads = Integer.parseInt(requestThreadsString);
         }
-		Server server = createServer(port, requestThreads, 200, "api-request-thread");
-		API_CONNECTOR = (SelectChannelConnector) server.getConnectors()[0];
+
+		Server server = createServer("server.port", defaultPort(), requestThreads, 200, 
+		        Runtime.getRuntime().availableProcessors(), "api-request-thread");
+		apiConnector = (SelectChannelConnector) server.getConnectors()[0];
 		return server;
 	}
 	
 	private Server createMonitoringServer() throws Exception {
-	    int port = 8081;
-        
-        String customPort = System.getProperty("monitoring.port");
-        if (customPort != null) {
-            port = Integer.parseInt(customPort);
-        }
-        
-        return createServer(port, 10, 20, "monitoring-request-thread");
+        return createServer("monitoring.port", 8081, 10, 20, 1, "monitoring-request-thread");
     }
 	
-	private Server createServer(int port, int maxThreads, int acceptQueueSize, String threadNamePrefix) {
+	private Server createServer(String portProperty, int defaultPort, int maxThreads, int acceptors, 
+	        int acceptQueueSize, String threadNamePrefix) {
 	    Server server = new Server();
         
 	    SelectChannelConnector connector = new SelectChannelConnector();
         connector.setStatsOn(true);
+        
+        int port = defaultPort;
+        String customPort = System.getProperty(portProperty);
+        if (customPort != null) {
+            port = Integer.parseInt(customPort);
+        }
         
         connector.setPort(port);
         connector.setAcceptQueueSize(acceptQueueSize);
@@ -101,8 +101,7 @@ public class AtlasMain implements Runnable {
         pool.setName(threadNamePrefix);
         connector.setThreadPool(pool);
         
-        // one acceptor per CPU (ish)
-        connector.setAcceptors(4);
+        connector.setAcceptors(acceptors);
         
         connector.setRequestBufferSize(1024);
         connector.setResponseHeaderSize(1024);
@@ -112,14 +111,25 @@ public class AtlasMain implements Runnable {
         return server;
 	}
 	
-
     private int defaultPort() {
         return IS_PROCESSING ? 8282 : 8080;
     }
     
-    public static int numberOfConnectionsInLastMinute() {
-        return API_CONNECTOR.getConnectionsOpen();
+    public int getNumberOfConnectionsMax() {
+        return apiConnector.getConnectionsOpenMax();
     }
 
-    
+    public static int getMaxNumberOfOpenConnectionsInLastMinute(Object atlasMain) throws IllegalArgumentException, 
+                            SecurityException, IllegalAccessException, InvocationTargetException {
+        Class<? extends Object> clazz = atlasMain.getClass();
+        if (clazz.getCanonicalName() != AtlasMain.class.getCanonicalName()) {
+            throw new IllegalArgumentException("Parameter must be instance of " + AtlasMain.class.getCanonicalName());
+        }
+        
+        try {
+            return (Integer) clazz.getDeclaredMethod("getNumberOfConnectionsMax").invoke(atlasMain);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException("There appears to be a mismatch between AtlasMain objects", e);
+        } 
+    }
 }
