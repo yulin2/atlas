1e47a553868154b2973cbc191b2b58b4d7b14364
liamgh <liam@greenhughes.com>
2013-09-11 11:18:54 +0100

    Add container methods for sources endpoint

--- src/main/java/org/atlasapi/application/SourcesController.java
+++ src/main/java/org/atlasapi/application/SourcesController.java
@@ -1,15 +1,40 @@
 package org.atlasapi.application;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 public class SourcesController {
-//    POST /4.0/sources/:sourceId/applications
-//    Updates permission for a source. Post with app id and permission (read/write) required. Replaces POST /admin/applications/{appSlug}/publishers/enabled.
-//    Params:  "id": "abc", "permission": "read"
-
-//    DELETE /4.0/sources/:sourceId/applications
-//    Removes a permission (read/write) from an app on a source. Post with app id and permission needed. Replaces DELETE /admin/applications/{appSlug}/publishers/enabled/{pubKey}.
-
-//    POST /4.0/sources/:sourceId/applications/readers/:appId/state
-//    Changes state of app for source, e.g. "available", "requested". Requires admin privs to change from requested to available. Replaces /admin/applications/{appSlug}/publishers/requested and /admin/sources/{id}/applications/approved.
-//    Params:  "state": "available"
+    /**
+     * POST /4.0/sources/:sourceId/applications
+     * Updates permission for a source. Post with app id and permission (read/write) required
+     * Params:  "id": "abc", "permission": "read"
+     * @param request
+     * @param response
+     */  
+    @RequestMapping(value ="/4.0/sources/{sid}/applications", method = RequestMethod.POST)
+    public void writeSourceForApplication(HttpServletRequest request, HttpServletResponse response) {
+        
+    }
+    
+    /**
+     * DELETE /4.0/sources/:sourceId/applications
+     * Removes a permission (read/write) from an app on a source. Post with app id and permission needed.
+     */
+    @RequestMapping(value ="/4.0/sources/{sid}/applications", method = RequestMethod.DELETE)
+    public void deleteSourceForApplication(HttpServletRequest request, HttpServletResponse response) {
+        
+    }
+    
+    /**
+     * POST /4.0/sources/:sourceId/applications/readers/:appId/state
+     * Changes state of app for source, e.g. "available", "requested". 
+     * Params:  "state": "available"
+     */
+    public void changeSourceStateForApplication(HttpServletRequest request, HttpServletResponse response) {
+        
+    }
 }
