3f14dca0379e74ecbb8b9c8496cc4431421d9f93
Fred van den Driessche <fred@metabroadcast.com>
2013-09-20 19:05:12 +0100

    Conditionally full-fetch Nitro items

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/ContentUpdatingNitroBroadcastHandler.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/ContentUpdatingNitroBroadcastHandler.java
@@ -1,30 +1,23 @@
 package org.atlasapi.remotesite.bbc.nitro;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkState;
 
 import org.atlasapi.media.entity.Brand;
 import org.atlasapi.media.entity.Broadcast;
-import org.atlasapi.media.entity.Content;
-import org.atlasapi.media.entity.Episode;
-import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Item;
-import org.atlasapi.media.entity.ParentRef;
 import org.atlasapi.media.entity.ScheduleEntry.ItemRefAndBroadcast;
 import org.atlasapi.media.entity.Series;
 import org.atlasapi.media.entity.Version;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
-import org.atlasapi.remotesite.FetchException;
 import org.atlasapi.remotesite.bbc.BbcFeeds;
 import org.atlasapi.remotesite.bbc.nitro.extract.NitroBroadcastExtractor;
+import org.atlasapi.remotesite.bbc.nitro.extract.NitroUtil;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Optional;
-import com.google.common.base.Supplier;
-import com.google.common.collect.ImmutableList;
 import com.metabroadcast.atlas.glycerin.model.PidReference;
-import com.metabroadcast.common.base.Maybe;
+import com.metabroadcast.common.time.Clock;
 
 /**
  * {@link NitroBroadcastHandler} which fetches, updates and writes relevant
@@ -32,25 +25,23 @@ import com.metabroadcast.common.base.Maybe;
  */
 public class ContentUpdatingNitroBroadcastHandler implements NitroBroadcastHandler<ItemRefAndBroadcast> {
 
-    private final ContentResolver resolver;
     private final ContentWriter writer;
-    private final NitroContentAdapter contentAdapter;
+    private final LocalOrRemoteNitroFetcher localOrRemoteFetcher;
     
     private final NitroBroadcastExtractor broadcastExtractor
         = new NitroBroadcastExtractor();
 
-    public ContentUpdatingNitroBroadcastHandler(ContentResolver resolver, ContentWriter writer, NitroContentAdapter contentAdapter) {
-        this.resolver = resolver;
+    public ContentUpdatingNitroBroadcastHandler(ContentResolver resolver, ContentWriter writer, NitroContentAdapter contentAdapter, Clock clock) {
         this.writer = writer;
-        this.contentAdapter = contentAdapter;
+        this.localOrRemoteFetcher = new LocalOrRemoteNitroFetcher(resolver, contentAdapter, clock);
     }
     
     @Override
     public ItemRefAndBroadcast handle(com.metabroadcast.atlas.glycerin.model.Broadcast nitroBroadcast) throws NitroException {
         
-        Item item = resolveOrFetchItem(nitroBroadcast);
-        Optional<Series> series = resolveOrFetchSeries(item);
-        Optional<Brand> brand = resolveOrFetchBrand(item);
+        Item item = localOrRemoteFetcher.resolveOrFetchItem(nitroBroadcast);
+        Optional<Series> series = localOrRemoteFetcher.resolveOrFetchSeries(item);
+        Optional<Brand> brand = localOrRemoteFetcher.resolveOrFetchBrand(item);
         
         Broadcast broadcast = broadcastExtractor.extract(nitroBroadcast);
         addBroadcast(item, versionUri(nitroBroadcast), broadcast);
@@ -85,106 +76,12 @@ public class ContentUpdatingNitroBroadcastHandler implements NitroBroadcastHandl
         version.setCanonicalUri(versionUri);
         return version;
     }
-
-    private Item resolveOrFetchItem(com.metabroadcast.atlas.glycerin.model.Broadcast nitroBroadcast) throws NitroException {
-        final PidReference pidRef = pidRefForType(nitroBroadcast, "episode");
-        checkArgument(pidRef != null,"Broadcast %s has no episode ref", nitroBroadcast.getPid());
-        
-        Optional<Item> local = resolveLocally(BbcFeeds.nitroUriForPid(pidRef.getPid()), Item.class);
-        try {
-            return local.or(new Supplier<Item>() {
-                @Override
-                public Item get() {
-                    try {
-                        return contentAdapter.fetchEpisode(pidRef);
-                    } catch (NitroException e) {
-                        throw new FetchException(pidRef.getPid(), e);
-                    }
-                }
-            });
-        } catch (FetchException e) {
-            throw (NitroException) e.getCause();
-        }
-    }
-
-    private Optional<Series> resolveOrFetchSeries(Item item) {
-        if (item instanceof Episode) {
-            ParentRef seriesRef = ((Episode)item).getSeriesRef();
-            if (seriesRef != null) {
-                final String seriesUri = seriesRef.getUri();
-                return Optional.of(resolveLocally(seriesUri, Series.class).or(new Supplier<Series>() {
-                    @Override
-                    public Series get() {
-                        PidReference pidRef = new PidReference();
-                        pidRef.setPid(BbcFeeds.pidFrom(seriesUri));
-                        pidRef.setResultType("series");
-                        try {
-                            return contentAdapter.fetchSeries(pidRef);
-                        } catch (NitroException e) {
-                            throw new FetchException(pidRef.getPid(), e);
-                        }
-                    }
-                }));
-            }
-        }
-        return Optional.absent();
-    }
-
-    private Optional<Brand> resolveOrFetchBrand(Item item) {
-        ParentRef containerRef = item.getContainer();
-        if (containerRef != null && !inTopLevelSeries(item)) {
-            final String containerUri = containerRef.getUri();
-            return Optional.of(resolveLocally(containerUri, Brand.class).or(new Supplier<Brand>() {
-                @Override
-                public Brand get() {
-                    PidReference pidRef = new PidReference();
-                    pidRef.setPid(BbcFeeds.pidFrom(containerUri));
-                    pidRef.setResultType("brand");
-                    try {
-                        return contentAdapter.fetchBrand(pidRef);
-                    } catch (NitroException e) {
-                        throw new FetchException(pidRef.getPid(), e);
-                    }
-                }
-            }));
-        }
-        return Optional.absent();
-    }
-
-
-    private boolean inTopLevelSeries(Item item) {
-        if (item instanceof Episode) {
-            Episode ep = (Episode)item;
-            return ep.getSeriesRef() != null 
-                && ep.getContainer().equals(ep.getSeriesRef());
-        }
-        return false;
-    }
-
-    private <T extends Content> Optional<T> resolveLocally(String uri, Class<T> expectedType) {
-        Maybe<Identified> possible = resolver.findByCanonicalUris(ImmutableList.of(uri)).get(uri);
-        if (possible.hasValue()) {
-            Identified ided = possible.requireValue();
-            checkState(expectedType.isAssignableFrom(ided.getClass()),  "%s %s not %s", 
-                    ided.getClass().getSimpleName(), uri, expectedType.getSimpleName());
-            return Optional.of(expectedType.cast(ided));
-        }
-        return Optional.absent();
-    }
     
     private String versionUri(com.metabroadcast.atlas.glycerin.model.Broadcast nitroBroadcast) {
-        PidReference pidRef = pidRefForType(nitroBroadcast, "version");
+        PidReference pidRef = NitroUtil.versionPid(nitroBroadcast);
         checkArgument(pidRef != null,"Broadcast %s has no version ref", nitroBroadcast.getPid());
         return BbcFeeds.nitroUriForPid(pidRef.getPid());
     }
 
-    private PidReference pidRefForType(com.metabroadcast.atlas.glycerin.model.Broadcast nitroBroadcast,
-            String type) {
-        for (PidReference pidRef : nitroBroadcast.getBroadcastOf()) {
-            if (type.equals(pidRef.getResultType())) {
-                return pidRef;
-            }
-        }
-        return null;
-    }
+
 }
---
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/LocalOrRemoteNitroFetcher.java
@@ -0,0 +1,158 @@
+package org.atlasapi.remotesite.bbc.nitro;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+import org.atlasapi.media.entity.Brand;
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Episode;
+import org.atlasapi.media.entity.Identified;
+import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.MediaType;
+import org.atlasapi.media.entity.ParentRef;
+import org.atlasapi.media.entity.Series;
+import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.remotesite.ContentMerger;
+import org.atlasapi.remotesite.FetchException;
+import org.atlasapi.remotesite.bbc.BbcFeeds;
+import org.atlasapi.remotesite.bbc.ion.BbcIonMediaTypeMapping;
+import org.atlasapi.remotesite.bbc.nitro.extract.NitroUtil;
+import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableList;
+import com.metabroadcast.atlas.glycerin.model.Broadcast;
+import com.metabroadcast.atlas.glycerin.model.PidReference;
+import com.metabroadcast.common.base.Maybe;
+import com.metabroadcast.common.time.Clock;
+
+
+public class LocalOrRemoteNitroFetcher {
+    
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    private final ContentResolver resolver;
+    private final NitroContentAdapter contentAdapter;
+    private final Clock clock;
+
+    public LocalOrRemoteNitroFetcher(ContentResolver resolver, NitroContentAdapter contentAdapter, Clock clock) {
+        this.resolver = resolver;
+        this.contentAdapter = contentAdapter;
+        this.clock = clock;
+    }
+    
+    public Item resolveOrFetchItem(Broadcast broadcast) throws NitroException {
+
+        final PidReference pidRef = NitroUtil.programmePid(broadcast);
+        checkArgument(pidRef != null, "Broadcast %s has no episode ref", broadcast.getPid());
+        
+        Optional<Item> local = resolveLocally(BbcFeeds.nitroUriForPid(pidRef.getPid()), Item.class);
+        
+        Item remote = null;
+        if (fullFetchPermitted(broadcast) || !local.isPresent()) {
+            try {
+                remote = contentAdapter.fetchEpisode(pidRef);
+            } catch (NitroException e) {
+                log.warn("Failed to fetch " + pidRef.getPid(), e);
+            }
+        }
+        
+        if (!local.isPresent() && remote == null) {
+            throw new NitroException("Failed to resolve or fetch " + pidRef.getPid());
+        } 
+
+        if (local.isPresent() && remote != null) {
+            return ContentMerger.merge(local.get(), remote);
+        } else {
+            return Objects.firstNonNull(remote, local.get());
+        }
+        
+    }
+    
+    protected boolean fullFetchPermitted(Broadcast broadcast) {
+        LocalDate today = clock.now().toLocalDate();
+        LocalDate broadcastDay = NitroUtil.toDateTime(broadcast.getPublishedTime().getStart()).toLocalDate();
+        
+        Maybe<MediaType> mediaType = BbcIonMediaTypeMapping.mediaTypeForService(broadcast.getService().getSid());
+        
+        // today -4 for radio, today ± 2 for tv
+        // radio are more likely to publish clips after a show has been broadcast
+        // so with a limited ingest window it is more important to go back as far as possible for radio
+        // to ensure that clips are not missed
+        if (mediaType.hasValue() && mediaType.requireValue().equals("audio")) {
+            return today.plusDays(1).isAfter(broadcastDay) && today.minusDays(5).isBefore(broadcastDay);    
+        } else {
+            return today.plusDays(3).isAfter(broadcastDay) && today.minusDays(3).isBefore(broadcastDay);
+        }
+    }
+    
+    
+    public Optional<Series> resolveOrFetchSeries(Item item) {
+        if (item instanceof Episode) {
+            ParentRef seriesRef = ((Episode)item).getSeriesRef();
+            if (seriesRef != null) {
+                final String seriesUri = seriesRef.getUri();
+                return Optional.of(resolveLocally(seriesUri, Series.class).or(new Supplier<Series>() {
+                    @Override
+                    public Series get() {
+                        PidReference pidRef = new PidReference();
+                        pidRef.setPid(BbcFeeds.pidFrom(seriesUri));
+                        pidRef.setResultType("series");
+                        try {
+                            return contentAdapter.fetchSeries(pidRef);
+                        } catch (NitroException e) {
+                            throw new FetchException(pidRef.getPid(), e);
+                        }
+                    }
+                }));
+            }
+        }
+        return Optional.absent();
+    }
+    
+    public Optional<Brand> resolveOrFetchBrand(Item item) {
+        ParentRef containerRef = item.getContainer();
+        if (containerRef != null && !inTopLevelSeries(item)) {
+            final String containerUri = containerRef.getUri();
+            return Optional.of(resolveLocally(containerUri, Brand.class).or(new Supplier<Brand>() {
+                @Override
+                public Brand get() {
+                    PidReference pidRef = new PidReference();
+                    pidRef.setPid(BbcFeeds.pidFrom(containerUri));
+                    pidRef.setResultType("brand");
+                    try {
+                        return contentAdapter.fetchBrand(pidRef);
+                    } catch (NitroException e) {
+                        throw new FetchException(pidRef.getPid(), e);
+                    }
+                }
+            }));
+        }
+        return Optional.absent();
+    }
+    
+    private boolean inTopLevelSeries(Item item) {
+        if (item instanceof Episode) {
+            Episode ep = (Episode)item;
+            return ep.getSeriesRef() != null 
+                && ep.getContainer().equals(ep.getSeriesRef());
+        }
+        return false;
+    }
+    
+    private <T extends Content> Optional<T> resolveLocally(String uri, Class<T> expectedType) {
+        Maybe<Identified> possible = resolver.findByCanonicalUris(ImmutableList.of(uri)).get(uri);
+        if (possible.hasValue()) {
+            Identified ided = possible.requireValue();
+            checkState(expectedType.isAssignableFrom(ided.getClass()),  "%s %s not %s", 
+                    ided.getClass().getSimpleName(), uri, expectedType.getSimpleName());
+            return Optional.of(expectedType.cast(ided));
+        }
+        return Optional.absent();
+    }
+    
+}
