a379d36bfb6508404c2a43cbd28f9cd1eb2c28d0
Tom McAdam <tom@metabroadcast.com>
2013-10-01 15:19:11 +0100

    Revert "Switch to a KnownTypeQueryExecutor, since it will allow alias lookups"
    This reverts commit 5fb92968e7e61b59be43690e587dc2dc09994f97.

--- src/main/java/org/atlasapi/remotesite/pa/features/PaFeaturesProcessor.java
+++ src/main/java/org/atlasapi/remotesite/pa/features/PaFeaturesProcessor.java
@@ -2,10 +2,8 @@ package org.atlasapi.remotesite.pa.features;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
-import org.atlasapi.content.criteria.ContentQuery;
 import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.media.entity.ChildRef;
 import org.atlasapi.media.entity.Content;
@@ -19,7 +17,6 @@ import org.atlasapi.persistence.content.ContentGroupResolver;
 import org.atlasapi.persistence.content.ContentGroupWriter;
 import org.atlasapi.persistence.content.EquivalentContentResolver;
 import org.atlasapi.persistence.content.ResolvedContent;
-import org.atlasapi.persistence.content.query.KnownTypeQueryExecutor;
 import org.atlasapi.remotesite.pa.PaHelper;
 import org.joda.time.Interval;
 import org.slf4j.Logger;
@@ -39,7 +36,7 @@ public class PaFeaturesProcessor {
     private static final String ALL_CONTENT_GROUP_URI = "http://pressassocation.com/features/tvpicks/all";
     private static final Ordering<Broadcast> BY_BROADCAST_DATE = Ordering.natural().onResultOf(Broadcast.TO_TRANSMISSION_TIME);
     
-    private final KnownTypeQueryExecutor queryExecutor;
+    private final EquivalentContentResolver contentResolver;
     private final ContentGroupWriter contentGroupWriter;
     private final ContentGroupResolver contentGroupResolver;
 
@@ -47,8 +44,8 @@ public class PaFeaturesProcessor {
     private ContentGroup todayContentGroup;
     private ContentGroup allFeaturedContentEverContentGroup;
     
-    public PaFeaturesProcessor(KnownTypeQueryExecutor queryExecutor, ContentGroupResolver contentGroupResolver, ContentGroupWriter contentGroupWriter) {
-        this.queryExecutor = queryExecutor;
+    public PaFeaturesProcessor(EquivalentContentResolver contentResolver, ContentGroupResolver contentGroupResolver, ContentGroupWriter contentGroupWriter) {
+        this.contentResolver = contentResolver;
         this.contentGroupWriter = contentGroupWriter;
         this.contentGroupResolver = contentGroupResolver;
     }
@@ -74,7 +71,10 @@ public class PaFeaturesProcessor {
         Set<String> candidateUris = ImmutableSet.of(PaHelper.getFilmUri(programmeId), 
                 PaHelper.getEpisodeUri(programmeId), PaHelper.getAlias(programmeId));
         log.trace("Looking up URIs {}", candidateUris);
-        List<Identified> resolved = Lists.newArrayList(Iterables.concat(queryExecutor.executeUriQuery(candidateUris, ContentQuery.MATCHES_EVERYTHING).values()));
+        
+        ArrayList<Content> resolved = Lists.newArrayList(contentResolver.resolveUris(candidateUris, 
+                ImmutableSet.of(Publisher.PA), ImmutableSet.<Annotation>of(), true).values());
+        
         log.trace("Resolved {}", Iterables.transform(resolved, Identified.TO_URI));
         
         Collections.sort(resolved, new PaIdentifiedComparator());
