69808c39075c64b39a038dfaf4d3bcfc357648da
Fred van den Driessche <fred@metabroadcast.com>
2013-10-09 19:15:23 +0100

    refactor C4 item extractors to be more templatey

--- src/main/java/org/atlasapi/remotesite/channel4/C4EpgEpisodeExtractor.java
+++ src/main/java/org/atlasapi/remotesite/channel4/C4EpgEpisodeExtractor.java
@@ -6,7 +6,6 @@ import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.media.entity.Episode;
 import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Version;
-import org.atlasapi.remotesite.ContentExtractor;
 import org.jdom.Element;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
@@ -20,8 +19,7 @@ import com.metabroadcast.common.time.Clock;
 import com.metabroadcast.common.time.DateTimeZones;
 import com.sun.syndication.feed.atom.Entry;
 
-public class C4EpgEpisodeExtractor extends BaseC4EpisodeExtractor implements
-        ContentExtractor<Entry, Episode> {
+public class C4EpgEpisodeExtractor extends BaseC4EpisodeExtractor {
     
     private static final String DC_START_TIME = "dc:relation.TXStartTime";
     private static final String DC_TX_CHANNEL = "dc:relation.TXChannel";
@@ -34,19 +32,20 @@ public class C4EpgEpisodeExtractor extends BaseC4EpisodeExtractor implements
         this.channelLookup = ImmutableMap.copyOf(
                 Maps.transformValues(atomApi.getChannelMap(), Identified.TO_URI));
     }
+    
+    @Override
+    protected String getUri(Entry source, Map<String, String> lookup) {
+        return C4AtomApi.canonicalUri(source);
+    }
 
     @Override // epg.atom entries don't have media groups/content, so no images.
     protected Element getMedia(Entry source) {
         return null;
     }
-
+    
     @Override
-    public Episode extract(Entry source) {
-
-        Map<String, String> lookup = C4AtomApi.foreignElementLookup(source);
-
-        Episode episode = createBasicEpisode(source, lookup);
-
+    protected Episode setAdditionalEpisodeFields(Entry entry, Map<String, String> lookup,
+            Episode episode) {
         Version version = new Version();
         episode.setVersions(Sets.newHashSet(version));
 
@@ -61,7 +60,7 @@ public class C4EpgEpisodeExtractor extends BaseC4EpisodeExtractor implements
                     .withChannel(txChannel)
                     .withTransmissionStart(txStart)
                     .withDuration(duration)
-                    .withAtomId(source.getId())
+                    .withAtomId(entry.getId())
                     .build();
             
             broadcast.setLastUpdated(episode.getLastUpdated());
@@ -71,11 +70,6 @@ public class C4EpgEpisodeExtractor extends BaseC4EpisodeExtractor implements
         }
 
         return episode;
-    }
-
-    @Override
-    protected String extractEpisodeUri(Entry source, Map<String, String> lookup) {
-        return C4AtomApi.canonicalUri(source);
     }
 
 }
