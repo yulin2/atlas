ffd2843f631cbfd46bba1563f231e4445d007ffd
liamgh <liam@greenhughes.com>
2013-10-29 14:39:32 +0000

    Capture name, homepage and profile image from Twitter

--- src/main/java/org/atlasapi/application/users/UserTranslator.java
+++ src/main/java/org/atlasapi/application/users/UserTranslator.java
@@ -12,6 +12,16 @@ import com.mongodb.DBObject;
 
 public class UserTranslator {
     
+    private static final String PROFILE_IMAGE_KEY = "profileImage";
+    private static final String WEBSITE_KEY = "website";
+    private static final String EMAIL_KEY = "email";
+    private static final String COMPANY_KEY = "company";
+    private static final String FULL_NAME_KEY = "fullName";
+    private static final String SCREEN_NAME_KEY = "screenName";
+    private static final String ROLE_KEY = "role";
+    private static final String MANAGES_KEY = "manages";
+    private static final String APPS_KEY = "apps";
+    private static final String USER_REF_KEY = "userRef";
     private final UserRefTranslator userTranslator;
 
     public UserTranslator(UserRefTranslator userTranslator) {
@@ -26,10 +36,16 @@ public class UserTranslator {
         BasicDBObject dbo = new BasicDBObject();
         
         TranslatorUtils.from(dbo, MongoConstants.ID, user.getId());
-        TranslatorUtils.from(dbo, "userRef", userTranslator.toDBObject(user.getUserRef()));
-        TranslatorUtils.from(dbo, "apps", user.getApplications());
-        TranslatorUtils.from(dbo, "manages", Iterables.transform(user.getSources(), Publisher.TO_KEY));
-        TranslatorUtils.from(dbo, "role", user.getRole().toString().toLowerCase());
+        TranslatorUtils.from(dbo, USER_REF_KEY, userTranslator.toDBObject(user.getUserRef()));
+        TranslatorUtils.from(dbo, SCREEN_NAME_KEY, user.getScreenName());
+        TranslatorUtils.from(dbo, FULL_NAME_KEY, user.getFullName());
+        TranslatorUtils.from(dbo, COMPANY_KEY, user.getCompany());
+        TranslatorUtils.from(dbo, EMAIL_KEY, user.getEmail());
+        TranslatorUtils.from(dbo, WEBSITE_KEY, user.getWebsite());
+        TranslatorUtils.from(dbo, PROFILE_IMAGE_KEY, user.getProfileImage());
+        TranslatorUtils.from(dbo, APPS_KEY, user.getApplications());
+        TranslatorUtils.from(dbo, MANAGES_KEY, Iterables.transform(user.getSources(), Publisher.TO_KEY));
+        TranslatorUtils.from(dbo, ROLE_KEY, user.getRole().toString().toLowerCase());
         
         return dbo;
     }
@@ -38,15 +54,19 @@ public class UserTranslator {
         if (dbo == null) {
             return null;
         }
-
-        User user = new User(TranslatorUtils.toLong(dbo, MongoConstants.ID));
         
-        user.setUserRef(userTranslator.fromDBObject(TranslatorUtils.toDBObject(dbo, "userRef")));
-        user.setApplications(TranslatorUtils.toSet(dbo, "apps"));
-        user.setSources(ImmutableSet.copyOf(Iterables.transform(TranslatorUtils.toSet(dbo, "manages"),Publisher.FROM_KEY)));
-        user.setRole(Role.valueOf(TranslatorUtils.toString(dbo, "role").toUpperCase()));
-        
-        return user;
-    }
+        return User.builder()
+                .withId(TranslatorUtils.toLong(dbo, MongoConstants.ID))
+                .withUserRef(userTranslator.fromDBObject(TranslatorUtils.toDBObject(dbo, USER_REF_KEY)))
+                .withScreenName(TranslatorUtils.toString(dbo, SCREEN_NAME_KEY))
+                .withFullName(TranslatorUtils.toString(dbo, FULL_NAME_KEY))
+                .withCompany(TranslatorUtils.toString(dbo, COMPANY_KEY))
+                .withEmail(TranslatorUtils.toString(dbo, EMAIL_KEY))
+                .withWebsite(TranslatorUtils.toString(dbo, WEBSITE_KEY))
+                .withProfileImage(TranslatorUtils.toString(dbo, PROFILE_IMAGE_KEY))
+                .withApplicationSlugs(TranslatorUtils.toSet(dbo, APPS_KEY))
+                .withSources(ImmutableSet.copyOf(Iterables.transform(TranslatorUtils.toSet(dbo, MANAGES_KEY),Publisher.FROM_KEY)))
+                .withRole(Role.valueOf(TranslatorUtils.toString(dbo, ROLE_KEY).toUpperCase()))
+                .build();    }
     
 }
