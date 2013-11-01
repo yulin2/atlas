145a561467d76bad95d2ab6f3ebe14e453e86487
liamgh <liam@greenhughes.com>
2013-11-01 17:50:30 +0000

    Output application Ids on users

--- src/main/java/org/atlasapi/application/ApplicationModule.java
+++ src/main/java/org/atlasapi/application/ApplicationModule.java
@@ -1,18 +1,16 @@
 package org.atlasapi.application;
 
-import org.atlasapi.application.auth.AuthProvidersListWriter;
-import org.atlasapi.application.auth.AuthProvidersQueryResultWriter;
 import org.atlasapi.application.auth.OAuthRequestListWriter;
 import org.atlasapi.application.auth.OAuthRequestQueryResultWriter;
 import org.atlasapi.application.auth.OAuthResultListWriter;
 import org.atlasapi.application.auth.OAuthResultQueryResultWriter;
 import org.atlasapi.application.auth.twitter.TwitterAuthController;
-import org.atlasapi.application.auth.www.AuthController;
 import org.atlasapi.application.users.MongoUserStore;
 import org.atlasapi.application.users.NewUserSupplier;
 import org.atlasapi.application.users.UserStore;
 import org.atlasapi.application.www.ApplicationWebModule;
 import org.atlasapi.persistence.application.ApplicationPersistenceModule;
+import org.atlasapi.persistence.application.ApplicationStore;
 import org.atlasapi.persistence.application.SourceRequestStore;
 import org.atlasapi.persistence.auth.MongoTokenRequestStore;
 import org.atlasapi.persistence.auth.TokenRequestStore;
@@ -38,13 +36,14 @@ public class ApplicationModule {
     private static final String APP_NAME = "atlas";
     private @Autowired @Qualifier(value = "adminMongo") DatabasedMongo adminMongo;
     private @Autowired SourceRequestStore sourceRequestStore;
+    private @Autowired ApplicationStore applicationStore;
     @Value("${twitter.auth.consumerKey}") private String consumerKey;
     @Value("${twitter.auth.consumerSecret}") private String consumerSecret;
     @Value("${local.host.name}") private String host;
 
     public @Bean
     UserStore userStore() {
-        return new MongoUserStore(adminMongo);
+        return new MongoUserStore(adminMongo, applicationStore);
     }
 
     public @Bean
