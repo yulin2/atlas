5cb16cfd3a094efd7bc996bc6a2168d21980630e
liamgh <liam@greenhughes.com>
2013-11-04 19:03:42 +0000

    Filter source requests by user permission

--- src/main/java/org/atlasapi/application/SourceRequestQueryExecutor.java
+++ src/main/java/org/atlasapi/application/SourceRequestQueryExecutor.java
@@ -1,6 +1,10 @@
 package org.atlasapi.application;
 
 import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.atlasapi.application.users.User;
 import org.atlasapi.content.criteria.AttributeQuerySet;
 import org.atlasapi.content.criteria.EnumAttributeQuery;
 import org.atlasapi.content.criteria.QueryVisitorAdapter;
@@ -12,6 +16,7 @@ import org.atlasapi.query.common.QueryExecutionException;
 import org.atlasapi.query.common.useraware.UserAwareQuery;
 import org.atlasapi.query.common.useraware.UserAwareQueryExecutor;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
 
@@ -37,14 +42,31 @@ public class SourceRequestQueryExecutor implements UserAwareQueryExecutor<Source
     public UserAwareQueryResult<SourceRequest> execute(UserAwareQuery<SourceRequest> query)
             throws QueryExecutionException {
         AttributeQuerySet operands = query.getOperands();
+        User user = query.getContext().getUser().get();
 
         List<Publisher> source = operands.accept(PUBLISHERS_VISITOR);
+        Iterable<SourceRequest> results;
         
-        if (source.isEmpty()) {
-            return UserAwareQueryResult.listResult(requestStore.all(), query.getContext());
+        if (source.isEmpty() && query.getContext().isAdminUser()) {
+            results = requestStore.all();
+        } else if (source.isEmpty() && !query.getContext().isAdminUser()) {
+            results = requestStore.sourceRequestsForApplicationIds(
+                     user.getApplicationIds());
+        } else if (!source.isEmpty() && query.getContext().isAdminUser()) {
+            results = requestStore.sourceRequestsFor(source.get(0));           
         } else {
-            return UserAwareQueryResult.listResult(requestStore.sourceRequestsFor(source.get(0)), query.getContext());            
+            results = filterRequestsByUserApplications(requestStore.sourceRequestsFor(source.get(0)), user);
         }
+        
+        return UserAwareQueryResult.listResult(results, query.getContext());
     }
+    
+    private Iterable<SourceRequest> filterRequestsByUserApplications(Iterable<SourceRequest> sourceRequests, final User user) {
+        return Iterables.filter(sourceRequests, new Predicate<SourceRequest>() {
 
+            @Override
+            public boolean apply(@Nullable SourceRequest input) {
+                return user.getApplicationIds().contains(input.getAppId());
+            }});
+    }
 }
