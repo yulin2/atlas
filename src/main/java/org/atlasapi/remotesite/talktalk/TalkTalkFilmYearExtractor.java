2249cf24a1b697f20422df453c678a9aa7a79974
Fred van den Driessche <fred@metabroadcast.com>
2013-11-14 12:58:34 +0000

    don't extract years from null descriptions

--- src/main/java/org/atlasapi/remotesite/talktalk/TalkTalkFilmYearExtractor.java
+++ src/main/java/org/atlasapi/remotesite/talktalk/TalkTalkFilmYearExtractor.java
@@ -11,6 +11,9 @@ public class TalkTalkFilmYearExtractor {
     private static final Pattern FILM_YEAR_PATTERN = Pattern.compile(".*\\((\\d{4})\\)\\(\\d+ ?mins\\).*");
     
     public Optional<Integer> extractYear(String description) {
+        if (description == null) {
+            return Optional.absent();
+        }
         Matcher matcher = FILM_YEAR_PATTERN.matcher(description);
         if (matcher.matches()) {
             return Optional.of(Integer.parseInt(matcher.group(1)));
