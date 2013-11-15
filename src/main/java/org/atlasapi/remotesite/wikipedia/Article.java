6876bf1bce6e4bfebbc019096ae9d24db00f5a09
Tom Spurling <tim@metabroadcast.com>
2013-11-15 14:50:38 +0000

    Get Wikipedia URLs right

--- src/main/java/org/atlasapi/remotesite/wikipedia/Article.java
+++ src/main/java/org/atlasapi/remotesite/wikipedia/Article.java
@@ -11,7 +11,14 @@ public abstract class Article {
     abstract String getTitle();
 
     public String getUrl() {
-        return "http://en.wikipedia.org/wiki/" + getTitle();
+        String safeTitle = getTitle().replaceAll(" ", "_")
+                .replaceAll("\"", "%22").replaceAll("'", "%27")
+                .replaceAll(",", "%2C").replaceAll(";", "%3B")
+                .replaceAll("<", "%3C").replaceAll(">", "%3E")
+                .replaceAll("\\[", "%5B").replaceAll("]", "%5D")
+                .replaceAll("\\?", "%3F");
+        // see: http://en.wikipedia.org/wiki/Help:URL#URLs_of_Wikipedia_pages
+        return "http://en.wikipedia.org/wiki/" + safeTitle;
     }
 
 }
