f1a860a4dd25d3d8e4e334eab38250e863849425
Tom McAdam <tom@metabroadcast.com>
2013-11-28 15:32:13 +0000

    Primetime overrides for MB picks
    Allow overrides based on channel or genre for periods that are
    considered to be primetime.

--- src/main/java/org/atlasapi/remotesite/metabroadcast/picks/PickPredicate.java
+++ src/main/java/org/atlasapi/remotesite/metabroadcast/picks/PickPredicate.java
@@ -1,15 +1,18 @@
 package org.atlasapi.remotesite.metabroadcast.picks;
 
+import java.util.Map;
 import java.util.Set;
 
+import org.atlasapi.genres.AtlasGenre;
 import org.atlasapi.media.channel.Channel;
 import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.media.entity.Item;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalTime;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.metabroadcast.common.time.LocalTimeRange;
@@ -17,6 +20,15 @@ import com.metabroadcast.common.time.LocalTimeRange;
 
 public class PickPredicate implements Predicate<Item>{
 
+    private static final LocalTimeRange CHILDRENS_PRIMETIME = new LocalTimeRange(new LocalTime(15, 0, 0), new LocalTime(19, 15, 0));
+    private static final Map<String, LocalTimeRange> CHANNEL_PRIMTIME_OVERRIDES = ImmutableMap.of(
+            
+    );
+    
+    private static final Map<String, LocalTimeRange> GENRE_OVERRIDES = ImmutableMap.of(
+            AtlasGenre.CHILDRENS.getUri(), CHILDRENS_PRIMETIME,
+            AtlasGenre.FACTUAL.getUri(), new LocalTimeRange(new LocalTime(0, 0, 0), new LocalTime(23, 59, 59)));
+    
     private static final DateTimeZone UK_TIMEZONE = DateTimeZone.forID("Europe/London");
     private static final LocalTimeRange PRIMETIME = new LocalTimeRange(
             new LocalTime(19, 0, 0), new LocalTime(23, 15, 0));
@@ -31,7 +43,7 @@ public class PickPredicate implements Predicate<Item>{
     public boolean apply(Item item) {
         Broadcast broadcast = Iterables.getOnlyElement(item.flattenBroadcasts());
         return channelUris.contains(broadcast.getBroadcastOn()) 
-                && isPrimetime(broadcast.getTransmissionTime())
+                && isPrimetime(item, broadcast)
                 && ( Boolean.TRUE.equals(broadcast.getPremiere()) 
                         || Boolean.TRUE.equals(broadcast.getNewEpisode()) 
                         || Boolean.TRUE.equals(broadcast.getNewSeries()) 
@@ -45,7 +57,25 @@ public class PickPredicate implements Predicate<Item>{
      * @param transmissionTime
      * @return
      */
-    private boolean isPrimetime(DateTime transmissionTime) {
-        return PRIMETIME.contains(transmissionTime.withZone(UK_TIMEZONE));
+    private boolean isPrimetime(Item item, Broadcast broadcast) {
+        LocalTimeRange primetime = primetimeForGenres(item.getGenres())
+                                    .or(primetimeForChannel(broadcast.getBroadcastOn()))
+                                    .or(PRIMETIME);
+        
+        return primetime.contains(broadcast.getTransmissionTime().withZone(UK_TIMEZONE));
+    }
+    
+    private Optional<LocalTimeRange> primetimeForGenres(Set<String> genres) {
+        for (String genre : genres) {
+            LocalTimeRange genreOverride = GENRE_OVERRIDES.get(genre);
+            if (genreOverride != null) {
+                return Optional.of(genreOverride);
+            }
+        }
+        return Optional.absent();
+    }
+    
+    private Optional<LocalTimeRange> primetimeForChannel(String channelUri) {
+        return Optional.fromNullable(CHANNEL_PRIMTIME_OVERRIDES.get(channelUri));
     }
 }
