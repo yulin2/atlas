ffce790159aa398c374c6e213e817e5447fe1443
Tom McAdam <tom@metabroadcast.com>
2013-11-29 16:33:13 +0000

    TalkTalk all content group
    Create an all content content group containing, you guessed it,
    all TalkTalk content. Well, items.

--- src/main/java/org/atlasapi/remotesite/talktalk/TalkTalkChannelProcessingTask.java
+++ src/main/java/org/atlasapi/remotesite/talktalk/TalkTalkChannelProcessingTask.java
@@ -2,11 +2,19 @@ package org.atlasapi.remotesite.talktalk;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import org.atlasapi.media.entity.ChildRef;
+import org.atlasapi.media.entity.ContentGroup;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.content.ContentGroupResolver;
+import org.atlasapi.persistence.content.ContentGroupWriter;
+import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.remotesite.talktalk.TalkTalkClient.TalkTalkTvStructureCallback;
 import org.atlasapi.remotesite.talktalk.vod.bindings.ChannelType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.metabroadcast.common.scheduling.ScheduledTask;
 import com.metabroadcast.common.scheduling.UpdateProgress;
 
@@ -17,15 +25,24 @@ import com.metabroadcast.common.scheduling.UpdateProgress;
  */
 public class TalkTalkChannelProcessingTask extends ScheduledTask {
     
+    public static final String ALL_CONTENT_CONTENT_GROUP_URI = "http://talktalk.net/content-groups/all-content";
+    
     private final Logger log = LoggerFactory.getLogger(getClass());
     
     private final TalkTalkClient client;
 
     private TalkTalkChannelProcessor<UpdateProgress> channelProcessor;
+    private final ContentGroupResolver contentGroupResolver;
+    private final ContentGroupWriter contentGroupWriter;
 
-    public TalkTalkChannelProcessingTask(TalkTalkClient talkTalkClient, TalkTalkChannelProcessor<UpdateProgress> channelProcessor) {
+    public TalkTalkChannelProcessingTask(TalkTalkClient talkTalkClient, 
+            TalkTalkChannelProcessor<UpdateProgress> channelProcessor,
+            ContentGroupResolver contentGroupResolver,
+            ContentGroupWriter contentGroupWriter) {
         this.client = checkNotNull(talkTalkClient);
         this.channelProcessor = checkNotNull(channelProcessor);
+        this.contentGroupResolver = checkNotNull(contentGroupResolver);
+        this.contentGroupWriter = checkNotNull(contentGroupWriter);
     }
 
     @Override
@@ -43,8 +60,11 @@ public class TalkTalkChannelProcessingTask extends ScheduledTask {
                 @Override
                 public void process(ChannelType channel) {
                     try {
+                        ContentGroup contentGroup = createOrGetContentGroup();
                         log.debug("processing channel {}", channel.getId());
-                        progress = progress.reduce(channelProcessor.process(channel));
+                        progress = progress.reduce(channelProcessor.process(channel, 
+                                Optional.of(contentGroup)));
+                        contentGroupWriter.createOrUpdate(contentGroup);
                         reportStatus(progress.toString());
                     } catch (TalkTalkException tte) {
                         log.error("failed to process " + channel.getId(), tte);
@@ -58,6 +78,21 @@ public class TalkTalkChannelProcessingTask extends ScheduledTask {
             // scheduler.
             throw new RuntimeException(tte);
         }
+    }
+    
+    private ContentGroup createOrGetContentGroup() {
+        ContentGroup contentGroup;
+        ResolvedContent resolvedContent = contentGroupResolver
+                .findByCanonicalUris(ImmutableList.of(ALL_CONTENT_CONTENT_GROUP_URI));
+        
+        if (resolvedContent.get(ALL_CONTENT_CONTENT_GROUP_URI).hasValue()) {
+            contentGroup = (ContentGroup) resolvedContent.get(ALL_CONTENT_CONTENT_GROUP_URI).requireValue();
+            contentGroup.setContents(ImmutableList.<ChildRef>of());
+        } else {
+            contentGroup = new ContentGroup(ALL_CONTENT_CONTENT_GROUP_URI, Publisher.TALK_TALK);
+        }
+        
+        return contentGroup;
     }
     
 }
--- src/test/java/org/atlasapi/remotesite/talktalk/TalkTalkChannelProcessingTaskTest.java
+++ src/test/java/org/atlasapi/remotesite/talktalk/TalkTalkChannelProcessingTaskTest.java
@@ -9,11 +9,16 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.atlasapi.persistence.content.ContentGroupResolver;
+import org.atlasapi.persistence.content.ContentGroupWriter;
+import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.remotesite.talktalk.vod.bindings.ChannelType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.metabroadcast.common.scheduling.UpdateProgress;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,17 +26,24 @@ public class TalkTalkChannelProcessingTaskTest {
 
     @SuppressWarnings("unchecked")
     private TalkTalkChannelProcessor<UpdateProgress> processor = mock(TalkTalkChannelProcessor.class);
+    private ContentGroupResolver contentGroupResolver = mock(ContentGroupResolver.class);
+    private ContentGroupWriter contentGroupWriter = mock(ContentGroupWriter.class);
+    
     private StubTalkTalkClient client = new StubTalkTalkClient();
-    private TalkTalkChannelProcessingTask task = new TalkTalkChannelProcessingTask(client, processor);
+    private TalkTalkChannelProcessingTask task = new TalkTalkChannelProcessingTask(client, processor,
+            contentGroupResolver, contentGroupWriter);
     
     @Test
     public void testUpdatesContent() throws TalkTalkException {
-        when(processor.process(argThat(is(any(ChannelType.class)))))
+        when(processor.process(argThat(is(any(ChannelType.class))), argThat(is(any(Optional.class)))))
             .thenReturn(UpdateProgress.SUCCESS);
+        
+        when(contentGroupResolver.findByCanonicalUris(ImmutableList.of(TalkTalkChannelProcessingTask.ALL_CONTENT_CONTENT_GROUP_URI)))
+            .thenReturn(ResolvedContent.builder().build());
         
         task.run();
         
-        verify(processor, times(1)).process(argThat(is(any(ChannelType.class))));
+        verify(processor, times(1)).process(argThat(is(any(ChannelType.class))), argThat(is(any(Optional.class))));
         
         assertThat(task.getCurrentStatusMessage(), is(UpdateProgress.SUCCESS.toString()));
         
