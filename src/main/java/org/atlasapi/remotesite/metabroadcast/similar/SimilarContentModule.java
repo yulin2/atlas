4fe28700be1c70ce8b29b49276ab171ebe56d923
Tom McAdam <tom@metabroadcast.com>
2014-01-16 19:11:24 +0000

    Take account of availability and upcoming broadcasts
    Similar content calculations now take account of available
    locations or upcoming broadcasts, by boosting the score if
    either is the case, across all equivalent content.

--- src/main/java/org/atlasapi/remotesite/metabroadcast/similar/SimilarContentModule.java
+++ src/main/java/org/atlasapi/remotesite/metabroadcast/similar/SimilarContentModule.java
@@ -7,6 +7,9 @@ import org.atlasapi.persistence.MongoContentPersistenceModule;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.content.listing.ContentLister;
+import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
+import org.atlasapi.persistence.output.MongoAvailableItemsResolver;
+import org.atlasapi.persistence.output.MongoUpcomingItemsResolver;
 import org.joda.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,12 +17,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
 import com.metabroadcast.common.scheduling.RepetitionRule;
 import com.metabroadcast.common.scheduling.RepetitionRules;
 import com.metabroadcast.common.scheduling.SimpleScheduler;
 
 @Configuration
-@Import({ MongoContentPersistenceModule.class })
+@Import({ MongoContentPersistenceModule.class, MongoContentPersistenceModule.class })
 public class SimilarContentModule {
 
     private static RepetitionRule SIMILAR_CONTENT_UPDATER_REPETITION = RepetitionRules.daily(LocalTime.parse("15:00"));
@@ -28,6 +32,8 @@ public class SimilarContentModule {
     @Autowired ContentWriter contentWriter;
     @Autowired ContentResolver contentResolver;
     @Autowired SimpleScheduler scheduler;
+    @Autowired LookupEntryStore lookupStore;
+    @Autowired DatabasedMongo mongo;
     
     @Value("${updaters.similarcontent.enabled}") 
     Boolean tasksEnabled;
@@ -47,12 +53,21 @@ public class SimilarContentModule {
     
     SimilarContentProvider similarContentProvider() {
         return new DefaultSimilarContentProvider(contentLister, Publisher.PA, 10, 
-                new GenreAndPeopleTraitHashCalculator());
+                new GenreAndPeopleTraitHashCalculator(), availableItemsResolver(), 
+                        upcomingItemsResolver());
     }
     
     SeparateSourceSimilarContentWriter similarContentWriter() {
         return new SeparateSourceSimilarContentWriter(Publisher.METABROADCAST_SIMILAR_CONTENT, contentResolver, 
                 contentWriter);
+    }
+    
+    MongoUpcomingItemsResolver upcomingItemsResolver() {
+        return new MongoUpcomingItemsResolver(mongo);
+    }
+
+    MongoAvailableItemsResolver availableItemsResolver() {
+        return new MongoAvailableItemsResolver(mongo, lookupStore);
     }
     
 }
