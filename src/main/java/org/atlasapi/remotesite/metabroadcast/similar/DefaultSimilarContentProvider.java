86180c172b158abc26d9fcd1ac83400d18f9db0d
Tom McAdam <tom@metabroadcast.com>
2014-01-16 19:11:24 +0000

    Take account of availability and upcoming broadcasts
    Similar content calculations now take account of available
    locations or upcoming broadcasts, by boosting the score if
    either is the case, across all equivalent content.

--- src/main/java/org/atlasapi/remotesite/metabroadcast/similar/DefaultSimilarContentProvider.java
+++ src/main/java/org/atlasapi/remotesite/metabroadcast/similar/DefaultSimilarContentProvider.java
@@ -9,36 +9,59 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.atlasapi.application.v3.ApplicationConfiguration;
+import org.atlasapi.application.v3.SourceStatus;
 import org.atlasapi.media.entity.ChildRef;
+import org.atlasapi.media.entity.Container;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Described;
+import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.ContentCategory;
 import org.atlasapi.persistence.content.listing.ContentLister;
 import org.atlasapi.persistence.content.listing.ContentListingCriteria;
+import org.atlasapi.persistence.output.AvailableItemsResolver;
+import org.atlasapi.persistence.output.UpcomingItemsResolver;
 
 import com.google.common.base.Function;
 import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
 import com.google.common.collect.MinMaxPriorityQueue;
 import com.google.common.collect.Sets;
 
 
 public class DefaultSimilarContentProvider implements SimilarContentProvider {
 
+    private static final int AVAILABLE_UPCOMING_BOOST_FACTOR = 10;
     private final ContentLister contentLister;
     private final Publisher publisher;
     private final int similarItemLimit;
-    private Map<ChildRef, Set<Integer>> similarHashes;
+    private Map<ChildRef, ContentSummary> similarHashes;
     private final TraitHashCalculator traitHashCalculator;
+    private final AvailableItemsResolver availableItemsResolver;
+    private final UpcomingItemsResolver upcomingItemsResolver;
+    private final ApplicationConfiguration appConfig = 
+            ApplicationConfiguration.defaultConfiguration()
+                                    .withSources(Maps.asMap(Publisher.all(), new Function<Publisher, SourceStatus>() {
+
+                                            @Override
+                                            public SourceStatus apply(Publisher publisher) {
+                                                return SourceStatus.AVAILABLE_ENABLED;
+                                            }
+        
+                                     }));
     
     public DefaultSimilarContentProvider (ContentLister contentLister, Publisher publisher, 
-            int similarItemLimit, TraitHashCalculator traitHashCalculator) {
+            int similarItemLimit, TraitHashCalculator traitHashCalculator, 
+            AvailableItemsResolver availableItemsResolver, UpcomingItemsResolver upcomingItemsResolver) {
         this.contentLister = checkNotNull(contentLister);
         this.publisher = checkNotNull(publisher);
         this.traitHashCalculator = checkNotNull(traitHashCalculator);
+        this.availableItemsResolver = checkNotNull(availableItemsResolver);
+        this.upcomingItemsResolver = checkNotNull(upcomingItemsResolver);
         this.similarItemLimit = similarItemLimit;
     }
     
@@ -49,13 +72,31 @@ public class DefaultSimilarContentProvider implements SimilarContentProvider {
                                                     .build();
         
         Iterator<Content> content = contentLister.listContent(criteria);
-        ImmutableMap.Builder<ChildRef, Set<Integer>> similarHashes = ImmutableMap.builder();
+        ImmutableMap.Builder<ChildRef, ContentSummary> similarHashes = ImmutableMap.builder();
         
         while (content.hasNext()) {
             Content c = content.next();
-            similarHashes.put(c.childRef(), traitHashCalculator.traitHashesFor(c));
+            similarHashes.put(c.childRef(), new ContentSummary(traitHashCalculator.traitHashesFor(c), 
+                    isUpcomingOrAvailable(c)));
         }
         this.similarHashes = similarHashes.build();
+    }
+
+    private boolean isUpcomingOrAvailable(Content content) {
+        if (content instanceof Container) {
+            Container container = (Container) content;
+            return availableItemsResolver.availableItemsFor(container, appConfig).iterator().hasNext()
+                    || upcomingItemsResolver.upcomingItemsFor(container).iterator().hasNext();
+        } else if (content instanceof Item) {
+            Item item = (Item) content;
+            return availableItemsResolver.isAvailable(item, appConfig)
+                    || upcomingItemsResolver.hasUpcomingBroadcasts(item, appConfig);
+        } else {
+            throw new IllegalArgumentException("Can't deal with Content of type " + 
+                            content.getClass().getSimpleName());
+        }
+        
+        
     }
 
     @Override
@@ -67,9 +108,12 @@ public class DefaultSimilarContentProvider implements SimilarContentProvider {
         
         Set<Integer> candidateHashes = traitHashCalculator.traitHashesFor(described);
         
-        for (Entry<ChildRef, Set<Integer>> entry : similarHashes.entrySet()) {
+        for (Entry<ChildRef, ContentSummary> entry : similarHashes.entrySet()) {
             if (!entry.getKey().getId().equals(described.getId())) {
-                int score = Sets.intersection(candidateHashes, entry.getValue()).size();
+                int score = Sets.intersection(candidateHashes, entry.getValue().traits).size();
+                if (entry.getValue().isUpcomingOrAvailable) {
+                    score *= AVAILABLE_UPCOMING_BOOST_FACTOR;
+                }
                 similarContent.add(new ScoredContent(entry.getKey(), score));
             }
         }
@@ -95,6 +139,17 @@ public class DefaultSimilarContentProvider implements SimilarContentProvider {
                     .compare(o.score, this.score)
                     .compare(this.ref, o.ref)
                     .result();
+        }
+    }
+    
+    private static class ContentSummary {
+        
+        private final Set<Integer> traits;
+        private final boolean isUpcomingOrAvailable;
+        
+        public ContentSummary(Iterable<Integer> traits, boolean isUpcomingOrAvailable) {
+            this.traits = ImmutableSet.copyOf(traits);
+            this.isUpcomingOrAvailable = isUpcomingOrAvailable;
         }
     }
     
