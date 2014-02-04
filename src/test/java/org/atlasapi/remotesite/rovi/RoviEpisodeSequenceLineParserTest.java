9c1eaaabfbf6346b366b2ae39040ae46e52857ec
Massimiliano Tomassi <max@metabroadcast.com>
2014-02-04 19:36:16 +0000

    Rovi episode sequence model and parser

---
+++ src/test/java/org/atlasapi/remotesite/rovi/RoviEpisodeSequenceLineParserTest.java
@@ -0,0 +1,28 @@
+package org.atlasapi.remotesite.rovi;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.atlasapi.remotesite.rovi.series.RoviEpisodeSequenceLine;
+import org.atlasapi.remotesite.rovi.series.RoviEpisodeSequenceLineParser;
+import org.junit.Test;
+
+
+public class RoviEpisodeSequenceLineParserTest {
+
+    private final RoviEpisodeSequenceLineParser parser = new RoviEpisodeSequenceLineParser();
+    
+    @Test
+    public void testParseLine() {
+        String line = "3757920|13376601|3832987|Amazon Redux|6|10|10|Ins";
+        
+        RoviEpisodeSequenceLine roviLine = parser.parseLine(line);
+        
+        assertEquals("3757920", roviLine.getSeriesId());
+        assertEquals("13376601", roviLine.getSeasonProgramId());
+        assertEquals("3832987", roviLine.getProgramId());
+        assertEquals("Amazon Redux", roviLine.getEpisodeTitle());
+        assertEquals(10, roviLine.getEpisodeSeasonSequence().intValue());
+    }
+    
+}
