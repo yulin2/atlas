8e953ce8d15261ff14f8451c321206dc671bfa43
Massimiliano Tomassi <max@metabroadcast.com>
2014-02-11 16:07:46 +0000

    Created a ProgramLineBaseItemExtractor so that ProgramLineEpisodeExtractor and ProgramLineFilmExtractor can extend it

--- src/main/java/org/atlasapi/remotesite/rovi/program/ProgramLineItemExtractor.java
+++ src/main/java/org/atlasapi/remotesite/rovi/program/ProgramLineItemExtractor.java
@@ -3,13 +3,11 @@ package org.atlasapi.remotesite.rovi.program;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.remotesite.rovi.KeyedFileIndex;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /*
  * Extracts an {@link Item} from a {@link RoviProgramLine} with {@link RoviShowType} OT (Other Program)
  */
-public class ProgramLineItemExtractor extends ProgramLineBaseExtractor<RoviProgramLine, Item> {
+public class ProgramLineItemExtractor extends ProgramLineBaseItemExtractor<Item>{
 
     public ProgramLineItemExtractor(
             KeyedFileIndex<String, RoviProgramDescriptionLine> descriptionIndex,
@@ -17,22 +15,14 @@ public class ProgramLineItemExtractor extends ProgramLineBaseExtractor<RoviProgr
         super(descriptionIndex, contentResolver);
     }
 
-    private final static Logger LOG = LoggerFactory.getLogger(ProgramLineItemExtractor.class);
-    
     @Override
-    protected Item createContent() {
-        return new Item();
+    protected void addItemSpecificData(Item content, RoviProgramLine programLine) {
+        // Do nothing
     }
 
     @Override
-    protected Item addSpecificData(Item content, RoviProgramLine programLine) {
-        // At the moment returns the item directly. It might be needed to add more fields in the future
-        return content;
-    }
-    
-    @Override
-    protected Logger log() {
-        return LOG;
+    protected Item createContent() {
+        return new Item();
     }
 
 }
