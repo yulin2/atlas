4a67d03bd498c5bd4e4ec10e813a6970b8f49a78
Massimiliano Tomassi <max@metabroadcast.com>
2014-02-24 17:08:37 +0000

    Got rid of RoviUtils class

--- src/main/java/org/atlasapi/remotesite/rovi/processing/RoviLineProcessor.java
+++ src/main/java/org/atlasapi/remotesite/rovi/processing/RoviLineProcessor.java
@@ -1,8 +1,11 @@
 package org.atlasapi.remotesite.rovi.processing;
 
-import java.nio.charset.Charset;
+import static org.atlasapi.remotesite.rovi.RoviConstants.FILE_CHARSET;
+import static org.atlasapi.remotesite.rovi.RoviConstants.UTF_16LE_BOM;
 
-import org.atlasapi.remotesite.rovi.RoviUtils;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
 import org.atlasapi.remotesite.rovi.indexing.IndexAccessException;
 import org.atlasapi.remotesite.rovi.indexing.KeyedLine;
 import org.atlasapi.remotesite.rovi.parsers.RoviLineParser;
@@ -87,8 +90,8 @@ public abstract class RoviLineProcessor<T extends KeyedLine<?>> implements LineP
     }
 
     private String stripBomIfNeeded(String line) {
-        if (RoviUtils.startsWithUTF16LEBom(line)) {
-            line = RoviUtils.stripBom(line);
+        if (startsWithUTF16LEBom(line)) {
+            line = stripBom(line);
             handleBom();
         }
         return line;
@@ -96,5 +99,48 @@ public abstract class RoviLineProcessor<T extends KeyedLine<?>> implements LineP
     
     private DateTime now() {
         return DateTime.now(DateTimeZone.UTC);
+    }
+    
+    /**
+     * Strips the UTF-16LE BOM (Byte Order Mark) from a line
+     * UTF-16LE BOM is composed by a sequence of two bytes: 0xFF followed by 0xFE
+     * 
+     * @param line - The line to strip the BOM from
+     * @return the line without the UTF-16LE BOM
+     */
+    public static String stripBom(String line) {
+        if (startsWithUTF16LEBom(line)) {
+            byte[] bytes = line.getBytes(FILE_CHARSET);
+            return new String(stripBomFromBytes(bytes), FILE_CHARSET);
+        }
+        
+        return line;
+    }
+
+    /**
+     * Detects if a String line starts with an UTF-16LE BOM (Byte Order Mark).
+     * UTF-16LE BOM is composed by a sequence of two bytes: 0xFF followed by 0xFE
+     * 
+     * @param line - The line to analyze
+     * @return true if the line starts with an UTF-16LE BOM, false otherwise
+     */
+    public static boolean startsWithUTF16LEBom(String line) {
+        byte[] bytes = line.getBytes(FILE_CHARSET);
+        
+        if (bytes.length >= UTF_16LE_BOM.length) {
+            int[] firstTwoBytes = {toUnsignedInt(bytes[0]), toUnsignedInt(bytes[1])};    
+            
+            return Arrays.equals(firstTwoBytes, UTF_16LE_BOM);
+        }
+        
+        return false;
+    }
+
+    private static byte[] stripBomFromBytes(byte[] bytes) {
+        return Arrays.copyOfRange(bytes, UTF_16LE_BOM.length, bytes.length);
+    }
+    
+    private static int toUnsignedInt(byte b) {
+        return (int) b & 0xFF;
     }
 }
