cf481a73f94419dddc3647578e2c3490c207fc79
Massimiliano Tomassi <max@metabroadcast.com>
2014-02-24 17:35:54 +0000

    Renamed from ContentCreator to ContentFactory

--- src/main/java/org/atlasapi/remotesite/rovi/processing/ContentFactory.java
+++ src/main/java/org/atlasapi/remotesite/rovi/processing/ContentFactory.java
@@ -14,7 +14,7 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
 
-public class ContentCreator {
+public class ContentFactory {
     
     private static final BiMap<RoviShowType, Class<? extends Content>> showTypeToClass = HashBiMap.create();
     
