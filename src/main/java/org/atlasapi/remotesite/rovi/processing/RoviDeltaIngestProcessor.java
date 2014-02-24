c243402c7cf6bb1ba1ce0cf75aa51a2cf5ed1687
Massimiliano Tomassi <max@metabroadcast.com>
2014-02-24 18:32:33 +0000

    Refactored ingest processors extracting a method for each processing step

--- src/main/java/org/atlasapi/remotesite/rovi/processing/RoviDeltaIngestProcessor.java
+++ src/main/java/org/atlasapi/remotesite/rovi/processing/RoviDeltaIngestProcessor.java
@@ -2,7 +2,9 @@ package org.atlasapi.remotesite.rovi.processing;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Predicates.and;
+import static com.google.common.base.Predicates.not;
 import static org.atlasapi.remotesite.rovi.RoviConstants.FILE_CHARSET;
+import static org.atlasapi.remotesite.rovi.RoviPredicates.IS_BRAND;
 import static org.atlasapi.remotesite.rovi.RoviPredicates.IS_BRAND_NO_PARENT;
 import static org.atlasapi.remotesite.rovi.RoviPredicates.IS_BRAND_WITH_PARENT;
 import static org.atlasapi.remotesite.rovi.RoviPredicates.IS_INSERT;
@@ -15,7 +17,6 @@ import java.util.Set;
 
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.remotesite.rovi.RoviContentWriter;
-import org.atlasapi.remotesite.rovi.RoviPredicates;
 import org.atlasapi.remotesite.rovi.indexing.KeyedFileIndex;
 import org.atlasapi.remotesite.rovi.indexing.KeyedFileIndexer;
 import org.atlasapi.remotesite.rovi.model.RoviEpisodeSequenceLine;
@@ -27,7 +28,6 @@ import org.atlasapi.remotesite.rovi.populators.ContentPopulatorSupplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Predicates;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Sets.SetView;
 import com.google.common.io.Files;
@@ -86,103 +86,144 @@ public class RoviDeltaIngestProcessor implements RoviIngestProcessor {
             
             LOG.info("Start processing programs");
             
-            // Step 1. Process brands with no parent
-            RoviDataProcessingResult processingBrandsNoParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
-                    new RoviProgramLineParser(),
-                    FILE_CHARSET,
-                    and(IS_INSERT, IS_BRAND_NO_PARENT),
-                    contentWriter,
-                    contentResolver,
-                    contentPopulator
-                    ));
+            processingBrandsWithoutParentInsert(programFile, contentPopulator);
+            processingBrandsWithParentInsert(programFile, contentPopulator);
+            processingAllBrandsUpdateAndDelete(programFile, contentPopulator);
+            processingSeasons(seasonsFile);
+            processItemsWithoutParentInsert(programFile, contentPopulator);
+            processItemsWithParentInsert(programFile, contentPopulator);
+            processingItemsUpdateAndDelete(programFile, contentPopulator);
+            processAuxiliaryActions(programIndex, descriptionIndex, episodeSequenceIndex);
+            processBroadcasts(scheduleFile);
             
-            LOG.info("Processing brands with no parent complete, result: {}", processingBrandsNoParentResult);
-    
-            // Step 2. Process brands with  parent
-            RoviDataProcessingResult processingBrandsWithParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
-                    new RoviProgramLineParser(),
-                    FILE_CHARSET,
-                    and(IS_INSERT, IS_BRAND_WITH_PARENT),
-                    contentWriter,
-                    contentResolver,
-                    contentPopulator
-                    ));
-            
-            LOG.info("Processing brands with parent complete, result: {}", processingBrandsWithParentResult);
-            
-            // Step 3. Process series
-            RoviDataProcessingResult processingSeriesResult = Files.readLines(seasonsFile, FILE_CHARSET, new RoviSeasonLineIngestor(
-                    new RoviSeasonHistoryLineParser(),
-                    FILE_CHARSET,
-                    contentResolver,
-                    contentWriter,
-                    auxCacheSupplier.parentPublisherCache(MAX_CACHE_SIZE)));
-            
-            LOG.info("Processing series complete, result: {}", processingSeriesResult);
-            
-            // Step 4. Process programs without parent
-            RoviDataProcessingResult processingNoBrandsNoParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
-                    new RoviProgramLineParser(),
-                    FILE_CHARSET,
-                    and(IS_INSERT, NO_BRAND_NO_PARENT),
-                    contentWriter,
-                    contentResolver,
-                    contentPopulator
-                    ));
-            
-            LOG.info("Processing programs (no brands) with no parent complete, result: {}", processingNoBrandsNoParentResult);
-            
-            // Step 5. Process programs with parent
-            RoviDataProcessingResult processingNoBrandsWithParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
-                    new RoviProgramLineParser(),
-                    FILE_CHARSET,
-                    and(IS_INSERT, NO_BRAND_WITH_PARENT),
-                    contentWriter,
-                    contentResolver,
-                    contentPopulator
-                    ));
-            
-            LOG.info("Processing programs (no brands) with parent complete, result: {}", processingNoBrandsWithParentResult);
-            
-            // Step 6. Process updates and deletes of all the types
-            RoviDataProcessingResult processingUpdatesAndDeletesResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
-                    new RoviProgramLineParser(),
-                    FILE_CHARSET,
-                    Predicates.not(RoviPredicates.IS_INSERT),
-                    contentWriter,
-                    contentResolver,
-                    contentPopulator
-                    ));
-            
-            LOG.info("Processing programs (no brands) with parent complete, result: {}", processingUpdatesAndDeletesResult);
-            
-            // Step 7. Process only auxiliary actions
-            Set<String> programsWithOnlyAuxiliaryActions = programsWithOnlyAuxiliaryActions(programIndex,
-                    descriptionIndex,
-                    episodeSequenceIndex);
-            
-            ContentPopulatorSupplier populator = new ContentPopulatorSupplier(
-                    descriptionIndex,
-                    episodeSequenceIndex,
-                    contentResolver,
-                    auxCacheSupplier.seasonNumberCache(100000));
-            
-            AuxiliaryActionsProcessor auxiliaryActionsProcessor = new AuxiliaryActionsProcessor(
-                    populator,
-                    contentResolver,
-                    contentWriter);
-            
-            auxiliaryActionsProcessor.process(programsWithOnlyAuxiliaryActions);
-
-            LOG.info("Processing auxiliary actions only, result: {}", programsWithOnlyAuxiliaryActions);
-            
-            // Step 8. Process schedule
-            scheduleFileProcessor.process(scheduleFile);
-      
-            LOG.info("Processing schedule complete");
         } finally {
             releaseIndexResources(programIndex, descriptionIndex, episodeSequenceIndex);
         }
+    }
+
+    private void processBroadcasts(File scheduleFile) throws IOException {
+        scheduleFileProcessor.process(scheduleFile);
+     
+        LOG.info("Processing schedule complete");
+    }
+
+    private void processAuxiliaryActions(KeyedFileIndex<String, RoviProgramLine> programIndex,
+            KeyedFileIndex<String, RoviProgramDescriptionLine> descriptionIndex,
+            KeyedFileIndex<String, RoviEpisodeSequenceLine> episodeSequenceIndex) {
+        
+        Set<String> programsWithOnlyAuxiliaryActions = programsWithOnlyAuxiliaryActions(programIndex,
+                descriptionIndex,
+                episodeSequenceIndex);
+        
+        ContentPopulatorSupplier populator = new ContentPopulatorSupplier(
+                descriptionIndex,
+                episodeSequenceIndex,
+                contentResolver,
+                auxCacheSupplier.seasonNumberCache(100000));
+        
+        AuxiliaryActionsProcessor auxiliaryActionsProcessor = new AuxiliaryActionsProcessor(
+                populator,
+                contentResolver,
+                contentWriter);
+        
+        RoviDataProcessingResult result = auxiliaryActionsProcessor.process(programsWithOnlyAuxiliaryActions);
+
+        LOG.info("Processing auxiliary actions only, result: {}", result);
+    }
+
+    private void processingItemsUpdateAndDelete(File programFile,
+            ContentPopulatorSupplier contentPopulator) throws IOException {
+        RoviDataProcessingResult processingUpdatesAndDeletesResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
+                new RoviProgramLineParser(),
+                FILE_CHARSET,
+                and(not(IS_INSERT), not(IS_BRAND)),
+                contentWriter,
+                contentResolver,
+                contentPopulator
+                ));
+        
+        LOG.info("Processing programs (no brands) complete (Update and Delete), result: {}", processingUpdatesAndDeletesResult);
+    }
+
+    private void processItemsWithParentInsert(File programFile,
+            ContentPopulatorSupplier contentPopulator) throws IOException {
+        RoviDataProcessingResult processingNoBrandsWithParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
+                new RoviProgramLineParser(),
+                FILE_CHARSET,
+                and(IS_INSERT, NO_BRAND_WITH_PARENT),
+                contentWriter,
+                contentResolver,
+                contentPopulator
+                ));
+        
+        LOG.info("Processing programs (no brands) with parent complete (Insert), result: {}", processingNoBrandsWithParentResult);
+    }
+
+    private void processItemsWithoutParentInsert(File programFile,
+            ContentPopulatorSupplier contentPopulator) throws IOException {
+        RoviDataProcessingResult processingNoBrandsNoParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
+                new RoviProgramLineParser(),
+                FILE_CHARSET,
+                and(IS_INSERT, NO_BRAND_NO_PARENT),
+                contentWriter,
+                contentResolver,
+                contentPopulator
+                ));
+        
+        LOG.info("Processing programs (no brands) with no parent complete (Insert), result: {}", processingNoBrandsNoParentResult);
+    }
+
+    private void processingSeasons(File seasonsFile) throws IOException {
+        RoviDataProcessingResult processingSeriesResult = Files.readLines(seasonsFile, FILE_CHARSET, new RoviSeasonLineIngestor(
+                new RoviSeasonHistoryLineParser(),
+                FILE_CHARSET,
+                contentResolver,
+                contentWriter,
+                auxCacheSupplier.parentPublisherCache(MAX_CACHE_SIZE)));
+        
+        LOG.info("Processing series complete (Ins/Upd/Del), result: {}", processingSeriesResult);
+    }
+    
+    private void processingAllBrandsUpdateAndDelete(File programFile,
+            ContentPopulatorSupplier contentPopulator) throws IOException {
+        RoviDataProcessingResult processingBrandsWithParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
+                new RoviProgramLineParser(),
+                FILE_CHARSET,
+                and(not(IS_INSERT), IS_BRAND),
+                contentWriter,
+                contentResolver,
+                contentPopulator
+                ));
+        
+        LOG.info("Processing all brands (Update and Delete) complete, result: {}", processingBrandsWithParentResult);
+    }
+
+    private void processingBrandsWithParentInsert(File programFile,
+            ContentPopulatorSupplier contentPopulator) throws IOException {
+        RoviDataProcessingResult processingBrandsWithParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
+                new RoviProgramLineParser(),
+                FILE_CHARSET,
+                and(IS_INSERT, IS_BRAND_WITH_PARENT),
+                contentWriter,
+                contentResolver,
+                contentPopulator
+                ));
+        
+        LOG.info("Processing brands with parent (Insert) complete, result: {}", processingBrandsWithParentResult);
+    }
+
+    private void processingBrandsWithoutParentInsert(File programFile,
+            ContentPopulatorSupplier contentPopulator) throws IOException {
+        RoviDataProcessingResult processingBrandsNoParentResult = Files.readLines(programFile, FILE_CHARSET, new RoviProgramLineIngestor(
+                new RoviProgramLineParser(),
+                FILE_CHARSET,
+                and(IS_INSERT, IS_BRAND_NO_PARENT),
+                contentWriter,
+                contentResolver,
+                contentPopulator
+                ));
+        
+        LOG.info("Processing brands with no parent (Insert) complete, result: {}", processingBrandsNoParentResult);
     }
     
 
