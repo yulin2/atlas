aa03fd48e0ffaa69a7b8461c7c941724ef4859b8
Tom McAdam <tom@metabroadcast.com>
2014-04-08 17:06:08 +0100

    Iterate over known channel aliases for redux latest ingest

--- src/main/java/org/atlasapi/remotesite/redux/ReduxLatestUpdateTasks.java
+++ src/main/java/org/atlasapi/remotesite/redux/ReduxLatestUpdateTasks.java
@@ -1,14 +1,19 @@
 package org.atlasapi.remotesite.redux;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.atlasapi.persistence.logging.AdapterLogEntry.errorEntry;
 
 import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Nullable;
+
+import org.atlasapi.media.channel.ChannelResolver;
 import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.persistence.content.ContentResolver;
@@ -21,6 +26,7 @@ import org.atlasapi.remotesite.redux.model.PaginatedBaseProgrammes;
 import com.google.common.base.Function;
 import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.metabroadcast.common.base.Maybe;
@@ -30,11 +36,12 @@ import com.metabroadcast.common.scheduling.UpdateProgress;
 
 public final class ReduxLatestUpdateTasks {
     
+    private static final String REDUX_CHANNEL_PREFIX = "http://devapi.bbcredux.com/channels/";
     private static final int SELECTION_LIMIT = 25;
     private static final Executor sharedExecutor = new ThreadPoolExecutor(0, 20, 1, TimeUnit.MINUTES, new SynchronousQueue<Runnable>(), new ThreadFactoryBuilder().setNameFormat("Redux Latest Thread %d").build(), new ThreadPoolExecutor.CallerRunsPolicy());
     
-    public static final ScheduledTask firstBatchOnlyReduxLatestTask(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, AdapterLog log) {
-        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, log) {
+    public static final ScheduledTask firstBatchOnlyReduxLatestTask(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, ChannelResolver channelResolver, AdapterLog log) {
+        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, channelResolver, log) {
             
             @Override
             protected boolean finished(PaginatedBaseProgrammes lastBatch) {
@@ -43,8 +50,8 @@ public final class ReduxLatestUpdateTasks {
         });
     }
 
-    public static final ScheduledTask untilFoundReduxLatestTask(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, AdapterLog log, final ContentResolver resolver) {
-        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, log) {
+    public static final ScheduledTask untilFoundReduxLatestTask(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, ChannelResolver channelResolver, AdapterLog log, final ContentResolver resolver) {
+        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, channelResolver, log) {
             
             @Override
             protected boolean finished(PaginatedBaseProgrammes lastBatch) {
@@ -56,8 +63,8 @@ public final class ReduxLatestUpdateTasks {
         });
     }
     
-    public static final ScheduledTask completeReduxLatestTask(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, AdapterLog log) {
-        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, log) {
+    public static final ScheduledTask completeReduxLatestTask(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, ChannelResolver channelResolver, AdapterLog log) {
+        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, channelResolver, log) {
             
             @Override
             protected boolean finished(PaginatedBaseProgrammes lastBatch) {
@@ -66,8 +73,8 @@ public final class ReduxLatestUpdateTasks {
         });
     }
     
-    public static final ScheduledTask maximumReduxLatestTask(final int maximum, ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, AdapterLog log) {
-        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, log) {
+    public static final ScheduledTask maximumReduxLatestTask(final int maximum, ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, ChannelResolver channelResolver, AdapterLog log) {
+        return taskFor(new BaseReduxLatestTaskProducer(client, writer, adapter, channelResolver, log) {
             
             private int seen = 0;
             private int max = maximum;
@@ -96,10 +103,12 @@ public final class ReduxLatestUpdateTasks {
         private final ReduxClient client;
         private final ReduxDiskrefUpdateTask.Builder taskBuilder;
         private final AdapterLog log;
+        private final ChannelResolver channelResolver;
 
-        public BaseReduxLatestTaskProducer(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, AdapterLog log) {
-            this.client = client;
-            this.log = log;
+        public BaseReduxLatestTaskProducer(ReduxClient client, ContentWriter writer, SiteSpecificAdapter<Item> adapter, ChannelResolver channelResolver, AdapterLog log) {
+            this.client = checkNotNull(client);
+            this.log = checkNotNull(log);
+            this.channelResolver = checkNotNull(channelResolver);
             this.taskBuilder = ReduxDiskrefUpdateTask.diskRefUpdateTaskBuilder(writer, adapter, log);
         }
         
@@ -133,11 +142,15 @@ public final class ReduxLatestUpdateTasks {
         
         private PaginatedBaseProgrammes batchForSelection(Selection selection) {
             try {
-                return client.latest(selection);
+                return client.latest(selection, channels());
             } catch (Exception e) {
                 log.record(errorEntry().withCause(e).withSource(getClass()).withDescription("Couldn't get Latest for %s", selection));
                 return null;
             }
+        }
+        
+        private Iterable<String> channels() {
+            return Iterables.transform(channelResolver.forAliases(REDUX_CHANNEL_PREFIX).keySet(), REMOVE_CHANNEL_PREFIX);
         }
         
         private Iterator<Callable<UpdateProgress>> transformedIterator(PaginatedBaseProgrammes latestBatch) {
@@ -150,6 +163,15 @@ public final class ReduxLatestUpdateTasks {
         }
         
         protected abstract boolean finished(PaginatedBaseProgrammes lastBatch);
+        
+        private static final Function<String, String> REMOVE_CHANNEL_PREFIX = new Function<String, String>() {
+
+            @Override
+            public String apply(String alias) {
+                return alias.replace(REDUX_CHANNEL_PREFIX, "");
+            }
+            
+        };
         
     }
     
