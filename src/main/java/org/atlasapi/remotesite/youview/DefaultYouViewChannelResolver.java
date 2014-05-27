89626f2301c9054170f31673d003ebc56f04dc68
Tom McAdam <tom@metabroadcast.com>
2014-05-27 17:46:23 +0100

    Ingest service-specific YouView schedules
    Their service IDs are provided by the PA for service ID
    10, BT TV. The PA channel ingester has been modified to
    ingest multiple YouView aliases, which are then used in
    the YouView ingester along with a map of YouView service
    to Atlas publisher, to separate out schedules by service.

--- src/main/java/org/atlasapi/remotesite/youview/DefaultYouViewChannelResolver.java
+++ src/main/java/org/atlasapi/remotesite/youview/DefaultYouViewChannelResolver.java
@@ -1,6 +1,7 @@
 package org.atlasapi.remotesite.youview;
 
-import java.util.List;
+import static org.atlasapi.remotesite.pa.channels.PaChannelsIngester.YOUVIEW_SERVICE_ID_ALIAS_PREFIXES;
+
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
@@ -8,34 +9,61 @@ import java.util.regex.Pattern;
 
 import org.atlasapi.media.channel.Channel;
 import org.atlasapi.media.channel.ChannelResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Maps;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 
 public class DefaultYouViewChannelResolver implements YouViewChannelResolver {
     
-    private static final String YOUVIEW_URI_PREFIX = "http://youview.com/service/";
-    static Pattern YOUVIEW_URI_MATCHER = Pattern.compile("^" + YOUVIEW_URI_PREFIX + "(\\d+)$");
+    private static final Logger log = LoggerFactory.getLogger(DefaultYouViewChannelResolver.class);
     
-    private final Map<Integer, Channel> channelMap = Maps.newHashMap();
+    private final Map<Integer, Channel> channelMap;
+    private final Map<Integer, String> aliasMap;
     
     public DefaultYouViewChannelResolver(ChannelResolver channelResolver) {
-        for (Entry<String, Channel> entry: channelResolver.forAliases(YOUVIEW_URI_PREFIX).entrySet()) {
-            Matcher m = YOUVIEW_URI_MATCHER.matcher(entry.getKey());
-            if(m.matches()) {
+        Builder<Integer, Channel> channelMapBuilder = ImmutableMap.builder();
+        Builder<Integer, String> aliasMapBuilder = ImmutableMap.builder();
+        
+        for (String prefix : YOUVIEW_SERVICE_ID_ALIAS_PREFIXES) {
+            buildEntriesForPrefix(channelResolver, prefix, channelMapBuilder, aliasMapBuilder);
+        }
+        
+        channelMap = channelMapBuilder.build();
+        aliasMap = aliasMapBuilder.build();
+    }
+    
+    private void buildEntriesForPrefix(ChannelResolver channelResolver, 
+            String prefix, Builder<Integer, Channel> channelMapBuilder,
+            Builder<Integer, String> aliasMapBuilder) {
+        
+        Pattern pattern = Pattern.compile("^" + prefix + "(\\d+)$");
+        for (Entry<String, Channel> entry: channelResolver.forAliases(prefix).entrySet()) {
+            Matcher m = pattern.matcher(entry.getKey());
+            if (m.matches()) {
                 Integer channel = Integer.decode(m.group(1));
-                channelMap.put(channel, entry.getValue());
+                
+                channelMapBuilder.put(channel, entry.getValue());
+                aliasMapBuilder.put(channel, entry.getKey());
+            } else {
+                log.error("Could not parse YouView alias " + entry.getKey());
             }
         }
     }
-    
+
     @Override
     public String getChannelUri(int channelId) {
         if (channelMap.containsKey(channelId)) {
             return channelMap.get(channelId).getUri();
         }
         return null;
+    }
+    
+    @Override
+    public String getChannelServiceAlias(int channelId) {
+        return aliasMap.get(channelId);
     }
     
     @Override
@@ -47,8 +75,13 @@ public class DefaultYouViewChannelResolver implements YouViewChannelResolver {
     }
 
     @Override
-    public List<Channel> getAllChannels() {
-        return ImmutableList.copyOf(channelMap.values());
+    public Iterable<Channel> getAllChannels() {
+        return channelMap.values();
+    }
+
+    @Override
+    public Map<Integer, Channel> getAllChannelsByServiceId() {
+        return channelMap;
     }
 
 }
