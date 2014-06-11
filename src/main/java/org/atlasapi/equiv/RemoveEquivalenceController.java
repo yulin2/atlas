79b3f7b930ce0f8d44fc7c40f2ddb65bf9377aad
Tom McAdam <tom@metabroadcast.com>
2014-06-11 17:06:06 +0100

    Add controller to break an equivalence
    Conflicts:
    	src/main/java/org/atlasapi/equiv/RemoveEquivalenceController.java

--- src/main/java/org/atlasapi/equiv/RemoveEquivalenceController.java
+++ src/main/java/org/atlasapi/equiv/RemoveEquivalenceController.java
@@ -2,14 +2,24 @@ package org.atlasapi.equiv;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.metabroadcast.common.http.HttpStatusCode;
+
 @Controller
 public class RemoveEquivalenceController {
 
+    private static final Logger log = LoggerFactory.getLogger(RemoveEquivalenceController.class);
+    
     private final EquivalenceBreaker equivalenceBreaker;
     
     public RemoveEquivalenceController(EquivalenceBreaker equivalenceBreaker) {
@@ -19,7 +29,15 @@ public class RemoveEquivalenceController {
     @RequestMapping(value="/system/equivalence/remove", method=RequestMethod.POST)
     public void removeEquivalence(
             @RequestParam(value="from", required=true) String sourceUri, 
-            @RequestParam(value="to", required=true) String targetUriToRemove) {
-        equivalenceBreaker.removeFromSet(sourceUri, targetUriToRemove);
+            @RequestParam(value="to", required=true) String targetUriToRemove,
+            HttpServletResponse response) throws IOException {
+        
+        try {
+            equivalenceBreaker.removeFromSet(sourceUri, targetUriToRemove);
+            response.setStatus(HttpStatusCode.OK.code());
+        } catch (Exception e) {
+            response.sendError(HttpStatusCode.SERVER_ERROR.code(), String.format("%s", e.getMessage()));
+            log.error("Error breaking equivalence from " + sourceUri + " to " + targetUriToRemove, e);
+        }
     }
 }
