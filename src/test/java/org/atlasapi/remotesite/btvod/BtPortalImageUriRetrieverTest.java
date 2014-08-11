3bd51782da841604032946d29c9973b9cb612bf2
Tom McAdam <tom@metabroadcast.com>
2014-08-11 16:53:07 +0100

    Retrieve VOD image URIs from BT portal

---
+++ src/test/java/org/atlasapi/remotesite/btvod/BtPortalImageUriRetrieverTest.java
@@ -0,0 +1,55 @@
+package org.atlasapi.remotesite.btvod;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+import com.metabroadcast.common.http.FixedResponseHttpClient;
+import com.metabroadcast.common.http.SimpleHttpClient;
+
+
+public class BtPortalImageUriRetrieverTest {
+
+    private static final String BASE_URI = "http://example.org/";
+    private static final String PRODUCT_ID_WITH_IMAGE = "1";
+    private static final String PRODUCT_ID_WITHOUT_IMAGE = "2";
+    private static final String PRODUCT_ID_WITH_NO_REMOTE_URI = "3";
+    
+    private String fileContentsFromResource(String resourceName)  {
+        try {
+            return Files.toString(new File(Resources.getResource(getClass(), resourceName).getFile()), Charsets.UTF_8);
+        } catch (IOException e) {
+            Throwables.propagate(e);
+        }
+        return null;
+    }
+    
+    private final SimpleHttpClient httpClient = new FixedResponseHttpClient(
+            ImmutableMap.of(BASE_URI + "xml/" + PRODUCT_ID_WITH_IMAGE + ".xml", 
+                            fileContentsFromResource("bt-vod-example.xml"),
+                            BASE_URI + "xml/" + PRODUCT_ID_WITHOUT_IMAGE + ".xml",
+                            fileContentsFromResource("bt-vod-example-no-image.xml")));
+    
+    private final BtPortalImageUriProvider uriRetriever = new BtPortalImageUriProvider(httpClient, BASE_URI);
+    
+    @Test
+    public void testRetrievesImage() {
+        assertThat(uriRetriever.imageUriFor(PRODUCT_ID_WITH_IMAGE).get(), is(BASE_URI + "b.png"));
+    }
+    
+    @Test
+    public void testReturnsAbsentIfAttributeMissing() {
+        assertFalse(uriRetriever.imageUriFor(PRODUCT_ID_WITHOUT_IMAGE).isPresent());
+    }
+    
+}
--- src/test/java/org/atlasapi/remotesite/btvod/BtVodItemWriterTest.java
+++ src/test/java/org/atlasapi/remotesite/btvod/BtVodItemWriterTest.java
@@ -22,7 +22,9 @@ import org.joda.time.DateTimeConstants;
 import org.joda.time.DateTimeZone;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -33,6 +35,7 @@ import com.google.common.collect.ImmutableMap.Builder;
 
 public class BtVodItemWriterTest {
 
+    private static final String IMAGE_URI = "http://example.org/123.png";
     private static final String IMAGE_FILENAME = "image.png";
     private static final String PRODUCT_ID = "1234";
     private static final String EPISODE_TITLE = "Title";
@@ -45,6 +48,7 @@ public class BtVodItemWriterTest {
     private final BtVodBrandWriter brandExtractor = mock(BtVodBrandWriter.class);
     private final BtVodSeriesWriter seriesExtractor = mock(BtVodSeriesWriter.class);
     private final BtVodContentListener contentListener = mock(BtVodContentListener.class);
+    private final ImageUriProvider imageUriProvider = mock(ImageUriProvider.class);
     
     private final BtVodItemWriter itemExtractor 
                     = new BtVodItemWriter(
@@ -54,14 +58,14 @@ public class BtVodItemWriterTest {
                                 seriesExtractor,
                                 PUBLISHER, URI_PREFIX,
                                 contentListener,
-                                new BtVodDescribedFieldsExtractor(),
+                                new BtVodDescribedFieldsExtractor(imageUriProvider),
                                 Sets.<String>newHashSet());
     
     @Test
     public void testExtractsEpisode() {
         when(contentResolver.findByCanonicalUris(ImmutableSet.of(itemUri())))
                 .thenReturn(ResolvedContent.builder().build());
-        
+        when(imageUriProvider.imageUriFor(PRODUCT_ID)).thenReturn(Optional.<String>of(IMAGE_URI));
         itemExtractor.process(itemRow());
         
         ArgumentCaptor<Item> itemCaptor = ArgumentCaptor.forClass(Item.class);
@@ -73,7 +77,7 @@ public class BtVodItemWriterTest {
         assertThat(writtenItem.getDescription(), is(SYNOPSIS));
         
         Image image = Iterables.getOnlyElement(writtenItem.getImages());
-        assertThat(image.getCanonicalUri(), is("http://portal.vision.bt.com/btvo/" + IMAGE_FILENAME));
+        assertThat(image.getCanonicalUri(), is(IMAGE_URI));
         assertThat(image.getType(), is(ImageType.PRIMARY));
         
         Location location = Iterables.getOnlyElement(
@@ -83,7 +87,7 @@ public class BtVodItemWriterTest {
                                             .getAvailableAt());
         
         DateTime expectedAvailabilityStart = new DateTime(2013, DateTimeConstants.MARCH, 31, 23, 0, 0, 0, DateTimeZone.UTC);
-        DateTime expectedAvailabilityEnd = new DateTime(2014, DateTimeConstants.APRIL, 30, 0, 0, 0).withZone(DateTimeZone.UTC);
+        DateTime expectedAvailabilityEnd = new DateTime(2014, DateTimeConstants.APRIL, 30, 00, 0, 0).withZone(DateTimeZone.UTC);
         assertThat(location.getPolicy().getAvailabilityStart(), is(expectedAvailabilityStart));
         assertThat(location.getPolicy().getAvailabilityEnd(), is(expectedAvailabilityEnd));
         //assertThat(Iterables.getOnlyElement(location.getPolicy().getAvailableCountries()).code(), is("GB"));
@@ -115,6 +119,7 @@ public class BtVodItemWriterTest {
         rows.put(BtVodFileColumn.SYNOPSIS.key(), SYNOPSIS);
         rows.put(BtVodFileColumn.PRODUCT_YOUVIEW_DURATION.key(), "PT0H03M39.458S");
         rows.put(BtVodFileColumn.METADATA_DURATION.key(), "PT00H03M36S");
+        rows.put(BtVodFileColumn.ASSET_TITLE.key(), "Title");
         
         Map<String, String> map = rows.build();
         return new BtVodDataRow(ImmutableList.copyOf(map.values()), 
