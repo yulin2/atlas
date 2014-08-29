3e2e4f9771d64595fee67e367c4968b010bb3c73
Tom Spurling <tim@metabroadcast.com>
2014-08-29 13:25:05 +0100

    Implement topic search cache persistence

--- src/main/java/org/atlasapi/remotesite/knowledgemotion/topics/cache/KeyphraseTopicCache.java
+++ src/main/java/org/atlasapi/remotesite/knowledgemotion/topics/cache/KeyphraseTopicCache.java
@@ -1,14 +1,30 @@
 package org.atlasapi.remotesite.knowledgemotion.topics.cache;
 
+import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+
 public class KeyphraseTopicCache {
 
+    private static final String COLLECTION_NAME = "spotlightTopicsCache";
+
+    private final DBCollection collection;
+    private final KeyphraseTopicCacheRowTranslator translator = new KeyphraseTopicCacheRowTranslator();
+
+    public KeyphraseTopicCache(DatabasedMongo mongo) {
+        this.collection = mongo.collection(COLLECTION_NAME);
+    }
+
     public KeyphraseTopicCacheRow get(String keyPhrase) {
-        // TODO load
-        return KeyphraseTopicCacheRow.newCacheRow(keyPhrase);
+        DBObject maybeFound = collection.findOne(keyPhrase);
+        if (maybeFound == null) {
+            return KeyphraseTopicCacheRow.newCacheRow(keyPhrase);
+        }
+        return translator.fromDBObject(maybeFound, null);
     }
 
     public void update(KeyphraseTopicCacheRow cacheRow) {
-        // TODO save
+        collection.save(translator.toDBObject(null, cacheRow));
     }
 
 }
