72d4687bf47c712b5c0fa2f22d3d9918a127513b
Tom Spurling <tim@metabroadcast.com>
2014-09-01 17:44:52 +0100

    Do awful correctness-checking manual topic merge

--- src/main/java/org/atlasapi/remotesite/knowledgemotion/DefaultKnowledgeMotionDataRowHandler.java
+++ src/main/java/org/atlasapi/remotesite/knowledgemotion/DefaultKnowledgeMotionDataRowHandler.java
@@ -2,11 +2,15 @@ package org.atlasapi.remotesite.knowledgemotion;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.HashMap;
+
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.TopicRef;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
+import org.atlasapi.persistence.topic.TopicQueryResolver;
 import org.atlasapi.remotesite.ContentExtractor;
 import org.atlasapi.remotesite.ContentMerger;
 import org.atlasapi.remotesite.ContentMerger.MergeStrategy;
@@ -23,11 +27,31 @@ public class DefaultKnowledgeMotionDataRowHandler implements KnowledgeMotionData
     private final ContentMerger contentMerger;
 
     public DefaultKnowledgeMotionDataRowHandler(ContentResolver resolver, ContentWriter writer, 
-            ContentExtractor<KnowledgeMotionDataRow, Optional<? extends Content>> extractor) {
+            ContentExtractor<KnowledgeMotionDataRow, Optional<? extends Content>> extractor, final TopicQueryResolver topicStore) {
         this.resolver = checkNotNull(resolver);
         this.writer = checkNotNull(writer);
         this.extractor = checkNotNull(extractor);
-        this.contentMerger = new ContentMerger(MergeStrategy.MERGE);
+        this.contentMerger = new ContentMerger(MergeStrategy.MERGE){
+            @Override
+            public Item merge(Item current, Item extracted) {
+                Item merged = super.merge(current, extracted);
+
+                HashMap<Long, TopicRef> mergedRefs = new HashMap<Long, TopicRef>();
+                for (TopicRef topicRef : merged.getTopicRefs()) {
+                    if (topicStore.topicForId(topicRef.getTopic()).hasValue()){
+                        // this check is only needed because at one point i accidentally created refs to topics that weren't written properly
+                        mergedRefs.put(topicRef.getTopic(), topicRef);
+                    }
+                }
+                for (TopicRef topicRef : extracted.getTopicRefs()) {
+                    mergedRefs.put(topicRef.getTopic(), topicRef);
+                }
+
+                merged.setTopicRefs(mergedRefs.values());
+                return merged;
+            }
+            // TODO support merging TopicRefs in ContentMerger -- but remember to handle offsets!
+        };
     }
 
     @Override
