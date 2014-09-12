4b2b3043c50eee2508ff7b91213056f2bd3d5051
Jamie Perkins <mail@jperkins.co.uk>
2014-09-12 11:27:50 +0100

    Simplifier to convert DateTime to Date

--- src/main/java/org/atlasapi/output/simple/ItemModelSimplifier.java
+++ src/main/java/org/atlasapi/output/simple/ItemModelSimplifier.java
@@ -309,8 +309,8 @@ public class ItemModelSimplifier extends ContentModelSimplifier<Item, org.atlasa
         org.atlasapi.media.entity.simple.Broadcast simpleModel = new org.atlasapi.media.entity.simple.Broadcast(broadcast.getBroadcastOn(), broadcast.getTransmissionTime(),
                 broadcast.getTransmissionEndTime(), broadcast.getSourceId());
 
-        simpleModel.setActualTransmissionTime(broadcast.getActualTransmissionTime());
-        simpleModel.setActualTransmissionEndTime(broadcast.getActualTransmissionEndTime());
+        simpleModel.setActualTransmissionTime(broadcast.getActualTransmissionTime().toDate());
+        simpleModel.setActualTransmissionEndTime(broadcast.getActualTransmissionEndTime().toDate());
         simpleModel.setRepeat(broadcast.getRepeat());
         simpleModel.setSubtitled(broadcast.getSubtitled());
         simpleModel.setSigned(broadcast.getSigned());
