45297768fe7f3f46f20dd2af208de739f34832b2
Tom McAdam <tom@metabroadcast.com>
2014-09-13 05:47:38 +0100

    Beginnings of brand synthesizing

--- src/main/java/org/atlasapi/remotesite/btvod/BtVodBrandWriter.java
+++ src/main/java/org/atlasapi/remotesite/btvod/BtVodBrandWriter.java
@@ -4,8 +4,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Map;
 import java.util.Set;
-
-import joptsimple.internal.Strings;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.atlasapi.media.entity.Brand;
 import org.atlasapi.media.entity.Container;
@@ -20,13 +20,24 @@ import org.atlasapi.remotesite.btvod.BtVodData.BtVodDataRow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
+import com.google.common.primitives.Ints;
 import com.metabroadcast.common.base.Maybe;
 import com.metabroadcast.common.scheduling.UpdateProgress;
 
+/**
+ * Create a {@link Brand} from a {@link BtVodDataRow}. This may either be
+ * from brand information contained explicitly in a row, or synthesized 
+ * from episode data
+ * 
+ * @author tom
+ *
+ */
 public class BtVodBrandWriter implements BtVodDataProcessor<UpdateProgress> {
 
+    private static final Pattern BRAND_TITLE_FROM_EPISODE_PATTERN = Pattern.compile("^(.*)\\ S[0-9]+\\-E.*");
     private static final Logger log = LoggerFactory.getLogger(BtVodBrandWriter.class);
     private static final boolean CONTINUE = true;
     
@@ -59,9 +70,9 @@ public class BtVodBrandWriter implements BtVodDataProcessor<UpdateProgress> {
         UpdateProgress thisProgress = UpdateProgress.FAILURE;
         try {
             String brandId = row.getColumnValue(BtVodFileColumn.BRANDIA_ID);
-            if (Strings.isNullOrEmpty(brandId)
+            if ( ( Strings.isNullOrEmpty(brandId) && !shouldSynthesizeBrand(row))
                     || processedBrands.containsKey(brandId)
-                    || processedRows.contains(row.getColumnValue(BtVodFileColumn.PRODUCT_ID))) {
+                    || processedRows.contains(getKey(row))) {
                 thisProgress = UpdateProgress.SUCCESS;
                 return CONTINUE;
             }
@@ -77,6 +88,29 @@ public class BtVodBrandWriter implements BtVodDataProcessor<UpdateProgress> {
             progress = progress.reduce(thisProgress);
         }
         return CONTINUE;
+    }
+
+    private String getKey(BtVodDataRow row) {
+        if (shouldSynthesizeBrand(row)) {
+            return brandTitleFromEpisodeTitle(row.getColumnValue(BtVodFileColumn.PRODUCT_TITLE));
+        } else {
+            return row.getColumnValue(BtVodFileColumn.PRODUCT_ID);
+        }
+    }
+    
+    private String brandTitleFromEpisodeTitle(String title) {
+        Matcher matcher = BRAND_TITLE_FROM_EPISODE_PATTERN.matcher(title);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    private boolean shouldSynthesizeBrand(BtVodDataRow row) {
+        String seriesNumber = row.getColumnValue(BtVodFileColumn.SERIES_NUMBER);
+        return Strings.isNullOrEmpty(row.getColumnValue(BtVodFileColumn.BRANDIA_ID))
+                && seriesNumber != null 
+                && Ints.tryParse(seriesNumber) != null;
     }
 
     private void write(Brand extracted) {
@@ -96,13 +130,21 @@ public class BtVodBrandWriter implements BtVodDataProcessor<UpdateProgress> {
 
     private Brand brandFrom(BtVodDataRow row) {
         Brand brand = new Brand(uriFor(row), null, publisher);
-        brand.setTitle(row.getColumnValue(BtVodFileColumn.BRAND_TITLE));
+        if (shouldSynthesizeBrand(row)) {
+            brand.setTitle(brandTitleFromEpisodeTitle(row.getColumnValue(BtVodFileColumn.PRODUCT_TITLE)));
+        } else {
+            brand.setTitle(row.getColumnValue(BtVodFileColumn.BRAND_TITLE));
+        }
         describedFieldsExtractor.setDescribedFieldsFrom(row, brand);
         return brand;
     }
     
     private String uriFor(BtVodDataRow row) {
-        return uriPrefix + "brands/" + row.getColumnValue(BtVodFileColumn.BRANDIA_ID);
+        if (shouldSynthesizeBrand(row)) {
+          return uriPrefix + "synthesized/brands/" + getKey(row);
+        } else {
+            return uriPrefix + "brands/" + row.getColumnValue(BtVodFileColumn.BRANDIA_ID);
+        }
     }
 
     @Override
--- src/test/java/org/atlasapi/remotesite/btvod/BtVodBrandWriterTest.java
+++ src/test/java/org/atlasapi/remotesite/btvod/BtVodBrandWriterTest.java
@@ -2,6 +2,9 @@ package org.atlasapi.remotesite.btvod;
 
 import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,6 +19,7 @@ import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.remotesite.btvod.BtVodData.BtVodDataRow;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -59,6 +63,35 @@ public class BtVodBrandWriterTest {
         verify(contentWriter).createOrUpdate(expectedBrand());
     }
     
+    @Test 
+    public void testCreatesSyntheticBrandFromEpisodeData() {
+        when(imageUriProvider.imageUriFor(Matchers.anyString())).thenReturn(Optional.<String>absent());
+        when(contentResolver.findByCanonicalUris(ImmutableSet.of(URI_PREFIX + "/synthesized/brands/perception")))
+            .thenReturn(ResolvedContent.builder().build());
+        
+        brandExtractor.process(episodeRow());
+        
+        ArgumentCaptor<Brand> captor = ArgumentCaptor.forClass(Brand.class);
+        verify(contentWriter).createOrUpdate(captor.capture());
+        
+        Brand saved = captor.getValue();
+        assertThat(saved.getCanonicalUri(), is(URI_PREFIX + "/synthesized/brands/perception"));
+        assertThat(saved.getTitle(), is("Perception"));
+    }
+    
+    private BtVodDataRow episodeRow() {
+        Builder<String, String> rows = ImmutableMap.builder();
+        rows.put(BtVodFileColumn.PRODUCT_TITLE.key(), "Perception S1-E9 Shadow");
+        rows.put(BtVodFileColumn.BRANDIA_ID.key(), "");
+        rows.put(BtVodFileColumn.PRODUCT_ID.key(), "DIS000604904_RL1");
+        rows.put(BtVodFileColumn.SERIES_NUMBER.key(), "2");
+        rows.put(BtVodFileColumn.SYNOPSIS.key(), "");
+        
+        Map<String, String> map = rows.build();
+        return new BtVodDataRow(ImmutableList.copyOf(map.values()), 
+                ImmutableList.copyOf(map.keySet()));
+    }
+    
     private BtVodDataRow brandRow() {
         Builder<String, String> rows = ImmutableMap.builder();
         rows.put(BtVodFileColumn.BRANDIA_ID.key(), BRAND_ID);
@@ -66,7 +99,6 @@ public class BtVodBrandWriterTest {
         rows.put(BtVodFileColumn.PRODUCT_ID.key(), BRAND_ID);
         rows.put(BtVodFileColumn.SYNOPSIS.key(), "A synopsis");
         rows.put(BtVodFileColumn.PACKSHOT.key(), "");
-        
         Map<String, String> map = rows.build();
         return new BtVodDataRow(ImmutableList.copyOf(map.values()), 
                 ImmutableList.copyOf(map.keySet()));
