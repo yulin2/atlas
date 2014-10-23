9271e86b8dead7e5cb645de2e91ece790232e5e4
Massimiliano Tomassi <max@metabroadcast.com>
2014-10-23 15:19:30 +0100

    Taking into account categories for an equivalence task

--- src/main/java/org/atlasapi/equiv/update/tasks/ContentEquivalenceUpdateTask.java
+++ src/main/java/org/atlasapi/equiv/update/tasks/ContentEquivalenceUpdateTask.java
@@ -2,17 +2,20 @@ package org.atlasapi.equiv.update.tasks;
 
 import static com.metabroadcast.common.scheduling.UpdateProgress.FAILURE;
 import static com.metabroadcast.common.scheduling.UpdateProgress.SUCCESS;
+import static java.util.Arrays.asList;
 import static org.atlasapi.persistence.content.ContentCategory.CONTAINER;
 import static org.atlasapi.persistence.content.ContentCategory.TOP_LEVEL_ITEM;
 import static org.atlasapi.persistence.content.listing.ContentListingCriteria.defaultCriteria;
 import static org.atlasapi.persistence.content.listing.ContentListingProgress.progressFrom;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.lang3.StringUtils;
 import org.atlasapi.equiv.update.EquivalenceUpdater;
 import org.atlasapi.equiv.update.RootEquivalenceUpdater;
 import org.atlasapi.media.entity.Content;
@@ -23,7 +26,9 @@ import org.atlasapi.persistence.content.listing.ContentLister;
 import org.atlasapi.persistence.content.listing.ContentListingCriteria;
 import org.atlasapi.persistence.content.listing.ContentListingProgress;
 
+import com.google.api.client.util.Lists;
 import com.google.common.base.Joiner;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.metabroadcast.common.scheduling.UpdateProgress;
@@ -34,7 +39,6 @@ public final class ContentEquivalenceUpdateTask extends AbstractContentListingTa
     private final EquivalenceUpdater<Content> updater;    
     private final Set<String> ignored;
 
-    private String schedulingKey = "equivalence";
     private List<Publisher> publishers;
     private int processed = 0;
     private UpdateProgress progress = UpdateProgress.START;
@@ -49,7 +53,6 @@ public final class ContentEquivalenceUpdateTask extends AbstractContentListingTa
 
     public ContentEquivalenceUpdateTask forPublishers(Publisher... publishers) {
         this.publishers = ImmutableList.copyOf(publishers);
-        this.schedulingKey = Joiner.on("-").join(Iterables.transform(this.publishers, Publisher.TO_KEY))+"-equivalence";
         return this;
     }
     
@@ -60,7 +63,7 @@ public final class ContentEquivalenceUpdateTask extends AbstractContentListingTa
 
     @Override
     protected ContentListingProgress getProgress() {
-        return progressStore.progressForTask(schedulingKey);
+        return progressStore.progressForTask(schedulingKey());
     }
 
     @Override
@@ -82,7 +85,7 @@ public final class ContentEquivalenceUpdateTask extends AbstractContentListingTa
     
     @Override
     protected void onStart(ContentListingProgress progress) {
-        log.info("Started: {} from {}", schedulingKey, describe(progress));
+        log.info("Started: {} from {}", schedulingKey(), describe(progress));
         processed  = 0;
         this.progress = UpdateProgress.START;
     }
@@ -120,17 +123,31 @@ public final class ContentEquivalenceUpdateTask extends AbstractContentListingTa
     private void persistProgress(boolean finished, Content content) {
         if (finished) {
             updateProgress(ContentListingProgress.START);
-            log.info("Finished: {}", schedulingKey);
+            log.info("Finished: {}", schedulingKey());
         } else {
             if (content != null) {
                 updateProgress(progressFrom(content));
-                log.info("Stopped: {}", schedulingKey, content);
+                log.info("Stopped: {}", schedulingKey(), content);
             }
         }
     }
 
     private void updateProgress(ContentListingProgress progress) {
-        progressStore.storeProgress(schedulingKey, progress);
+        progressStore.storeProgress(schedulingKey(), progress);
+    }
+
+    private String schedulingKey() {
+        List<String> parts = Lists.newArrayList();
+
+        if (categories != null) {
+            parts = FluentIterable.from(asList(categories))
+                    .transform(ContentCategory.TO_NAME)
+                    .toList();
+        }
+
+        parts.addAll(FluentIterable.from(this.publishers).transform(Publisher.TO_KEY).toList());
+
+        return Joiner.on("-").join(parts)+"-equivalence";
     }
 
 }
