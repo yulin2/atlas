18f26dd3fe154cd07d2634a7a93b89f88e94a2d5
Tom Spurling <tim@metabroadcast.com>
2014-11-04 18:51:50 +0000

    ContentMerger now merges topics!
    - ContentMerger now requires a TopicMergeStrategy
    
    - Available strategies include:
      - Throw away new topics
      - Throw away old topics
      - Replace topics based on a given equivalence

--- src/test/java/org/atlasapi/remotesite/ContentMergerTest.java
+++ src/test/java/org/atlasapi/remotesite/ContentMergerTest.java
@@ -1,13 +1,21 @@
 package org.atlasapi.remotesite;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.TopicRef;
 import org.atlasapi.media.entity.Version;
 import org.atlasapi.remotesite.ContentMerger.MergeStrategy;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
+import com.google.common.base.Equivalence;
+import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.metabroadcast.common.time.DateTimeZones;
@@ -59,6 +67,45 @@ public class ContentMergerTest {
         Item merged = contentMerger.merge(current, extracted);
         
         assertEquals("http://example.org/2", Iterables.getOnlyElement(merged.getVersions()).getCanonicalUri());
+    }
+
+    @Test
+    public void testTopicMergerOnSuppliedEquivalence() {
+        final ContentMerger contentMerger = new ContentMerger(MergeStrategy.KEEP, MergeStrategy.replaceTopicsBasedOn(new Equivalence<TopicRef>() {
+            @Override
+            protected boolean doEquivalent(TopicRef a, TopicRef b) {
+                return Objects.equal(a.getOffset(), b.getOffset());
+            }
+
+            @Override
+            protected int doHash(TopicRef topicRef) {
+                return Objects.hashCode(topicRef.getOffset());
+            }
+        }));
+
+        TopicRef a1 = new TopicRef(9000L, 0f, false, TopicRef.Relationship.ABOUT, 45);
+        TopicRef a2 = new TopicRef(9001L, 0f, true, TopicRef.Relationship.TRANSCRIPTION, 45);
+        TopicRef b1 = new TopicRef(9000L, 0f, false, TopicRef.Relationship.ABOUT, 450);
+        TopicRef b2 = new TopicRef(9001L, 0f, true, TopicRef.Relationship.TRANSCRIPTION, 450);
+        TopicRef c1 = new TopicRef(9000L, 0f, false, TopicRef.Relationship.ABOUT, 324324);
+        TopicRef d2 = new TopicRef(9001L, 0f, true, TopicRef.Relationship.TRANSCRIPTION, 234098);
+        TopicRef n1 = new TopicRef(201L, 0f, true, TopicRef.Relationship.ABOUT);
+        TopicRef n2 = new TopicRef(9001L, 0f, true, TopicRef.Relationship.ABOUT);
+
+        Item current = new Item();
+        current.setTopicRefs(ImmutableList.of(a1, b1, c1, n1));
+
+        Item extracted = new Item();
+        extracted.setTopicRefs(ImmutableList.of(a2, b2, d2, n2));
+
+        Item merged = contentMerger.merge(current, extracted);
+        List<TopicRef> mergedRefs = merged.getTopicRefs();
+        assertEquals(5, mergedRefs.size());
+        assertTrue(mergedRefs.contains(a2));
+        assertTrue(mergedRefs.contains(b2));
+        assertTrue(mergedRefs.contains(c1));
+        assertTrue(mergedRefs.contains(d2));
+        assertTrue(mergedRefs.contains(n2));
     }
 
 }
