d96183ea8798205dfc7bc0e8b11e6068f677e15c
Jamie <jamie@metabroadcast.com>
2014-11-18 11:10:02 +0000

    Set version on item in segment transformation

--- src/main/java/org/atlasapi/input/SegmentModelTransformer.java
+++ src/main/java/org/atlasapi/input/SegmentModelTransformer.java
@@ -1,13 +1,21 @@
 package org.atlasapi.input;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import org.atlasapi.media.entity.Description;
 import org.atlasapi.media.segment.SegmentEvent;
 import org.atlasapi.media.segment.SegmentRef;
 import org.joda.time.Duration;
 
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+
 public class SegmentModelTransformer {
 
     public SegmentEvent transform(org.atlasapi.media.entity.simple.SegmentEvent segmentEvent) {
+        checkArgument(segmentEvent.getSegment() != null
+                && !Strings.isNullOrEmpty(segmentEvent.getSegment().getId()),
+                    "You must specify a segment ID");
+
         SegmentEvent complex = new SegmentEvent();
         complex.setPosition(segmentEvent.getPosition());
         complex.setIsChapter(segmentEvent.getIsChapter());
