9f5121d2fbdd6aab91c271212539eeea2809019c
Massimiliano Tomassi <max@metabroadcast.com>
2014-11-21 12:53:19 +0000

    Ingested bid for pips authority in broadcasts

--- src/test/java/org/atlasapi/remotesite/bbc/nitro/NitroBroadcastExtractorTest.java
+++ src/test/java/org/atlasapi/remotesite/bbc/nitro/NitroBroadcastExtractorTest.java
@@ -1,25 +1,45 @@
 package org.atlasapi.remotesite.bbc.nitro;
 
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import org.atlasapi.media.entity.Alias;
 import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.remotesite.bbc.nitro.extract.NitroBroadcastExtractor;
+import org.hamcrest.Matchers;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
+import com.google.api.client.repackaged.com.google.common.base.Joiner;
 import com.google.common.base.Optional;
+import com.google.common.collect.Iterables;
 import com.metabroadcast.atlas.glycerin.model.BroadcastTime;
+import com.metabroadcast.atlas.glycerin.model.Id;
+import com.metabroadcast.atlas.glycerin.model.Ids;
 import com.metabroadcast.atlas.glycerin.model.ServiceReference;
 import com.metabroadcast.common.time.DateTimeZones;
 
 
 public class NitroBroadcastExtractorTest {
+
+    private static final String TERRESTRIAL_EVENT_LOCATOR_TYPE = "terrestrial_event_locator";
+    private static final String TERRESTRIAL_PROGRAM_CRID_TYPE = "terrestrial_programme_crid";
+    private static final String BID_TYPE = "bid";
+    private static final String PIPS_AUTHORITY = "pips";
+    private static final String TELEVIEW_AUTHORITY = "teleview";
+    private static final String TEL_VALUE = "tel value";
+    private static final String CRID_VALUE = "crid value";
+    private static final String BID_VALUE = "bid value";
+    private static final String ALIAS_PREFIX = "bbc";
+    private static final String OTHER_AUTHORITY = "other";
 
     private final NitroBroadcastExtractor extractor
         = new NitroBroadcastExtractor();
@@ -42,6 +62,7 @@ public class NitroBroadcastExtractorTest {
         publishedTime.setEnd(end);
         nitro.setPublishedTime(publishedTime);
         nitro.setIsAudioDescribed(true);
+        nitro.setIds(ids());
         
         nitro.setIsRepeat(true);
         nitro.setIsAudioDescribed(true);
@@ -55,8 +76,38 @@ public class NitroBroadcastExtractorTest {
         assertThat(atlas.getSourceId(), is("bbc:p01g1w71"));
         assertTrue(atlas.getRepeat());
         assertTrue(atlas.getAudioDescribed());
-        assertTrue(atlas.getAudioDescribed());
-        
+        assertThat(atlas.getAliases(), hasItem(alias(TERRESTRIAL_EVENT_LOCATOR_TYPE, TELEVIEW_AUTHORITY, TEL_VALUE)));
+        assertThat(atlas.getAliases(), hasItem(alias(TERRESTRIAL_PROGRAM_CRID_TYPE, TELEVIEW_AUTHORITY, CRID_VALUE)));
+        assertThat(atlas.getAliases(), hasItem(alias(BID_TYPE, PIPS_AUTHORITY, BID_VALUE)));
+        assertThat(atlas.getAliases(), Matchers.not(hasItem(alias(BID_TYPE, OTHER_AUTHORITY, BID_VALUE))));
+    }
+
+    private Alias alias(String type, String authority, String value) {
+        return new Alias(namespace(type, authority), value);
+    }
+
+    private String namespace(String type, String authority) {
+        return Joiner.on(':').join(ALIAS_PREFIX, type, authority);
+    }
+
+    private Ids ids() {
+        Ids ids = new Ids();
+
+        ids.getId().add(id(TERRESTRIAL_EVENT_LOCATOR_TYPE, TELEVIEW_AUTHORITY, TEL_VALUE));
+        ids.getId().add(id(TERRESTRIAL_PROGRAM_CRID_TYPE, TELEVIEW_AUTHORITY, CRID_VALUE));
+        ids.getId().add(id(BID_TYPE, PIPS_AUTHORITY, BID_VALUE));
+
+        return ids;
+    }
+
+    private Id id(String type, String authority, String value) {
+        Id id = new Id();
+
+        id.setType(type);
+        id.setAuthority(authority);
+        id.setValue(value);
+
+        return id;
     }
 
     private XMLGregorianCalendar xmlGregCal(String lexicalRepresentation)
