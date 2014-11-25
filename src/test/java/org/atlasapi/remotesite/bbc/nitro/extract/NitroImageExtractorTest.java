d76782b38b004fc5072cb6e6c0e69255f90b7989
Tom McAdam <tom@metabroadcast.com>
2014-11-25 16:26:50 +0000

    Use correct Glycerin image type
    The cherry-pick was from a branch with a newer version of
    Glycerin, where the Image type has moved packages.

--- src/test/java/org/atlasapi/remotesite/bbc/nitro/extract/NitroImageExtractorTest.java
+++ src/test/java/org/atlasapi/remotesite/bbc/nitro/extract/NitroImageExtractorTest.java
@@ -5,15 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.atlasapi.media.entity.Image;
 import org.junit.Test;
 
-import com.metabroadcast.atlas.glycerin.model.Brand;
-
 
 public class NitroImageExtractorTest {
 
     @Test
     public void testImageExtraction() {
         NitroImageExtractor extractor = new NitroImageExtractor(1024, 576);
-        Brand.Image source = new Brand.Image();
+        com.metabroadcast.atlas.glycerin.model.Image source = new com.metabroadcast.atlas.glycerin.model.Image();
         source.setTemplateUrl("http://hostname/image_$recipe");
 
         Image extracted = extractor.extract(source);
