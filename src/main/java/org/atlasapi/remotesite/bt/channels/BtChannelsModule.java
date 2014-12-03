7b69d98b14c488a61be5fc0bb2ca4f3a70b889e9
Tom McAdam <tom@metabroadcast.com>
2014-12-03 09:11:51 +0000

    Add multiple BT channels environments

--- src/main/java/org/atlasapi/remotesite/bt/channels/BtChannelsModule.java
+++ src/main/java/org/atlasapi/remotesite/bt/channels/BtChannelsModule.java
@@ -1,5 +1,6 @@
 package org.atlasapi.remotesite.bt.channels;
 
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 import javax.annotation.PostConstruct;
@@ -27,12 +28,26 @@ import com.metabroadcast.common.scheduling.SimpleScheduler;
 @Import( PaModule.class )
 public class BtChannelsModule {
 
-    private static final String URI_PREFIX = "http://tv-channels.bt.com/";
+    private static final String URI_PREFIX_STRING_FORMAT = "http://%s/";
 
-    private static final RepetitionRule DAILY_AT_3PM = RepetitionRules.daily(new LocalTime(15,0));
+    private static final RepetitionRule PROD_INGEST_REPETITION = RepetitionRules.daily(new LocalTime(15,0));
+    private static final LocalTime NON_PROD_BASE_START_TIME = new LocalTime(16,0);
     
-    @Value("${bt.channels.baseUri}")
+    private static final RepetitionRule TEST1_INGEST_REPETITION = RepetitionRules.daily(NON_PROD_BASE_START_TIME);
+    private static final RepetitionRule TEST2_INGEST_REPETITION = RepetitionRules.daily(NON_PROD_BASE_START_TIME.plusHours(1));
+    private static final RepetitionRule REFERENCE_INGEST_REPETITION = RepetitionRules.daily(NON_PROD_BASE_START_TIME.plusHours(2));
+    
+    @Value("${bt.channels.baseUri.production}")
     private String baseUri;
+    
+    @Value("${bt.channels.baseUri.test1}")
+    private String test1BaseUri;
+    
+    @Value("${bt.channels.baseUri.test2}")
+    private String test2BaseUri;
+    
+    @Value("${bt.channels.baseUri.reference}")
+    private String test3BaseUri;
     
     @Value("${bt.channels.freeviewPlatformChannelGroupId}")
     private String freeviewPlatformChannelGroupId;
@@ -53,7 +68,7 @@ public class BtChannelsModule {
     private SimpleScheduler scheduler;
     
     @Autowired
-    private WriteLock channelWriterLock;
+    private ReentrantLock channelWriterLock;
     
     @Bean
     public BtMpxClient btMpxClient() {
@@ -66,22 +81,60 @@ public class BtChannelsModule {
     
     @Bean 
     public BtChannelGroupUpdater productionChannelGroupUpdater() {
-        return new BtChannelGroupUpdater(btMpxClient(), Publisher.BT_TV_CHANNELS, 
-                URI_PREFIX,  "bt", channelGroupResolver, channelGroupWriter, 
-                channelResolver, channelWriter, allChannelsUpdater(), channelWriterLock);
+        return perEnvironmentChannelGroupUpdater(Publisher.BT_TV_CHANNELS, "bt", baseUri);
     }
     
-    @Bean
-    public BtAllChannelsChannelGroupUpdater allChannelsUpdater() {
-        return new BtAllChannelsChannelGroupUpdater(channelGroupWriter, 
-                channelGroupResolver, freeviewPlatformChannelGroupId, 
-                URI_PREFIX, Publisher.BT_TV_CHANNELS);
+    @Bean 
+    public BtChannelGroupUpdater dev1ChannelGroupUpdater() {
+        return perEnvironmentChannelGroupUpdater(Publisher.BT_TV_CHANNELS_TEST1, "bt-test1", test1BaseUri);
     }
+    
+    @Bean 
+    public BtChannelGroupUpdater dev2ChannelGroupUpdater() {
+        return perEnvironmentChannelGroupUpdater(Publisher.BT_TV_CHANNELS_TEST2, "bt-test2", test2BaseUri);
+    }
+    
+    @Bean 
+    public BtChannelGroupUpdater dev3ChannelGroupUpdater() {
+        return perEnvironmentChannelGroupUpdater(Publisher.BT_TV_CHANNELS_REFERENCE, "bt-reference", test3BaseUri);
+    }
+    
+    private BtChannelGroupUpdater perEnvironmentChannelGroupUpdater(Publisher publisher, 
+            String aliasNamespacePrefix, String mpxUriBase) {
+        GsonBtMpxClient mpxClient = new GsonBtMpxClient(httpClient(), baseUri);
+        
+        BtAllChannelsChannelGroupUpdater btAllChannelsChannelGroupUpdater 
+            = new BtAllChannelsChannelGroupUpdater(channelGroupWriter, 
+                channelGroupResolver, freeviewPlatformChannelGroupId, 
+                uriPrefixFromPublisher(publisher), publisher);
+        
+        return new BtChannelGroupUpdater(mpxClient, publisher, uriPrefixFromPublisher(publisher), 
+                aliasNamespacePrefix, channelGroupResolver, channelGroupWriter, 
+                channelResolver, channelWriter, btAllChannelsChannelGroupUpdater, channelWriterLock);
+        
+    }
+    
+    public String uriPrefixFromPublisher(Publisher publisher) {
+        return String.format(URI_PREFIX_STRING_FORMAT, publisher.key());
+    }
+    
     @PostConstruct
     public void scheduleTasks() {
         scheduler.schedule(productionChannelGroupUpdater()
-                .withName("BT Channel Group Ingester"), 
-                DAILY_AT_3PM);
+                .withName("BT Channel Group (PROD) Ingester"), 
+                PROD_INGEST_REPETITION);
+        
+        scheduler.schedule(productionChannelGroupUpdater()
+                .withName("BT Channel Group (TEST1) Ingester"), 
+                TEST1_INGEST_REPETITION);
+        
+        scheduler.schedule(productionChannelGroupUpdater()
+                .withName("BT Channel Group (TEST2) Ingester"), 
+                TEST2_INGEST_REPETITION);
+        
+        scheduler.schedule(productionChannelGroupUpdater()
+                .withName("BT Channel Group (REFERENCE) Ingester"), 
+                REFERENCE_INGEST_REPETITION);
     }
     
 }
